e1198a11188a54e588d0ccaf13ac83e88c7b776b
Boris Boesler <boesler@ipd.info.uni-karlsruhe.de>
2001-03-12 17:17:28 +0000

    autodocified
    [r128]

--- ir/ident/ident.h
+++ ir/ident/ident.h
@@ -12,16 +12,79 @@
 
 # include "assert.h"
 
-/* Identifiers */
-typedef const struct set_entry ident;
+/****h* libfirm/ident
+ *
+ * NAME
+ *   ident -- identifiers in the firm library
+ * NOTES
+ *  Identifiers are used in the firm library. This is the interface to it.
+ *
+ ******
+ */
 
-/* Stores a string in the ident module and returns a handle for
-   the string. */
+/* Identifiers */
+/****s* ident/ident
+ *
+ * NAME
+ *  ident - the abstract data type ident
+ * SOURCE
+ */
+typedef const struct set_entry ident;
+/*****/
+
+/****f* ident/id_from_str
+ *
+ * NAME
+ *  id_from_str - store a string and create an ident
+ * SYNOPSIS
+ *  ident *id = id_from_str (char *str, int len);
+ * FUNCTION
+ *  Stores a string in the ident module and returns a handle for the string.
+ * INPUTS
+ *  str - the string (or whatever) which shall be stored
+ *  len - the length of the data in bytes
+ * RESULT
+ *  id - a handle for the generated ident
+ * SEE ALSO
+ *  id_to_str, id_to_strlen
+ ***
+ */
 inline ident      *id_from_str (char *str, int len);
-/* Returns the string represented by id.  This string is not Null
-   terminated! */
+
+/****f* ident/id_to_str
+ *
+ * NAME
+ *  id_to_str - return a string represented by an ident
+ * SYNOPSIS
+ *  char *cp = id_to_str (ident *id);
+ * FUNCTION
+ *  Returns the string cp represented by id. This string cp is not Null terminated!
+ * INPUTS
+ *  id - the ident
+ * RESULT
+ *  cp - a string
+ * SEE ALSO
+ *  id_from_str, id_to_strlen
+ ***
+ */
 inline const char *id_to_str   (ident *id);
-/* Returns the length of the string represented by id. */
+
+/****f* ident/id_to_strlen
+ *
+ * NAME
+ *  id_to_strlen - return the length of a string represented by an ident
+ * SYNOPSIS
+ *  int len = id_to_strlen (ident *id);
+ * FUNCTION
+ *  Returns the length of string represented by id.
+ * INPUTS
+ *  id - the ident
+ * RESULT
+ *  len - the length of the string
+ * SEE ALSO
+ *  id_from_str, id_to_str
+ ***
+ */
 inline int         id_to_strlen(ident *id);
 
 # endif /* _IDENT_H_ */
