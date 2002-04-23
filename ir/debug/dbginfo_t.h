8410ebe71f429a39822b055009b850ad2169c600
Götz Lindenmaier <goetz@ipd.info.uni-karlsruhe.de>
2002-04-23 12:37:00 +0000

    *** empty log message ***
    [r373]

---
+++ ir/debug/dbginfo_t.h
@@ -0,0 +1,26 @@
+/*
+**  Copyright (C) 2001 by Universitaet Karlsruhe
+**  All rights reserved.
+**
+**  Authors: Goetz Lindenmaier
+**
+**  dbginfo: This is a empty implementation of the Firm interface to
+**  debugging support.  It only guarantees that the Firm library compiles
+**  and runs without any real debugging support.
+**  The functions herein are declared weak so that they can be overriden
+**  by a real implementation.
+*/
+
+/* $Id$ */
+
+#ifndef __DBGINFO_T_H__
+#define __DBGINFO_T_H__
+
+#include "dbginfo.h"
+
+extern void (*__dbg_info_merge_pair)(ir_node *nw, ir_node *old, ident *info);
+
+extern void (*__dbg_info_merge_sets)(ir_node **new_nodes, ir_node **old_nodes,
+				     ident *info);
+
+#endif /* __DBGINFO_T_H__ */
