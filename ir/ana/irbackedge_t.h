45fa79efe8fc4f4a887f7f5243bed08597cc0865
Götz Lindenmaier <goetz@ipd.info.uni-karlsruhe.de>
2002-12-13 12:36:20 +0000

    Added routine to update backedge array if in array changes. untested.
    [r547]

--- ir/ana/irbackedge_t.h
+++ ir/ana/irbackedge_t.h
@@ -10,4 +10,18 @@ static INLINE int * new_backedge_arr(struct obstack *obst, int size) {
 }
 
 
+static INLINE void fix_backedges(struct obstack *obst, ir_node *n) {
+  opcode opc = get_irn_opcode(n);
+  int ** arr = NULL;
+  if (opc == iro_Phi)    arr = &n->attr.phi_backedge;
+  if ((opc == iro_Block) && !interprocedural_view)
+                         arr = &n->attr.block.backedge;
+  if ((opc == iro_Block) && interprocedural_view)
+                         arr = &n->attr.block.cg_backedge;
+  if (opc == iro_Filter) arr = &n->attr.filter.backedge;
+
+  if (ARR_LEN(arr) != ARR_LEN(get_irn_in(n))-1)
+    *arr = new_backedge_arr(obst, ARR_LEN(get_irn_in(n))-1);
+}
+
 #endif /* _IRBACKEDGE_T_H_ */
