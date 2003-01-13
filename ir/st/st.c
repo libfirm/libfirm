50574ae54ef94efe8d1b0c36d8216dd45f12a0df
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2003-01-13 15:38:20 +0000

    Removed non-portable __PRETTY_FUNCTION__
    [r600]

--- ir/st/st.c
+++ ir/st/st.c
@@ -48,7 +48,7 @@ static void count_block (ir_node *block, void *env)
 # endif /* def DEBUG_libfirm */
 
   fprintf (stdout, "%s: Block(%p) has # (%i)\n",
-		   __FILE__ ":" __PRETTY_FUNCTION__, block, *n);
+		   __FILE__ ":count_block", block, *n);
 
   (*n) ++;
 }
@@ -70,7 +70,7 @@ static int get_n_blocks (ir_graph *graph)
   irg_block_walk (end_block, count_block, NULL, &n);
 
   fprintf (stdout, "%s: Graph(%p) has (%i) blocks\n",
-		   __FILE__ ":" __PRETTY_FUNCTION__, graph, n);
+		   __FILE__ ":get_n_blocks", graph, n);
 
   return (n);
 }
