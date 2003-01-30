4e3a6b9cb57f7fd434fbb5bedb65cfa3c558b77a
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2003-01-30 18:29:57 +0000

    converted to doxygen comments
    [r696]

--- ir/ir/irmode_t.h
+++ ir/ir/irmode_t.h
@@ -8,14 +8,14 @@
 
 struct ir_mode {
   modecode code;
-  ident *name;            /* Name of this mode */
-  int    size;            /* size of the mode in Bytes. */
-  int    ld_align;        /* ld means log2 */
-  tarval *min;            /* largest value to be represented by this mode */
-  tarval *max;            /* smallest value to be represented by this mode */
-  tarval *null;           /* Representation of zero in this mode */
-  unsigned fsigned:1;     /* signedness of this mode */
-  unsigned ffloat:1;      /* true if this is a float */
+  ident *name;            /**< Name of this mode */
+  int    size;            /**< size of the mode in Bytes. */
+  int    ld_align;        /**< ld means log2 */
+  tarval *min;            /**< largest value to be represented by this mode */
+  tarval *max;            /**< smallest value to be represented by this mode */
+  tarval *null;           /**< Representation of zero in this mode */
+  unsigned fsigned:1;     /**< signedness of this mode */
+  unsigned ffloat:1;      /**< true if this is a float */
 };
 
 
