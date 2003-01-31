d86e196ccea6af1700c18d873f6f95fd126041c1
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2003-01-31 10:48:37 +0000

    doxygen comments added
    [r698]

--- ir/st/bs.h
+++ ir/st/bs.h
@@ -1,6 +1,6 @@
 /* Copyright (c) 2002 by Universität Karlsruhe (TH).  All Rights Reserved */
 
-/**
+/*
    NAME
      bs
    PURPOSE
@@ -11,20 +11,32 @@
      liekweg - Feb 27, 2002: Created.
    CVS:
      $Id$
-***/
+ */
 
 # ifndef _BS_H_
 # define _BS_H_
 
+/**
+ * the type of a bit set
+ */
 typedef long int bs_t;
 
+/** set bit in a bit set */
 # define bs_set(bs, i) (bs) |= (0x00000001 << i)
+
+/** get bit in a bit set */
 # define bs_get(bs, i) (bs) &  (0x00000001 << i)
 
+/** logical AND of two bit sets */
 # define bs_and(bsa, bsb) (bsa) &= (bsb)
+
+/** logical OR of two bit sets */
 # define bs_or(bsa, bsb)  (bsa) |= (bsb)
+
+/** logical XOR of two bit sets */
 # define bs_xor(bsa, bsb) (bsa) ^= (bsb)
 
+/** returns TRUE if at least one bit is set */
 # define bs_zro(bs) (0x00000000 != bs)
 
 # endif /* ndef _BS_H_ */
