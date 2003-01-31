5af9f7f2bf4bc65ddbc60f4aa55bce607c316476
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2003-01-31 14:39:22 +0000

    comments converted to doxygen
    [r704]

--- ir/ana/irdom.h
+++ ir/ana/irdom.h
@@ -1,16 +1,21 @@
 /* Copyright (C) 2002 by Universitaet Karlsruhe
 * All rights reserved.
 *
-* Authors: Goetz Lindenmaier
+*/
+
+/**
+* @file irdom.h
 *
-* irdom.h:  This file contains routines to construct and access dominator
-*   information.
+*   This file contains routines to construct and access dominator information.
+*
 *   The dominator information is stored in three fields of block nodes:
-*     idom: a reference to the block that is the immediate dominator of
+*     - idom: a reference to the block that is the immediate dominator of
 *       this block.
-*     dom_depth: a number giving the depth of the block in the dominator
+*     - dom_depth: a number giving the depth of the block in the dominator
 *       tree.
-*     pre_num:  Number in preorder traversal.
+*     - pre_num:  Number in preorder traversal.
+*
+* @author Goetz Lindenmaier
 */
 
 /* $Id$ */
@@ -22,12 +27,11 @@
 # include "irnode.h"
 
 
-/**********************************************************************/
-/** Accessing the dopminator datastructure.                          **/
-/** These routines only work properly if the ir_graph is in state    **/
-/** dom_consistent or dom_inconsistent.                              **/
-/**********************************************************************/
-
+/* Accessing the dominator datastructure.
+ *
+ * These routines only work properly if the ir_graph is in state
+ * dom_consistent or dom_inconsistent.
+ */
 ir_node *get_Block_idom(ir_node *bl);
 void set_Block_idom(ir_node *bl, ir_node *n);
 
@@ -38,21 +42,22 @@ int get_Block_pre_num(ir_node *bl);
 void set_Block_pre_num(ir_node *bl, int num);
 
 
-/**********************************************************************/
-/* Building and Removing the dominator datasturcture                 **/
-/**********************************************************************/
+/* ------------ Building and Removing the dominator datasturcture ----------- */
 
-/* Computes the dominator trees.  Sets a flag in irg to "dom_consistent".
-   If the control flow of the graph is changed this flag must be set to
-   "dom_inconsistent".
-   Does not compute dominator information for control dead code.  Blocks
-   not reachable from Start contain the following information:
-     idom = NULL;
-     dom_depth = -1;
-     pre_num = -1; */
+/** Computes the dominator trees.
+ *
+ * Sets a flag in irg to "dom_consistent".
+ * If the control flow of the graph is changed this flag must be set to
+ * "dom_inconsistent".
+ * Does not compute dominator information for control dead code.  Blocks
+ * not reachable from Start contain the following information:
+ *   idom = NULL;
+ *   dom_depth = -1;
+ *   pre_num = -1;
+ */
 void compute_doms(ir_graph *irg);
 
-/* Frees the dominator datastructures.  Sets the flag in irg to "no_dom". */
+/** Frees the dominator datastructures.  Sets the flag in irg to "no_dom". */
 void free_dom_and_peace(ir_graph *irg);
 
 #endif /* _IRDOM_H_ */
