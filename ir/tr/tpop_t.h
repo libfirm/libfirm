823a3abad1ab22cc4db2073410bf8f0e200441bc
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2003-02-03 12:33:26 +0000

    fixed doxygen comments
    [r730]

--- ir/tr/tpop_t.h
+++ ir/tr/tpop_t.h
@@ -1,3 +1,4 @@
+/*   (C) 2001 by Universitaet Karlsruhe  */
 
 /* $Id$ */
 
@@ -7,14 +8,15 @@
 # include <stddef.h>
 # include "tpop.h"
 /**
+ * @file tpop_t.h
  *
- *   file tpop_t.h
- *   (C) 2001 by Universitaet Karlsruhe
- *   Goetz Lindenmaier
- *   This file contains the datatypes hidden in tpop.h.
+ * This file contains the datatypes hidden in tpop.h.
+ *
+ * @autor Goetz Lindenmaier
  * @see  tpop.h
  */
 
+/** The type opcode */
 struct tp_op {
   tp_opcode code;
   ident *name;
@@ -22,38 +24,39 @@ struct tp_op {
 };
 
 /**
- *
  *   Returns a new type opcode.
+ *
  *   Allocates a new tp_op struct and initializes it's fields with
  *   the passed values.  This function is only to be used during
  *   initialization of the library.
- *   @param code      - the enum for this type opcode.
- *   @param name      - an ident for the name of the type opcode.
- *   @param attr_size - the size of the attributes necessary for a type with
- *               this opcode
+ *
+ *   @param code        the enum for this type opcode.
+ *   @param name        an ident for the name of the type opcode.
+ *   @param attr_size   the size of the attributes necessary for a type with
+ *                      this opcode
  *   @return A new type opcode.
  *
  */
 tp_op * new_tpop (tp_opcode code, ident *name, size_t attr_size);
 
 /**
- *
  *   Initialize the tpop module.
+ *
  *   Must be called during the initizlization of the library. Allocates
  *   opcodes and sets the globals that are external visible as specified
  *   in tpop.h.
  *   Allocates opcodes for classes, struct, method, union, array,
  *   enumeration, pointer and primitive and sets the according values.
- *
  */
 void init_tpop (void);
 
 /**
- *
  *   Returns the size of the attribute to this kind
  *   of type.
+ *
  *   Internal feature.
- *   @param op - The type opcode to get the size for.
+ *
+ *   @param op  The type opcode to get the size for.
  *   @return The size of the attribute of types with this opcode.
  *
  */
