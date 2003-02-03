7023ae0df14b6797bdb6fa22050cf7c1c12ab9df
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2003-02-03 12:27:55 +0000

    Fixed doxygen comments
    [r729]

--- ir/tr/tpop.h
+++ ir/tr/tpop.h
@@ -1,3 +1,4 @@
+/* (C) 2001 by Universitaet Karlsruhe */
 
 /* $Id$ */
 
@@ -7,18 +8,18 @@
 #include "ident.h"
 
 /**
+ *  @file tpop.h
  *
- *  file tpop.h
- *   (C) 2001 by Universitaet Karlsruhe
- *   Goetz Lindenmaier
- *  This module specifies the kinds of types available in firm.  They are
- *  called type opcodes. These include classes, structs, methods, unions,
+ *  This module specifies the kinds of types available in firm.
+ *
+ *  @author  Goetz Lindenmaier
+ *
+ *  They are called type opcodes. These include classes, structs, methods, unions,
  *  arrays, enumerations, pointers and primitive types.
  */
 
 /**
- *
- *   - an enum for the type kinds
+ *   an enum for the type kinds.
  *   For each type kind exists a typecode to identify it.
  */
 typedef enum {
@@ -34,47 +35,43 @@ typedef enum {
 } tp_opcode;
 
 /**
- *
- *   a structure containing information about a kind of type.
+ *   A structure containing information about a kind of type.
  *   A structure containing information about a kind of type.  So far
  *   this is only the kind name, an enum for case-switching and some
  *   internal values.
- * @see  get_tpop_name, get_tpop_code, get_tpop_ident
+ *
+ * @see  get_tpop_name(), get_tpop_code(), get_tpop_ident()
  */
 typedef struct tp_op tp_op;
 
 
 /**
- *
  *   Returns the string for the type opcode.
- *   @param op - The type opcode to get the string from.
- *   @return a string.  (@@@ Null terminated???)
  *
+ *   @param op  The type opcode to get the string from.
+ *   @return a string.  (@todo Null terminated???)
  */
 const char *get_tpop_name (tp_op *op);
 
 /**
- *
  *   Returns an enum for the type opcode.
- *   @param op - The type opcode to get the enum from.
- *   @return the enum.
  *
+ *   @param op   The type opcode to get the enum from.
+ *   @return the enum.
  */
 tp_opcode get_tpop_code (tp_op *op);
 
 /**
- *
  *   Returns the ident for the type opcode.
- *   @param op - The type opcode to get the ident from.
- *   @return The ident.
  *
+ *   @param op   The type opcode to get the ident from.
+ *   @return The ident.
  */
 ident *get_tpop_ident (tp_op *op);
 
 /**
- *
- *
  *   This type opcode marks that the corresponding type is a class type.
+ *
  *   Consequently the type refers to supertypes, subtypes and entities.
  *   Entities can be any fields, but also methods.
  *   @@@ value class or not???
@@ -82,58 +79,54 @@ ident *get_tpop_ident (tp_op *op);
  *   of the library.
  */
 extern tp_op *type_class;
-tp_op *get_type_class();
+tp_op *get_type_class(void);
 
 /**
- *
- *   type_struct
  *   This type opcode marks that the corresponding type is a compound type
- *   as a struct in C.  Consequently the type refers to a list of entities
+ *   as a struct in C.
+ *
+ *   Consequently the type refers to a list of entities
  *   which may not be methods (but pointers to methods).
  *   This struct is dynamically allocated but constant for the lifetime
  *   of the library.
  */
 extern tp_op *type_struct;
-tp_op *get_type_struct();
+tp_op *get_type_struct(void);
 
 /**
- *
- *   type_method
  *   This type opcode marks that the corresponding type is a method type.
+ *
  *   Consequently it refers to a list of arguments and results.
  *   This struct is dynamically allocated but constant for the lifetime
  *   of the library.
  */
 extern tp_op *type_method;
-tp_op *get_type_method();
+tp_op *get_type_method(void);
 
 /**
- *
- *   type_union
  *   This type opcode marks that the corresponding type is a union type.
+ *
  *   Consequently it refers to a list of unioned types.
  *   This struct is dynamically allocated but constant for the lifetime
  *   of the library.
  */
 extern tp_op *type_union;
-tp_op *get_type_union();
+tp_op *get_type_union(void);
 
 /**
- *
- *   type_array
  *   This type opcode marks that the corresponding type is an array type.
+ *
  *   Consequently it contains a list of dimensions (lower and upper bounds)
  *   and an element type.
  *   This struct is dynamically allocated but constant for the lifetime
  *   of the library.
  */
 extern tp_op *type_array;
-tp_op *get_type_array();
+tp_op *get_type_array(void);
 
 /**
- *
- *   type_enumeration
  *   This type opcode marks that the corresponding type is an enumeration type.
+ *
  *   Consequently it contains a list of idents for the enumeration identifiers
  *   and a list of tarbet values that are the constants used to implement
  *   the enumerators.
@@ -141,36 +134,34 @@ tp_op *get_type_array();
  *   of the library.
  */
 extern tp_op *type_enumeration;
-tp_op *get_type_enumeration();
+tp_op *get_type_enumeration(void);
 
 /**
- *
- *   type_pointer
  *   This type opcode marks that the corresponding type is a pointer type.
+ *
  *   It contains a reference to the type the pointer points to.
  *   This struct is dynamically allocated but constant for the lifetime
  *   of the library.
  */
 extern tp_op *type_pointer;
-tp_op *get_type_pointer();
+tp_op *get_type_pointer(void);
 
 /**
- *
- *   type_primitive
  *   This type opcode marks that the corresponding type is a primitive type.
+ *
  *   Primitive types are types that are directly mapped to target machine
  *   modes.
  *   This struct is dynamically allocated but constant for the lifetime
  *   of the library.
  */
 extern tp_op *type_primitive;
-tp_op *get_type_primitive();
+tp_op *get_type_primitive(void);
 
 /**
- *
- *   type_id
  *   This type opcode is an auxiliary opcode dedicated to support transformations
- *   of the type structure.  If a type is changed to another type with another
+ *   of the type structure.
+ *
+ *   If a type is changed to another type with another
  *   opcode the new type will be allocated with new memory.  All nodes refering
  *   to the old type need to be changed to refer the new one.  This is simplified
  *   by turning the old type into an id type that merely forwards to the new type
@@ -184,6 +175,6 @@ tp_op *get_type_primitive();
  *   of the library.
  */
 extern tp_op *type_id;
-tp_op *get_type_id();
+tp_op *get_type_id(void);
 
 # endif /*_TYPEOP_H_ */
