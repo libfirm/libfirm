ac478f0c38756f3df1abc00ffe63bd890c16c209
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2003-02-03 11:12:53 +0000

    converted to doxygen comments
    [r725]

--- ir/ir/irop_t.h
+++ ir/ir/irop_t.h
@@ -6,19 +6,20 @@
 
 # include "irop.h"
 
+/** the type of an ir_op */
 struct ir_op {
   opcode code;
   ident *name;
-  size_t attr_size;     /* Space needed in memory for private attributes */
-  int labeled;          /* Output edge labels on in-edges in vcg graph */
-  int pinned;           /* How to deal with the node in cse, pre. */
+  size_t attr_size;     /**< Space needed in memory for private attributes */
+  int labeled;          /**< Output edge labels on in-edges in vcg graph */
+  int pinned;           /**< How to deal with the node in cse, pre. */
 };
 
-/* create a new ir operation */
+/** create a new ir operation */
 ir_op * new_ir_op (opcode code, char *name, op_pinned p,
 		   int labeled, size_t attr_size);
 
-/* initialize the irop module */
+/** initialize the irop module */
 void init_op (void);
 
 #endif
