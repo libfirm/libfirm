a7212f39d345d3bf1a6eef06cf1853f41e409e05
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2004-05-26 12:58:53 +0000

    More functionality implemented, used pset for hash tables
    [r2939]

--- ir/stat/firmstat.c
+++ ir/stat/firmstat.c
@@ -17,8 +17,19 @@
 
 # include "irop_t.h"
 # include "irnode_t.h"
+# include "irgraph_t.h"
+# include "pset.h"
+# include "irprog.h"
+# include "firmstat.h"
 
-# include "xmalloc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#undef obstack_chunk_alloc
+#undef obstack_chunk_free
+#define obstack_chunk_alloc	malloc
+#define obstack_chunk_free	free
+#include <obstack.h>
 
 /*
  * 64 bit should be enough for now
@@ -29,28 +40,211 @@ typedef struct _counter_t {
   unsigned cnt[STAT_CNT_NUM];
 } counter_t;
 
+/*
+ * An entry for ir_nodes
+ */
+typedef struct _node_entry_t {
+  counter_t   new_node;		/**< amount of new nodes for this entry */
+  counter_t   into_Id;		/**< amount of nodes that turned into Id's for this entry */
+  const ir_op *op;		/**< the op for this entry */
+} node_entry_t;
 
-static INLINE cnt_inc(counter_t *cnt)
+/*
+ * An entry for ir_graphs
+ */
+typedef struct _graph_entry_t {
+  pset           *opcode_hash;			/**< hash map containing the opcode counter */
+  counter_t      walked;			/**< walker walked over the graph */
+  counter_t      walked_blocks;			/**< walker walked over the graph blocks */
+  const ir_graph *irg;				/**< the graph of this object */
+  pset           *opt_hash[STAT_OPT_MAX];	/**< hash maps containing opcode counter for optimizations */
+} graph_entry_t;
+
+/**
+ * An entry for optimized ir_nodes
+ */
+typedef struct _opt_entry_t {
+  counter_t   count;		/**< optimization counter */
+  const ir_op *op;		/**< the op for this entry */
+} opt_entry_t;
+
+/**
+ * statistics info
+ */
+typedef struct _statistic_info_t {
+  struct obstack cnts;		/**< obstack containing the counters */
+  pset           *opcode_hash;	/**< hash map containing the opcode counter */
+  pset           *irg_hash;	/**< hash map containing the counter for irgs */
+  FILE           *f;		/**< outputfile */
+  ir_op          *op_Phi0;	/**< needed pseudo op */
+} stat_info_t;
+
+/*
+ * global status
+ */
+static stat_info_t _status, *status = &_status;
+
+/**
+ * increase a counter
+ */
+static INLINE void cnt_inc(counter_t *cnt)
 {
   int i;
 
   for (i = 0; i < STAT_CNT_NUM; ++i) {
     if (++cnt->cnt[i])
       break;
+  }
 }
 
-static INLINE cnt_dec(counter_t *cnt)
+/**
+ * decreace a counter
+ */
+static INLINE void cnt_dec(counter_t *cnt)
 {
   int i;
 
   for (i = 0; i < STAT_CNT_NUM; ++i) {
     if (--cnt->cnt[i] != -1)
       break;
+  }
 }
+
+/**
+ * set a counter to zero
+ */
+static INLINE void cnt_clr(counter_t *cnt)
+{
+  memset(cnt->cnt, 0, sizeof(cnt->cnt));
+}
+
+/*
+ * compare two elements of the opcode hash
+ */
+static int opcode_cmp(const void *elt, const void *key)
+{
+  const node_entry_t *e1 = elt;
+  const node_entry_t *e2 = key;
+
+  return e1->op->code - e2->op->code;
+}
+
+/*
+ * compare two elements of the graph hash
+ */
+static int graph_cmp(const void *elt, const void *key)
+{
+  const graph_entry_t *e1 = elt;
+  const graph_entry_t *e2 = key;
+
+  return e1->irg != e2->irg;
+}
+
+/*
+ * compare two elements of the optimization hash
+ */
+static int opt_cmp(const void *elt, const void *key)
+{
+  const opt_entry_t *e1 = elt;
+  const opt_entry_t *e2 = key;
+
+  return e1->op->code != e2->op->code;
+}
+
+/**
+ * Returns the associates node_entry_t for an ir_op
+ */
+static node_entry_t *opcode_get_entry(const ir_op *op, pset *set)
+{
+  node_entry_t key;
+  node_entry_t *elem;
+
+  key.op = op;
+
+  elem = pset_find(set, &key, op->code);
+  if (elem)
+    return elem;
+
+  elem = obstack_alloc(&status->cnts, sizeof(*elem));
+
+  /* clear new counter */
+  cnt_clr(&elem->new_node);
+  cnt_clr(&elem->into_Id);
+
+  elem->op = op;
+
+  return pset_insert(set, elem, op->code);
+}
+
+/**
+ * calculates a hash value for an irg
+ */
+static INLINE unsigned irg_hash(const ir_graph *irg)
+{
+  return (unsigned)irg;
+}
+
+/**
+ * Returns the acssociates graph_entry_t for an irg
+ */
+static graph_entry_t *graph_get_entry(const ir_graph *irg, pset *set)
+{
+  graph_entry_t key;
+  graph_entry_t *elem;
+  int i;
+
+  key.irg = irg;
+
+  elem = pset_find(set, &key, irg_hash(irg));
+  if (elem)
+    return elem;
+
+  elem = obstack_alloc(&status->cnts, sizeof(*elem));
+
+  cnt_clr(&elem->walked);
+  cnt_clr(&elem->walked_blocks);
+
+  /* new hash table for opcodes here  */
+  elem->opcode_hash = new_pset(opcode_cmp, 8);
+  elem->irg         = irg;
+
+  for (i = 0; i < sizeof(elem->opt_hash)/sizeof(elem->opt_hash[0]); ++i)
+    elem->opt_hash[i] = new_pset(opt_cmp, 8);
+
+  return pset_insert(set, elem, irg_hash(irg));
+}
+
+/**
+ * Returns the associates opt_entry_t for an ir_op
+ */
+static opt_entry_t *opt_get_entry(const ir_op *op, pset *set)
+{
+  opt_entry_t key;
+  opt_entry_t *elem;
+
+  key.op = op;
+
+  elem = pset_find(set, &key, op->code);
+  if (elem)
+    return elem;
+
+  elem = obstack_alloc(&status->cnts, sizeof(*elem));
+
+  /* clear new counter */
+  cnt_clr(&elem->count);
+
+  elem->op = op;
+
+  return pset_insert(set, elem, op->code);
+}
+
+/* ---------------------------------------------------------------------- */
 
 /* A new IR op is registered. */
 void stat_new_ir_op(const ir_op *op)
 {
+  /* execute for side effect :-) */
+  opcode_get_entry(op, status->opcode_hash);
 }
 
 /* An IR op is freed. */
@@ -61,9 +255,210 @@ void stat_free_ir_op(const ir_op *op)
 /* initialize the statistics module. */
 void stat_init(void)
 {
+  obstack_init(&status->cnts);
+
+  /* create the hash-tables */
+  status->opcode_hash = new_pset(opcode_cmp, 8);
+  status->irg_hash    = new_pset(graph_cmp, 8);
+
+  status->f           = fopen("firmstat.txt", "w");
+  status->op_Phi0     = NULL;
 }
 
 /* A new node is created. */
 void stat_new_node(const ir_node *node)
 {
+  node_entry_t *entry;
+  graph_entry_t *graph;
+  ir_op *op = get_irn_op(node);
+
+  if (op->code == iro_Phi && get_irn_arity(node) == 0) {
+    /* special case, a Phi0 node, count on extra counter */
+    if (! status->op_Phi0) {
+      ir_op *op_Phi = get_op_Phi();
+
+      status->op_Phi0 = new_ir_op(0xFFFF, "Phi0", op_Phi->pinned, op_Phi->flags, op_Phi->opar, op_Phi->op_index, op_Phi->attr_size);
+    }
+    op = status->op_Phi0;
+  }
+
+  entry = opcode_get_entry(op, status->opcode_hash);
+  graph = graph_get_entry(current_ir_graph, status->irg_hash);
+
+  /* increase global value */
+  cnt_inc(&entry->new_node);
+
+  /* increase local value */
+  entry = opcode_get_entry(op, graph->opcode_hash);
+  cnt_inc(&entry->new_node);
+}
+
+/* A node is changed into a Id node */
+void stat_turn_into_id(const ir_node *node)
+{
+  node_entry_t *entry;
+  graph_entry_t *graph;
+  ir_op *op = get_irn_op(node);
+
+  if (op->code == iro_Phi && get_irn_arity(node) == 0) {
+    /* special case, a Phi0 node, count on extra counter */
+    if (! status->op_Phi0) {
+      ir_op *op_Phi = get_op_Phi();
+
+      status->op_Phi0 = new_ir_op(0xFFFF, "Phi0", op_Phi->pinned, op_Phi->flags, op_Phi->opar, op_Phi->op_index, op_Phi->attr_size);
+    }
+    op = status->op_Phi0;
+  }
+
+  entry = opcode_get_entry(op, status->opcode_hash);
+  graph = graph_get_entry(current_ir_graph, status->irg_hash);
+
+  /* increase global value */
+  cnt_inc(&entry->into_Id);
+
+  /* increase local value */
+  entry = opcode_get_entry(op, graph->opcode_hash);
+  cnt_inc(&entry->into_Id);
+}
+
+/* A new graph was created */
+void stat_new_graph(const ir_graph *irg)
+{
+  /* execute for side effect :-) */
+  graph_get_entry(irg, status->irg_hash);
+}
+
+/*
+ * A walk over a graph is initiated
+ */
+void stat_irg_walk(const ir_graph *irg, void *pre, void *post)
+{
+  graph_entry_t *graph = graph_get_entry(irg, status->irg_hash);
+
+  cnt_inc(&graph->walked);
+}
+
+/*
+ * A walk over the graph's blocks is initiated
+ */
+void stat_irg_block_walk(const ir_graph *irg, const ir_node *node, void *pre, void *post)
+{
+  graph_entry_t *graph = graph_get_entry(irg, status->irg_hash);
+
+  cnt_inc(&graph->walked_blocks);
+}
+
+/**
+ * called for every node that is removed due to an optimization
+ */
+static void removed_due_opt(ir_node *n, pset *set)
+{
+  ir_op *op          = get_irn_op(n);
+  opt_entry_t *entry = opt_get_entry(op, set);
+
+  /* increase global value */
+  cnt_inc(&entry->count);
+}
+
+/*
+ * Some nodes were optimized into some others due to an optimization
+ */
+void stat_merge_nodes(
+    ir_node **new_node_array, int new_num_entries,
+    ir_node **old_node_array, int old_num_entries,
+    stat_opt_kind opt)
+{
+  int i, j;
+  graph_entry_t *graph = graph_get_entry(current_ir_graph, status->irg_hash);
+
+  for (i = 0; i < old_num_entries; ++i) {
+    for (j = 0; j < new_num_entries; ++j)
+      if (old_node_array[i] == new_node_array[j])
+	break;
+
+    /* nodes might be in new and old, these are NOT removed */
+    if (j >= new_num_entries) {
+      removed_due_opt(old_node_array[i], graph->opt_hash[opt]);
+    }
+  }
+}
+
+/**
+ * dumps a opcode hash
+ */
+static void dump_opcode_hash(pset *set)
+{
+  node_entry_t *entry;
+
+  fprintf(status->f, "%-16s %-8s %-8s\n", "Opcode", "created", "->Id");
+  for (entry = pset_first(set); entry; entry = pset_next(set)) {
+    fprintf(status->f, "%-16s %8d %8d\n",
+	get_id_str(entry->op->name), entry->new_node.cnt[0], entry->into_Id.cnt[0]);
+  }
+}
+
+static const char *opt_names[] = {
+  "straightening optimization",
+  "if simplification",
+  "algebraic simplification",
+  "Phi optmization",
+  "Write-After-Write optimization",
+  "Write-After-Read optimization",
+  "Tuple optimization",
+  "ID optimization",
+  "Constant evaluation",
+};
+
+/**
+ * dumps a optimization hash
+ */
+static void dump_opt_hash(pset *set, int index)
+{
+  opt_entry_t *entry = pset_first(set);
+
+  if (entry) {
+    fprintf(status->f, "\n%s:\n", opt_names[index]);
+    fprintf(status->f, "%-16s %-8s\n", "Opcode", "deref");
+
+    for (; entry; entry = pset_next(set)) {
+      fprintf(status->f, "%-16s %8d\n",
+	  get_id_str(entry->op->name), entry->count.cnt[0]);
+    }
+  }
+}
+
+/* Finish the statistics */
+void stat_finish(void)
+{
+  int i;
+  graph_entry_t *entry;
+  ir_graph *const_irg = get_const_code_irg();
+
+  /* dump global */
+  fprintf(status->f, "\nGlobal counts:\n");
+  dump_opcode_hash(status->opcode_hash);
+
+  for (entry = pset_first(status->irg_hash); entry; entry = pset_next(status->irg_hash)) {
+    entity *ent = entry->irg->ent;
+
+    if (entry->irg == const_irg) {
+      fprintf(status->f, "\nConst code Irg %p", entry->irg);
+    }
+    else {
+      if (ent)
+	fprintf(status->f, "\nEntity %s, Irg %p", get_entity_name(ent), entry->irg);
+      else
+	fprintf(status->f, "\nIrg %p", entry->irg);
+    }
+
+    fprintf(status->f, " walked %d over blocks %d:\n", entry->walked.cnt[0], entry->walked_blocks.cnt[0]);
+
+    dump_opcode_hash(entry->opcode_hash);
+
+    for (i = 0; i < sizeof(entry->opt_hash)/sizeof(entry->opt_hash[0]); ++i) {
+      dump_opt_hash(entry->opt_hash[i], i);
+    }
+  }
+
+  fclose(status->f);
 }
--- ir/stat/firmstat.h
+++ ir/stat/firmstat.h
@@ -15,10 +15,30 @@
 
 #ifdef FIRM_STATISTICS
 
+typedef enum {
+  STAT_OPT_STG        = 0,		/**< straightening optimization */
+  STAT_OPT_IFSIM      = 1,		/**< if simplification */
+  STAT_OPT_ALGSIM     = 2,		/**< algebraic simplification */
+  STAT_OPT_PHI        = 3,		/**< Phi optmization */
+  STAT_OPT_WAW        = 4,		/**< Write-After-Write optimization */
+  STAT_OPT_WAR        = 5,		/**< Write-After-Read optimization */
+  STAT_OPT_TUPLE      = 6,		/**< Tuple optimization */
+  STAT_OPT_ID         = 7,		/**< ID optimization */
+  STAT_OPT_CONST_EVAL = 8,		/**< constant evaluation */
+
+  STAT_OPT_MAX        = 9
+}
+stat_opt_kind;
+
 /**
  * initialize the statistics module.
  */
 void stat_init(void);
+
+/**
+ * Finish the statistics .
+ */
+void stat_finish(void);
 
 /**
  * A new IR op is registered.
@@ -35,14 +55,47 @@ void stat_free_ir_op(const ir_op *op);
  */
 void stat_new_node(const ir_node *node);
 
+/**
+ * A node is changed into a Id node
+ */
+void stat_turn_into_id(const ir_node *node);
+
+/**
+ * A new graph was created
+ */
+void stat_new_graph(const ir_graph *irg);
+
+/**
+ * A walk over a graph is initiated
+ */
+void stat_irg_walk(const ir_graph *irg, void *pre, void *post);
+
+/**
+ * A walk over the graph's blocks is initiated
+ */
+void stat_irg_block_walk(const ir_graph *irg, const ir_node *node, void *pre, void *post);
+
+/**
+ * Some nodes were optimized into some others due to an optimization
+ */
+void stat_merge_nodes(
+    ir_node **new_node_array, int new_num_entries,
+    ir_node **old_node_array, int old_num_entries,
+    stat_opt_kind opt);
+
 #else
 
 #define stat_init()
+#define stat_finish()
 #define stat_new_ir_op(op)
 #define stat_free_ir_op(op)
 #define stat_new_node(node)
+#define stat_turn_into_id(node)
+#define stat_new_graph(irg)
+#define stat_irg_walk(irg, pre, post)
+#define stat_irg_block_walk(irg, node, pre, post)
+#define stat_merge_nodes(new_node_array, new_num_entries, old_node_array, old_num_entries, opt)
 
 #endif
-
 
 #endif /* _FIRMSTAT_H_ */
