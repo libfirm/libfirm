2aeb3c31792527de90cb13b4c99ce8ece09ac519
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2004-07-20 09:20:59 +0000

    added prototypes from irgwalk_blk.c
    [r3517]

--- ir/ir/irgwalk.h
+++ ir/ir/irgwalk.h
@@ -60,7 +60,7 @@ void finish_ip_walk(void);
  * flag.  Therefore current_ir_graph must be set before calling the walker.
  * It marks the node as visited before executing pre.
  * The void* env can be used to pass status information between the
- * pre and post functions.  Does not use the link field.
+ * pre and post functions.  Does not use the link fields.
  *
  * @param node - the start node
  * @param pre  - walker function, executed before the predecessor of a node are visited
@@ -150,5 +150,41 @@ void irg_block_walk_graph(ir_graph *irg, irg_walk_func *pre, irg_walk_func *post
  * Uses visited flag in const_code_irg.  Does not use the link field.
  */
 void walk_const_code(irg_walk_func *pre, irg_walk_func *post, void *env);
+
+/**
+ * Walks over reachable nodes in block-wise order, i.e. visit all nodes in a block
+ * before going to another block, starting at the node given as first argument.
+ * Executes pre before visiting the predecessor of a node, post after.
+ * irg_walk_blkwise() uses the visited flag in irg and the nodes to determine visited
+ * nodes.  It executes inc_irg_visited(current_ir_graph) to generate a new
+ * flag.  Therefore current_ir_graph must be set before calling the walker.
+ * It marks the node as visited before executing pre.
+ * The void *env can be used to pass status information between the
+ * pre and post functions.  Does not use the link fields.
+ * Do NOT leave the graph in interprocedural view.
+ *
+ * @param pre  - walker function, executed before the predecessor of a node are visited
+ * @param post - walker function, executed after the predecessor of a node are visited
+ * @param env  - environment, passend to pre and post
+ *
+ * This function works like irg_walk, except that it enters and leaves blocks only once.
+ * So, the post walker see a 'valid schedule' of the code, the pre-walker a 'reversed schedule'
+ */
+void irg_walk_blkwise(ir_node *node, irg_walk_func *pre, irg_walk_func *post, void *env);
+
+/**
+ * Walks over all reachable nodes in the ir graph in block-wise order.
+ *
+ * @param irg  - the irg graph
+ * @param pre  - walker function, executed before the predecessor of a node are visited
+ * @param post - walker function, executed after the predecessor of a node are visited
+ * @param env  - environment, passend to pre and post
+ *
+ * Like irg_walk_blkwise(), but walks over all reachable nodes in the ir
+ * graph, starting at the end operation. During the walk current_ir_graph
+ * is set to irg.  Does not use the link fields.
+ * Do NOT leave the graph in interprocedural view.
+ */
+void irg_walk_blkwise_graph(ir_graph *irg, irg_walk_func *pre, irg_walk_func *post, void *env);
 
 # endif /* _IRGWALK_H_ */
