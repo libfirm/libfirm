64e76fd68b2da60f28bf2d2f9970dedfe6cf8732
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2004-12-02 16:10:51 +0000

    make FOURCC macro produce right ordering for bigendien machines
    [r4547]

--- ir/adt/fourcc.h
+++ ir/adt/fourcc.h
@@ -12,6 +12,13 @@
 #ifndef _FOURCC_H
 #define _FOURCC_H
 
+#include "firm_config.h"
+
+/* define an always readable fourcc code */
+#ifdef WORDS_BIGENDIAN
+#define FOURCC(a,b,c,d)         ((d) | ((c) << 8) | ((b) << 16) | ((a) << 24))
+#else
 #define FOURCC(a,b,c,d)         ((a) | ((b) << 8) | ((c) << 16) | ((d) << 24))
+#endif
 
 #endif /* _FOURCC_H */
