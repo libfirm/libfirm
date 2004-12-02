33c204f3f48520caec1c852cbbc5c8e85ebda22f
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2004-12-02 14:39:27 +0000

    containing the famous FOURCC macro
    [r4538]

---
+++ ir/adt/fourcc.h
@@ -0,0 +1,17 @@
+/*
+ * Project:     libFIRM
+ * File name:   ir/adt/fourcc.h
+ * Purpose:     define the famous infame FOURCC macro.
+ * Author:
+ * Modified by:
+ * Created:     02.01.2004
+ * CVS-ID:      $Id$
+ * Copyright:   (C) 2004 University of Karlsruhe
+ * Licence:     This file protected by GPL -  GNU GENERAL PUBLIC LICENSE.
+ */
+#ifndef _FOURCC_H
+#define _FOURCC_H
+
+#define FOURCC(a,b,c,d)         ((a) | ((b) << 8) | ((c) << 16) | ((d) << 24))
+
+#endif /* _FOURCC_H */
