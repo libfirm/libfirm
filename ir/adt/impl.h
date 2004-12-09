7d4c0fa6504f0d6c030e04156b806aa08bc60745
Sebastian Hack <hack@ipd.info.uni-karlsruhe.de>
2004-12-09 12:51:43 +0000

    Removed parameter names from macros
    [r4619]

--- ir/adt/impl.h
+++ ir/adt/impl.h
@@ -7,75 +7,75 @@
 #ifndef _IMPL_H
 #define _IMPL_H
 
-#define _IMPL1(name,prefix,ret,res,t1,p1) \
+#define _IMPL1(name,prefix,ret,res,t1) \
 res name (t1 p1) { \
 	ret prefix ## name (p1); \
 }
 
-#define _IMPL2(name,prefix,ret,res,t1,p1,t2,p2) \
+#define _IMPL2(name,prefix,ret,res,t1,t2) \
 res name (t1 p1, t2 p2) { \
 	ret prefix ## name (p1, p2); \
 }
 
-#define _IMPL3(name,prefix,ret,res,t1,p1,t2,p2,t3,p3) \
+#define _IMPL3(name,prefix,ret,res,t1,t2,t3) \
 res name (t1 p1, t2 p2, t3 p3) { \
 	ret prefix ## name (p1, p2, p3); \
 }
 
-#define _IMPL4(name,prefix,ret,res,t1,p1,t2,p2,t3,p3,t4,p4) \
+#define _IMPL4(name,prefix,ret,res,t1,t2,t3,t4) \
 res name (t1 p1, t2 p2, t3 p3, t4 p4) { \
 	ret prefix ## name (p1, p2, p3, p4); \
 }
 
-#define IMPL1_VOID(name,prefix,t1,p1) \
-  _IMPL1(name, prefix, , void, t1, p1)
+#define IMPL1_VOID(name,prefix,t1) \
+  _IMPL1(name, prefix, (void), void, t1)
 
-#define IMPL2_VOID(name,prefix,t1,p1,t2,p2) \
-  _IMPL2(name, prefix, , void, t1, p1, t2, p2)
+#define IMPL2_VOID(name,prefix,t1,t2) \
+  _IMPL2(name, prefix, (void), void, t1, t2)
 
-#define IMPL3_VOID(name,prefix,t1,p1,t2,p2,t3,p3) \
-  _IMPL3(name, prefix, , void, t1, p1, t2, p2, t3, p3)
+#define IMPL3_VOID(name,prefix,t1,t2,t3) \
+  _IMPL3(name, prefix, (void), void, t1, t2, t3)
 
-#define IMPL4_VOID(name,prefix,t1,p1,t2,p2,t3,p3,t4,p4) \
-  _IMPL4(name, prefix, , void, t1, p1, t2, p2, t3, p3, t4, p4)
-
-
-#define IMPL1(name,type,prefix,t1,p1) \
-  _IMPL1(name, prefix, return, type, t1, p1)
-
-#define IMPL2(name,type,prefix,t1,p1,t2,p2) \
-  _IMPL2(name, prefix, return, type, t1, p1, t2, p2)
-
-#define IMPL3(name,type,prefix,t1,p1,t2,p2,t3,p3) \
-  _IMPL3(name, prefix, return, type, t1, p1, t2, p2, t3, p3)
-
-#define IMPL4(name,type,prefix,t1,p1,t2,p2,t3,p3,t4,p4) \
-  _IMPL4(name, prefix, return, type, t1, p1, t2, p2, t3, p3, t4, p4)
+#define IMPL4_VOID(name,prefix,t1,t2,t3,t4) \
+  _IMPL4(name, prefix, (void), void, t1, t2, t3, t4)
 
 
-#define FIRM_IMPL1(name,type,t1,p1) \
+#define IMPL1(name,type,prefix,t1) \
+  _IMPL1(name, prefix, return, type, t1)
+
+#define IMPL2(name,type,prefix,t1,t2) \
+  _IMPL2(name, prefix, return, type, t1, t2)
+
+#define IMPL3(name,type,prefix,t1,t2,t3) \
+  _IMPL3(name, prefix, return, type, t1, t2, t3)
+
+#define IMPL4(name,type,prefix,t1,t2,t3,t4) \
+  _IMPL4(name, prefix, return, type, t1, t2, t3, t4)
+
+
+#define FIRM_IMPL1(name,type,t1) \
   _IMPL1(name, __, return, type, t1, p1)
 
-#define FIRM_IMPL2(name,type,t1,p1,t2,p2) \
-  _IMPL2(name, __, return, type, t1, p1, t2, p2)
+#define FIRM_IMPL2(name,type,t1,t2) \
+  _IMPL2(name, __, return, type, t1, t2)
 
-#define FIRM_IMPL3(name,type,t1,p1,t2,p2,t3,p3) \
-  _IMPL3(name, __, return, type, t1, p1, t2, p2, t3, p3)
+#define FIRM_IMPL3(name,type,t1,t2,t3) \
+  _IMPL3(name, __, return, type, t1, t2, t3)
 
-#define FIRM_IMPL4(name,type,t1,p1,t2,p2,t3,p3,t4,p4) \
-  _IMPL4(name, __, return, type, t1, p1, t2, p2, t3, p3, t4, p4)
+#define FIRM_IMPL4(name,type,t1,t2,t3,t4) \
+  _IMPL4(name, __, return, type, t1, t2, t3, t4)
 
 
-#define FIRM_IMPL1_VOID(name,t1,p1) \
-  _IMPL1(name, __, , void, t1, p1)
+#define FIRM_IMPL1_VOID(name,t1) \
+  _IMPL1(name, __, (void), void, p1)
 
-#define FIRM_IMPL2_VOID(name,t1,p1,t2,p2) \
-  _IMPL2(name, __, , void, t1, p1, t2, p2)
+#define FIRM_IMPL2_VOID(name,t1,t2) \
+  _IMPL2(name, __, (void), void, t1, t2)
 
-#define FIRM_IMPL3_VOID(name,t1,p1,t2,p2,t3,p3) \
-  _IMPL3(name, __, , void, t1, p1, t2, p2, t3, p3)
+#define FIRM_IMPL3_VOID(name,t1,t2,t3) \
+  _IMPL3(name, __, (void), void, t1, t2, t3)
 
-#define FIRM_IMPL4_VOID(name,t1,p1,t2,p2,t3,p3,t4,p4) \
-  _IMPL4(name, __, , void, t1, p1, t2, p2, t3, p3, t4, p4)
+#define FIRM_IMPL4_VOID(name,t1,t2,t3,t4) \
+  _IMPL4(name, __, (void), void, t1, t2, t3, t4)
 
 #endif
