774b22ae73983fbabfc2ce762a262dd68453d62a
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2004-12-21 15:51:07 +0000

    simplifyed
    [r4696]

--- ir/ana2/pto_debug.h
+++ ir/ana2/pto_debug.h
@@ -16,15 +16,15 @@
 # ifndef _PTO_DEBUG_
 # define _PTO_DEBUG_
 
-# include "irnode.h"
+# include "irnode_t.h"
 
 /* ===================================================
    Global Defines:
    =================================================== */
 # define DBGPRINT(lvl, args) if (get_dbg_lvl () > lvl) { fprintf args; }
 # define DBGEXE(lvl, cmd) if (get_dbg_lvl () > lvl) {cmd;}
-# define OPNAME(node) get_op_name (get_irn_op (node))
-# define OPNUM(node) get_irn_node_nr (node)
+# define OPNAME(node) get_irn_opname(node)
+# define OPNUM(node) get_irn_node_nr(node)
 # define HERE(msg)  fprintf (stdout, "%s:%i %s\n", __FUNCTION__, __LINE__, msg)
 # define HERE2(msg1, msg2)  fprintf (stdout, "%s:%i: %s %s\n", __FUNCTION__, __LINE__, msg1, msg2)
 # define HERE3(msg1, msg2, msg3)  fprintf (stdout, "%s:%i: %s %s %s\n", __FUNCTION__, __LINE__, msg1, msg2, msg3)
@@ -51,6 +51,9 @@ void pto_print_pto (ir_node*);
 
 /*
   $Log$
+  Revision 1.4  2004/12/21 15:51:07  beck
+  simplifyed
+
   Revision 1.3  2004/12/20 17:34:35  liekweg
   fix recursion handling
 
