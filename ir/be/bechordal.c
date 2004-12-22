8922ac3e031ddbd8f2c73a5e876d495f7216bab7
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2004-12-22 11:04:09 +0000

    make allocation C-like

--- ir/be/bechordal.c
+++ ir/be/bechordal.c
@@ -86,7 +86,7 @@ static void draw_interval_graphs(ir_node *block,
 
 	if((f = fopen(buf, "wt")) != NULL) {
 		border_t *b;
-		int *seen = calloc(get_graph_node_count(irg), sizeof(int));
+		int *seen = calloc(get_graph_node_count(irg), sizeof(*seen));
 		int last_pos = list_empty(border_head) ? 0 : list_entry(border_head->prev, border_t, list)->step;
 		int max_col = 0;
 
