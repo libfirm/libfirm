4c79f97ef6efa5410b9f57837e255e5b9c094669
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2004-12-23 12:38:44 +0000

    updated doxygen docu, fixed typos
    [r4731]

--- ir/ir/irarch.h
+++ ir/ir/irarch.h
@@ -7,8 +7,8 @@
  * $Id$
  */
 
-#ifndef __FIRM_IRARCH_H
-#define __FIRM_IRARCH_H
+#ifndef _FIRM_IRARCH_H
+#define _FIRM_IRARCH_H
 
 #include "irnode.h"
 
@@ -18,7 +18,7 @@
  */
 typedef struct {
   /* Mul optimization */
-  int also_use_subs : 1;                /**< Use also Subs when resolving muls to shifts */
+  int also_use_subs : 1;                /**< Use also Subs when resolving Muls to shifts */
   int maximum_shifts;                   /**< The maximum number of shifts that shall be inserted for a mul. */
   unsigned highest_shift_amount;        /**< The highest shift amount you want to
 			                     tolerate. Muls which would require a higher
@@ -49,7 +49,7 @@ typedef enum {
   arch_dep_none         = 0,
   arch_dep_mul_to_shift = 1,	/**< optimize Mul into Shift/Add/Sub */
   arch_dep_div_by_const = 2,	/**< optimize Div into Shift/Add/Mulh */
-  arch_dep_mod_by_const = 4	/**< optimize Mod into Shift/Add/Mulh */
+  arch_dep_mod_by_const = 4	    /**< optimize Mod into Shift/Add/Mulh */
 } arch_dep_opts_t;
 
 /**
@@ -74,10 +74,10 @@ void arch_dep_set_opts(arch_dep_opts_t opts);
  * - maximum_shifts
  * - highest_shift_amount
  *
- * If irn is a Mul with a Const, The constant is inspected, if it meets the
+ * If irn is a Mul with a Const, the constant is inspected if it meets the
  * requirements of the three variables stated above. If a Shl/Add/Sub
- * sequence can be generated, that meets these requirements, this expression
- * is returned. In each other case, irn is returned unmodified.
+ * sequence can be generated that meets these requirements, this expression
+ * is returned. In each other case irn is returned unmodified.
  *
  * @param irn       The Firm node to inspect.
  * @return          A replacement expression for irn.
@@ -89,11 +89,12 @@ ir_node *arch_dep_replace_mul_with_shifts(ir_node *irn);
  * This function is driven by the 3 parameters:
  * - allow_mulhu
  * - allow_mulhs
+ * - max_bits_for_mulh
  *
- * If irn is a Div with a Const, The constant is inspected, if it meets the
+ * If irn is a Div with a Const, the constant is inspected if it meets the
  * requirements of the variables stated above. If a Shl/Add/Sub/Mulh
- * sequence can be generated, that meets these requirements, this expression
- * is returned. In each other case, irn is returned unmodified.
+ * sequence can be generated that meets these requirements, this expression
+ * is returned. In each other case irn is returned unmodified.
  *
  * @param irn       The Firm node to inspect.
  * @return          A replacement expression for irn.
@@ -105,11 +106,12 @@ ir_node *arch_dep_replace_div_by_const(ir_node *irn);
  * This function is driven by the 3 parameters:
  * - allow_mulhu
  * - allow_mulhs
+ * - max_bits_for_mulh
  *
- * If irn is a Mod with a Const, The constant is inspected, if it meets the
+ * If irn is a Mod with a Const, the constant is inspected if it meets the
  * requirements of the variables stated above. If a Shl/Add/Sub/Mulh
- * sequence can be generated, that meets these requirements, this expression
- * is returned. In each other case, irn is returned unmodified.
+ * sequence can be generated that meets these requirements, this expression
+ * is returned. In each other case irn is returned unmodified.
  *
  * @param irn       The Firm node to inspect.
  * @return          A replacement expression for irn.
@@ -121,11 +123,12 @@ ir_node *arch_dep_replace_mod_by_const(ir_node *irn);
  * This function is driven by the 3 parameters:
  * - allow_mulhu
  * - allow_mulhs
+ * - max_bits_for_mulh
  *
- * If irn is a DivMod with a Const, The constant is inspected, if it meets the
+ * If irn is a DivMod with a Const, the constant is inspected if it meets the
  * requirements of the variables stated above. If a Shl/Add/Sub/Mulh
- * sequence can be generated, that meets these requirements, this expression
- * is returned. In each other case, irn is returned unmodified.
+ * sequence can be generated that meets these requirements, this expression
+ * is returned. In each other case irn is returned unmodified.
  *
  * @param div       After call contains the Firm node div result or NULL.
  * @param mod       After call contains the Firm node mod result or NULL.
@@ -133,4 +136,4 @@ ir_node *arch_dep_replace_mod_by_const(ir_node *irn);
  */
 void arch_dep_replace_divmod_by_const(ir_node **div, ir_node **mod, ir_node *irn);
 
-#endif
+#endif /* _FIRM_IRARCH_H */
