bdb990ad3f58ef1729795a1bac851deab774286a
Daniel Grund <grund@cs.uni-saarland.de>
2005-01-05 12:26:33 +0000

    Bugfix

--- ir/be/phistat.c
+++ ir/be/phistat.c
@@ -1,5 +1,11 @@
+/**
+ * @author Daniel Grund
+ * @date 09.12.2004
+ */
+
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "config.h"
 
@@ -10,57 +16,145 @@
 #include "irprog.h"
 #include "irmode_t.h"
 #include "bephicongr_t.h"
+#include "bechordal.h"
 
 
-#define SUMMARY_FILE_NAME "all.phi"
+#define SUMMARY_FILE_NAME "all.phistat"
 
-#define ARG_CNT_MAX 10
-#define PHI_CLS_MAX 10
+#define MAX_ARITY 10
+#define MAX_CLS_SIZE 10
 
 enum vals_t {
-	I_PHI_CNT   = 0,
-	I_BLK_CNT,
-	I_SPACE3,
-	I_ARG_CNT_S,
-	I_ARG_CNT_E = I_ARG_CNT_S+ARG_CNT_MAX,
-	I_SPACE1,
-	I_PHI_CLS_S,
-	I_PHI_CLS_E = I_PHI_CLS_S+PHI_CLS_MAX,
-	I_SPACE2,
-	I_INTERFP,          /* number of interfering pairs */
-	I_INTERFV,          /* number of interfering values */
-	I_PHICLSCNT,        /* number of phi classes */
-	I_INTERFPHI,        /* number of phi classes which have interfering vals */
+	I_ARG         = 0,
 	I_CONST,
 	I_PRED,
 	I_GLOB,
+
+	I_SPACE1,
+	I_BLOCKS,
+	I_PHIS,
+	I_PHICLS,
+	I_PHICLSI,
+	I_PAIRS,
+	I_PAIRSI,
+	I_VALUES,
+	I_VALUESI,
+
+	I_SPACE2,
+	I_ARITY_S,
+	I_ARITY_E    = I_ARITY_S+MAX_ARITY,
+
+	I_SPACE3,
+	I_CLS_SIZE_S,
+	I_CLS_SIZE_E = I_CLS_SIZE_S+MAX_CLS_SIZE,
+	I_ALL_NODES,
 	ASIZE
 };
 
 static int curr_vals[ASIZE];
 
 
+/**
+ * Dump statistic values and some annotations for current irp
+ */
+static void dump_file(void) {
+	int i, next;
+	FILE *out;
+	char buf[200];
+
+	next = sprintf(buf, get_irp_prog_name());
+	sprintf(buf+next, ".phistat");
+	out = fopen(buf, "wt");
+
+	fprintf(out, "\nPhi argument types\n");
+	fprintf(out, "Total     %4d\n", curr_vals[I_ARG]);
+	fprintf(out, "Constants %4d\n", curr_vals[I_CONST]);
+	fprintf(out, "CF-Pred   %4d\n", curr_vals[I_PRED]);
+	fprintf(out, "Others    %4d\n", curr_vals[I_GLOB]);
+
+	fprintf(out, "\nPhi class interference\n");
+	fprintf(out, "Blocks         %4d\n", curr_vals[I_BLOCKS]);
+	fprintf(out, "Phis           %4d\n", curr_vals[I_PHIS]);
+	fprintf(out, "Interf classes %4d of %4d\n", curr_vals[I_PHICLSI], curr_vals[I_PHICLS]);
+	fprintf(out, "Interf pairs   %4d of %4d\n", curr_vals[I_PAIRSI], curr_vals[I_PAIRS]);
+	fprintf(out, "Interf values  %4d of %4d\n", curr_vals[I_PAIRSI], curr_vals[I_VALUES]);
+
+	fprintf(out, "\nPhi arity\n");
+	for (i = I_ARITY_S; i<=I_ARITY_E; i++)
+		fprintf(out, "%2i %4d\n", i-I_ARITY_S, curr_vals[i]);
+
+	fprintf(out, "\nPhi class sizes\n");
+	for (i = I_CLS_SIZE_S; i<=I_CLS_SIZE_E; i++)
+		fprintf(out, "%2i %4d\n", i-I_CLS_SIZE_S, curr_vals[i]);
+
+	fprintf(out, "\n\nTotal nodes:    %4d\n", curr_vals[I_ALL_NODES]);
+
+	fclose(out);
+}
+
+
+/**
+ * Updates the summary file with cumulated values
+ */
+static void update_all_file(void) {
+    int i;
+	FILE *all;
+	int vals[ASIZE];
+
+	/* read in */
+	all = fopen(SUMMARY_FILE_NAME, "rt");
+    if (all) {
+		for (i = 0; i<ASIZE; i++) {
+			if (i >= I_ARITY_S && i <= I_ARITY_E)
+				fscanf(all, "%i %i\n", &vals[i], &vals[I_ALL_NODES]);
+			else
+				fscanf(all, "%i\n", &vals[i]);
+		}
+	    fclose(all);
+	} else {
+		for (i = 0; i<ASIZE; i++)
+			vals[i] = 0;
+	}
+
+	/* write out */
+	all = fopen(SUMMARY_FILE_NAME, "wt");
+	for (i = 0; i<ASIZE; i++) {
+		if (i >= I_ARITY_S && i <= I_ARITY_E)
+			fprintf(all, "%i %i\n", vals[i]+curr_vals[i], vals[I_ALL_NODES]+curr_vals[I_ALL_NODES]);
+		else
+			fprintf(all, "%i\n", vals[i]+curr_vals[i]);
+	}
+    fclose(all);
+}
+
+/**
+ * Collect the statistical data
+ */
 static void phi_stat_walker(ir_node *node, void *env) {
- 	int count, i, size;
+ 	int arity, i;
+
+	/* count all nodes */
+	curr_vals[I_ALL_NODES]++;
 
 	/* count all block nodes */
  	if (is_Block(node))
- 		curr_vals[I_BLK_CNT]++;
+ 		curr_vals[I_BLOCKS]++;
 
 	if (!(is_Phi(node) && mode_is_datab(get_irn_mode(node)))) return;
 
 	/* count all phi nodes */
-	curr_vals[I_PHI_CNT]++;
+	curr_vals[I_PHIS]++;
 
 	/* argument count */
-	count = get_irn_arity(node);
-	if (count > ARG_CNT_MAX)
-		curr_vals[I_ARG_CNT_E]++;
+	arity = get_irn_arity(node);
+	curr_vals[I_ARG] += arity;
+	if (arity > MAX_ARITY)
+		curr_vals[I_ARITY_E]++;
 	else
-		curr_vals[I_ARG_CNT_S + count]++;
+		curr_vals[I_ARITY_S + arity]++;
 
 	/* type of argument */
-	for (i = 0; i < count; i++) {
+	for (i = 0; i < arity; i++) {
         ir_node *block_of_arg, *block_ith_pred;
 		ir_node *arg = get_irn_n(node, i);
 
@@ -80,95 +174,57 @@ static void phi_stat_walker(ir_node *node, void *env) {
 		curr_vals[I_GLOB]++;
 	}
 
-
-	size = get_phi_class_size(node);
-	/* phi class count */
-	if (size > 0)
-		curr_vals[I_PHICLSCNT]++;
-
-	/* phi class size */
-	if (size > PHI_CLS_MAX)
-		curr_vals[I_PHI_CLS_E]++;
-	else
-		curr_vals[I_PHI_CLS_S + size]++;
-
-	/* count of interfering pairs / values */
-	curr_vals[I_INTERFP] += get_irn_phi_info(node)->interf_pairs;
-	curr_vals[I_INTERFV] += get_irn_phi_info(node)->interf_vals;
-	if (get_irn_phi_info(node)->interf_vals > 0)
-		curr_vals[I_INTERFPHI]++;
 }
 
+static void phi_class_stat_walker(ir_node *node, void *env) {
+	int i, o, size, doit, sth_interfered;
+	ir_node **members, *p;
+	pset *pc;
 
-/**
- * Dump values and some annotations for current irp
- */
-static void dump_files(void) {
-	int i, sum1, sum2, sum3, next;
-	FILE *out;
-	char buf[200];
+	pc = get_phi_class(node);
+	if (pc) {
 
-	sum1 = curr_vals[I_CONST] + curr_vals[I_PRED] + curr_vals[I_GLOB];
-	sum2 = 0;
-	for (i = I_ARG_CNT_S; i<=I_ARG_CNT_E; i++)
-		sum2 += curr_vals[i];
-	sum3 = 0;
-	for (i = I_PHI_CLS_S; i<=I_PHI_CLS_E; i++)
-		sum3 += curr_vals[i];
+		/* phi class count */
+		curr_vals[I_PHICLS]++;
 
-	next = sprintf(buf, get_irp_prog_name());
-	sprintf(buf+next, ".phi.stat");
-	out = fopen(buf, "wt");
+		/* phi class size */
+		size = pset_count(pc);
+		if (size > MAX_CLS_SIZE)
+			curr_vals[I_CLS_SIZE_E]++;
+		else
+			curr_vals[I_CLS_SIZE_S + size]++;
 
-	fprintf(out, "Phis %i\n", curr_vals[I_PHI_CNT]);
-	fprintf(out, "Blks %i\n", curr_vals[I_BLK_CNT]);
+		/* count interfering pairs / values */
+		members = (ir_node **) malloc(size * sizeof(ir_node*));
 
-	fprintf(out, "\nArgument counts\n");
-	for (i = I_ARG_CNT_S; i<=I_ARG_CNT_E; i++)
-		fprintf(out, "%2i %2.4f\n", i-I_ARG_CNT_S, (double) curr_vals[i] / sum2);
+		for (i=0, p = (ir_node *)pset_first(pc); p; p = (ir_node *)pset_next(pc))
+			members[i++] = p;
+		assert(i == size);
 
-	fprintf(out, "\nPhi class sizes\n");
-	for (i = I_PHI_CLS_S; i<=I_PHI_CLS_E; i++)
-		fprintf(out, "%2i %2.4f\n", i-I_PHI_CLS_S, (double) curr_vals[i] / sum3);
+		/* determine interference of phi args */
+		curr_vals[I_VALUES] += size;
+		sth_interfered = 0;
+		for (i = 0; i < size-1; ++i) {
+			doit = 1;
+			for (o = i+1; o < size; ++o) {
+				curr_vals[I_PAIRS]++;
+				if (phi_ops_interfere(members[i], members[o])) {
+					sth_interfered = 1;
+					curr_vals[I_PAIRSI]++;
+					if (doit) {
+						curr_vals[I_VALUESI]++;
+						doit = 0;
+					}
+				}
+			}
+		}
 
-	fprintf(out, "\n");
-	fprintf(out, "Interf Pairs %d\n", curr_vals[I_INTERFP]);
-	fprintf(out, "Interf Values %d\n", curr_vals[I_INTERFV]);
-	fprintf(out, "PhiCls Count %d\n", curr_vals[I_PHICLSCNT]);
-	fprintf(out, "Interf PhisCl %d\n", curr_vals[I_INTERFPHI]);
-	fprintf(out, "Const %2.2f\n", (double) curr_vals[I_CONST] / sum1);
-	fprintf(out, "Pred %2.2f\n", (double) curr_vals[I_PRED] / sum1);
-	fprintf(out, "Glob %2.2f\n", (double) curr_vals[I_GLOB] / sum1);
-
-	fclose(out);
-}
-
-
-/**
- * Updates the summary file with cumulated values
- */
-static void update_all_file(void) {
-    int i;
-	FILE *all;
-	int vals[ASIZE];
-
-	/* read in */
-	all = fopen(SUMMARY_FILE_NAME, "rt");
-    if (all) {
-		for (i = 0; i<ASIZE; i++)
-			fscanf(all, "%i\n", &vals[i]);
-	    fclose(all);
-	} else {
-		for (i = 0; i<ASIZE; i++)
-			vals[i] = 0;
+		/* has this phi class an interference? */
+		curr_vals[I_PHICLSI] += sth_interfered;
+		free(members);
 	}
-
-	/* write out */
-	all = fopen(SUMMARY_FILE_NAME, "wt");
-	for (i = 0; i<ASIZE; i++)
-		fprintf(all, "%i\n", vals[i]+curr_vals[i]);
-    fclose(all);
 }
+
 
 void do_phi_statistics(void) {
 	int i, n;
@@ -179,9 +235,10 @@ void do_phi_statistics(void) {
 	for (i = 0, n = get_irp_n_irgs(); i < n; i++) {
 		ir_graph *irg = get_irp_irg(i);
 		irg_walk_graph(irg, phi_stat_walker, NULL, NULL);
-		curr_vals[I_BLK_CNT] -= 2;
+		irg_walk_graph(irg, phi_class_stat_walker, NULL, NULL);
+		curr_vals[I_BLOCKS] -= 2;
 	}
 
-	dump_files();
+	dump_file();
 	update_all_file();
 }
