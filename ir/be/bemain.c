81676e143f3386ea6511c72d14d357becb80002a
Daniel Grund <grund@cs.uni-saarland.de>
2005-01-15 02:03:14 +0000

    removed call to my funcs. had to check in but couldn't test

--- ir/be/bemain.c
+++ ir/be/bemain.c
@@ -126,7 +126,7 @@ static void be_main_loop(void)
 #ifdef DUMP_ALLOCATED
 		dump_allocated_irg(irg);
 #endif
-		be_phi_opt(irg);
+		//be_phi_opt(irg);
 
 		be_ra_chordal_done(irg);
 		be_numbering_done(irg);
