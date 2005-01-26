99e7c98691cdd138dfb32e2e040b76dfc4c92b39
Sebastian Hack <hack@ipd.info.uni-karlsruhe.de>
2005-01-26 16:15:07 +0000

    New internal regalloc header

---
+++ ir/be/bechordal_t.h
@@ -0,0 +1,32 @@
+
+/**
+ * Internal datastructures for the chordal register allocator.
+ * @author Sebastian Hack
+ * @date 25.1.2005
+ */
+
+#ifndef _BECHORDAL_T_H
+#define _BECHORDAL_T_H
+
+/**
+ * A liveness interval border.
+ */
+typedef struct _border_t {
+#ifdef DEBUG_libfirm
+	unsigned magic;								/**< A magic number for checking. */
+#endif
+	struct list_head list;				/**< list head for queuing. */
+	struct _border_t *other_end;	/**< The other end of the border. */
+	const ir_node *irn;						/**< The node. */
+	unsigned step;								/**< The number equal to the interval border. */
+	unsigned pressure;						/**< The pressure at this interval border.
+																	(The border itself is counting). */
+	unsigned is_def : 1;					/**< Does this border denote a use or a def. */
+	unsigned is_real : 1;					/**< Is the def/use real? Or is it just inserted
+																	at block beginnings or ends to ensure that inside
+																	a block, each value has one begin and one end. */
+} border_t;
+
+extern void be_ra_chordal_spill(ir_graph *irg);
+
+#endif /* _BECHORDAL_T_H */
