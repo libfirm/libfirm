984fa970f9c6d5dfe9d363257d24ab6af688b7a9
Sebastian Hack <hack@ipd.info.uni-karlsruhe.de>
2005-01-26 10:06:11 +0000

    Added spilling file

---
+++ ir/be/bechordalspill.c
@@ -0,0 +1,33 @@
+/**
+ * Spilling for chordal interference graphs.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "obst.h"
+#include "list.h"
+#include "bitset.h"
+#include "iterator.h"
+
+#include "irmode_t.h"
+#include "irgraph_t.h"
+#include "irprintf_t.h"
+#include "irgwalk.h"
+#include "irdump.h"
+#include "irdom.h"
+#include "debug.h"
+#include "xmalloc.h"
+
+#include "beutil.h"
+#include "besched.h"
+#include "bera_t.h"
+#include "benumb_t.h"
+#include "besched_t.h"
+#include "belive_t.h"
+#include "bechordal_t.h"
+
+void be_ra_chordal_spill(ir_graph *irg)
+{
+}
