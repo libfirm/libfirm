8232963d5867b775e4251f6ba2bef69591042581
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2005-03-01 15:31:15 +0000

    scalar replacement added
    [r5270]

---
+++ ir/opt/scalar_replace.c
@@ -0,0 +1,20 @@
+/*
+ * Project:     libFIRM
+ * File name:   ir/opt/scalar_replace.c
+ * Purpose:     scalar replacement of arrays and compounds
+ * Author:      Beyhan
+ * Created:
+ * CVS-ID:      $Id$
+ * Copyright:   (c) 1998-2005 Universität Karlsruhe
+ * Licence:     This file protected by GPL -  GNU GENERAL PUBLIC LICENSE.
+ */
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+/*
+ * find possible scalar replacements
+ */
+void find_scalar_replacements(irgraph *irg)
+{
+}
