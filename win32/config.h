109868f7700a29a61100c417ef38b7e994579185
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2005-05-13 14:57:33 +0000

    io.h define added
    [r5842]

--- win32/config.h
+++ win32/config.h
@@ -54,6 +54,9 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
+/* Define to 1 if you have the <io.h> header file. */
+#define HAVE_IO_H 1
+
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
 
