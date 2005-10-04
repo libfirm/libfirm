12a5e831792ad7c520c7402bcb8d49906dfa7522
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2005-10-04 15:50:45 +0000

    added configure stuff
    [r6654]

--- ir/ir/iredges.c
+++ ir/ir/iredges.c
@@ -4,10 +4,15 @@
  * @date 14.1.2005
  */
 
-#include <alloca.h>
-
 #ifdef HAVE_CONFIG_H
 #include "config.h"
+#endif
+
+#ifdef HAVE_ALLOCA_H
+#include <alloca.h>
+#endif
+#ifdef HAVE_MALLOC_H
+#include <malloc.h>
 #endif
 
 #include "irnode_t.h"
