b5a167c64808986a1409fa52aee70b76d6efd4f6
Christian Würdig <chriswue@ipd.info.uni-karlsruhe.de>
2005-11-02 14:45:39 +0000

    added offset and symconst as attributes

--- ir/be/firm2arch_nodes_attr.h
+++ ir/be/firm2arch_nodes_attr.h
@@ -4,7 +4,13 @@
 #include "firm_types.h"
 
 typedef struct {
-  tarval *tv;
+  enum { asmop_Const, asmop_SymConst } tp;
+  struct {
+    tarval  *tv;       /**<< tarval for immediate operations */
+    tarval  *offset;   /**<< offset for LEA */
+    ir_node *symconst; /**<< old symconst node in case of asmop_SymConst */
+  } data;
+
   void *reg_constraints;
 } asmop_attr;
 
