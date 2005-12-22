430b7a78c8e1b631b7051b30b4656236283b1eb6
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2005-12-22 11:50:18 +0000

    compute_loop_info moved into an own file
    [r7114]

---
+++ ir/ana/compute_loop_info.h
@@ -0,0 +1,18 @@
+/*
+ * Project:     libFIRM
+ * File name:   ir/ana/compute_loop_info.h
+ * Purpose:     Construct some loop infromation.
+ * Author:      Beyhan Veliev
+ * Created:
+ * CVS-ID:      $Id$
+ * Copyright:   (c) 1998-2005 Universität Karlsruhe
+ * Licence:     This file protected by GPL -  GNU GENERAL PUBLIC LICENSE.
+ */
+#ifndef _COMPUTE_LOOP_INFO_
+#define _COMPUTE_LOOP_INFO_
+
+#include "irgraph.h"
+
+void compute_loop_info(ir_graph *irg);
+
+#endif  /* _COMPUTE_LOOP_INFO_ */
