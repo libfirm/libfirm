b07ec8c98f79e2f8404c47f769cddf4062fc18a5
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2005-12-27 17:13:55 +0000

    includes firm_types.h now
    [r7152]

--- ir/tr/type_or_entity.h
+++ ir/tr/type_or_entity.h
@@ -21,10 +21,12 @@
 # ifndef _TYPE_OR_ENTITY_H_
 # define _TYPE_OR_ENTITY_H_
 
-/** A datatype to treat types and entities as the same. */
+#include "firm_types.h"
+
+/** A data type to treat types and entities as the same. */
 typedef union {
-  struct type   *typ;     /**< points to a type */
-  struct entity *ent;     /**< points to an entity */
+  ir_type *typ;     /**< points to a type */
+  entity  *ent;     /**< points to an entity */
 } type_or_ent;
 
 
