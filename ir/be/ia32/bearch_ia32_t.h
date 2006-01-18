2808fdeddf46746cb0580dbc422da9afe6499331
Christian Würdig <chriswue@ipd.info.uni-karlsruhe.de>
2006-01-18 15:54:23 +0000

    new header for internal usage

---
+++ ir/be/ia32/bearch_ia32_t.h
@@ -0,0 +1,16 @@
+#ifndef _BEARCH_IA32_T_H_
+#define _BEARCH_IA32_T_H_
+
+#include "bearch_ia32.h"
+
+typedef struct _ia32_code_gen_t {
+	const arch_code_generator_if_t *impl;     /* implementation */
+	ir_graph                       *irg;      /* current irg */
+	FILE                           *out;      /* output file */
+	const arch_env_t               *arch_env; /* the arch env */
+	set                            *reg_set;  /* set to memorize registers for non-ia32 nodes (e.g. phi nodes) */
+	firm_dbg_module_t              *mod;      /* debugging module */
+	int                             emit_decls;
+} ia32_code_gen_t;
+
+#endif /* _BEARCH_IA32_T_H_ */
