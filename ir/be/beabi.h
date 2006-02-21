bcc3cc69f3fe3b5262f456ef9a7398f708a7cb21
Sebastian Hack <hack@ipd.info.uni-karlsruhe.de>
2006-02-21 12:41:52 +0000

    Added header files for ABI

---
+++ ir/be/beabi.h
@@ -0,0 +1,23 @@
+
+/**
+ * Backend ABI implementation.
+ */
+
+#ifndef _BEABI_H
+#define _BEABI_H
+
+#include "bearch.h"
+#include "beabi_t.h"
+
+typedef enum {
+	BE_ABI_NONE = 0,
+	BE_ABI_LEFT_TO_RIGHT = 1,
+	BE_ABI_USE_PUSH = 2
+} be_abi_call_flags_t;
+
+void be_abi_call_set_flags(be_abi_call_t *call, be_abi_call_flags_t flags);
+void be_abi_call_param_stack(be_abi_call_t *call, int pos);
+void be_abi_call_param_reg(be_abi_call_t *call, int pos, const arch_register_t *reg);
+void be_abi_call_res_reg(be_abi_call_t *call, int pos, const arch_register_t *reg);
+
+#endif
---
+++ ir/be/beabi_t.h
@@ -0,0 +1,12 @@
+
+/**
+ * Type declarations for the BAI module.
+ */
+
+#ifndef _BEABI_T_h
+#define _BEABI_T_h
+
+typedef struct _be_abi_call_t be_abi_call_t;
+typedef struct _be_abi_irg_t  be_abi_irg_t;
+
+#endif
