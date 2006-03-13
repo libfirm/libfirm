a8648d35f25f74878280f352d740fb7a50a56be0
Christian Würdig <chriswue@ipd.info.uni-karlsruhe.de>
2006-03-13 11:33:16 +0000

    initial checkin for BEARCH typedefs

---
+++ ir/be/bearch_t.h
@@ -0,0 +1,21 @@
+
+/**
+ * Type declarations for the BEARCH module.
+ * $Id$
+ */
+
+#ifndef _BEARCH_T_H_
+#define _BEARCH_T_H_
+
+typedef struct _arch_register_class_t     arch_register_class_t;
+typedef struct _arch_register_t           arch_register_t;
+typedef struct _arch_isa_if_t             arch_isa_if_t;
+typedef struct _arch_isa_t                arch_isa_t;
+typedef struct _arch_env_t                arch_env_t;
+typedef struct _arch_irn_ops_if_t         arch_irn_ops_if_t;
+typedef struct _arch_irn_ops_t            arch_irn_ops_t;
+typedef struct _arch_irn_handler_t        arch_irn_handler_t;
+typedef struct _arch_code_generator_t     arch_code_generator_t;
+typedef struct _arch_code_generator_if_t  arch_code_generator_if_t;
+
+#endif /* _BEARCH_T_H_ */
