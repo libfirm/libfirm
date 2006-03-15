ab8af87bb40750c4a5271a43728a4af5503b6668
Sebastian Hack <hack@ipd.info.uni-karlsruhe.de>
2006-03-15 16:22:17 +0000

    Added some types

--- ir/be/beabi_t.h
+++ ir/be/beabi_t.h
@@ -6,7 +6,10 @@
 #ifndef _BEABI_T_h
 #define _BEABI_T_h
 
-typedef struct _be_abi_call_t be_abi_call_t;
-typedef struct _be_abi_irg_t  be_abi_irg_t;
+typedef struct _be_abi_call_flags_bits_t be_abi_call_flags_bits_t;
+typedef union  _be_abi_call_flags_t      be_abi_call_flags_t;
+typedef struct _be_abi_callbacks_t       be_abi_callbacks_t;
+typedef struct _be_abi_call_t            be_abi_call_t;
+typedef struct _be_abi_irg_t             be_abi_irg_t;
 
 #endif
