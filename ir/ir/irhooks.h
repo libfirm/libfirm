de8269f08de3f68e62dfc6fe7f0c29c976489fdd
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2006-03-18 12:05:11 +0000

    HOOK_OPT_SYNC added
    [r7468]

--- ir/ir/irhooks.h
+++ ir/ir/irhooks.h
@@ -34,6 +34,7 @@ typedef enum {
   HOOK_OPT_CONST_EVAL,  /**< constant evaluation */
   HOOK_OPT_ALGSIM,      /**< algebraic simplification */
   HOOK_OPT_PHI,         /**< Phi optmization */
+  HOOK_OPT_SYNC,        /**< Sync optmization */
   HOOK_OPT_WAW,         /**< Write-After-Write optimization */
   HOOK_OPT_WAR,         /**< Write-After-Read optimization */
   HOOK_OPT_RAW,         /**< Read-After-Write optimization */
