7a6ba6c35fd9fa2b50019b476b9d9bb502fb8f8b
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2006-03-20 00:44:26 +0000

    doxygen docu added

--- ir/be/be.h
+++ ir/be/be.h
@@ -3,10 +3,14 @@
 #define _BE_MAIN_H
 
 void be_init(void);
+
+/**
+ * Main interface to the frontend.
+ */
 void be_main(FILE *file_handle);
 
 typedef struct _be_main_env_t be_main_env_t;
 typedef struct _be_irg_t be_irg_t;
 typedef struct _be_options_t be_options_t;
 
-#endif
+#endif /* _BE_MAIN_H */
