e16f7acac75b8094921f62ffe2c90d0ba65dc026
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2006-03-27 12:58:20 +0000

    added support for x87 fp unit

--- ir/be/ia32/bearch_ia32.h
+++ ir/be/ia32/bearch_ia32.h
@@ -4,6 +4,9 @@
 #include "pset.h"
 #include "../bearch.h"
 
+#define USE_SSE2(cg) ((cg)->fp_kind == fp_sse2)
+#define USE_x87(cg)  ((cg)->fp_kind == fp_x87)
+
 extern const arch_isa_if_t ia32_isa_if;
 
 #endif /* _BEARCH_IA32_H_ */
