135b5932836da8299feccd3a4e0e54b900da7ce0
Christian Würdig <chriswue@ipd.info.uni-karlsruhe.de>
2006-04-06 16:01:30 +0000

    removed 64bit convs

--- ir/be/test/biggest_prime.c
+++ ir/be/test/biggest_prime.c
@@ -1,12 +1,12 @@
 int m = 754974721, N, t[1 << 22], a, *p, i, e = 1 << 22, j, s, b, c, U;
 f (d)
 {
-  for (s = 1 << 23; s; s /= 2, d = d * 1LL * d % m)
+  for (s = 1 << 23; s; s /= 2, d = d * 1L * d % m)
     if (s < N)
       for (p = t; p < t + N; p += s)
         for (i = s, c = 1; i; i--)
           b = *p + p[s], p[s] = (m + *p - p[s]) *
-          1LL * c % m, *p++ = b % m, c = c * 1LL * d % m;
+          1L * c % m, *p++ = b % m, c = c * 1L * d % m;
   for (j = 0; i < N - 1;)
     {
       for (s = N / 2; !((j ^= s) & s); s /= 2);
@@ -22,10 +22,10 @@ main ()
   while (e /= 2)
     {
       N *= 2;
-      U = U * 1LL * (m + 1) / 2 % m;
+      U = U * 1L * (m + 1) / 2 % m;
       f (362);
       for (p = t; p < t + N;)
-	*p++ = (*p * 1LL ** p % m) * U % m;
+	*p++ = (*p * 1L ** p % m) * U % m;
       f (415027540);
       for (a = 0, p = t; p < t + N;)
 	a += (6972593 & e ? 2 : 1) ** p, *p++ = a % 10, a /= 10;
