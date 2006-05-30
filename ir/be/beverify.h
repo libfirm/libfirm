22956a6dc537339e29736e61a3f76ca28abca741
Christian Würdig <chriswue@ipd.info.uni-karlsruhe.de>
2006-05-30 09:05:36 +0000

    added cvs id updated comments

--- ir/be/beverify.h
+++ ir/be/beverify.h
@@ -1,8 +1,9 @@
 /**
- * Author:      Matthias Braun
- * Date:		05.05.2006
- * Copyright:   (c) Universitaet Karlsruhe
- * License:     This file protected by GPL -  GNU GENERAL PUBLIC LICENSE.
+ * Author:    Matthias Braun
+ * Date:      05.05.2006
+ * Copyright: (c) Universitaet Karlsruhe
+ * License:   This file protected by GPL -  GNU GENERAL PUBLIC LICENSE.
+ * CVS-Id:    $Id$
  */
 
 /**
@@ -20,12 +21,20 @@
 /**
  * Verifies, that the register pressure for a given register class doesn't exceed the limit
  * of available registers.
+ *
+ * @param arch_env   An architecture environment
+ * @param cls        The register class to check
+ * @param irg        The irg to check
+ * @return 1 on success, 0 otherwise
  */
 int be_verify_register_pressure(const arch_env_t *arch_env, const arch_register_class_t* cls, ir_graph *irg);
 
 /**
- * Does some sanity checks on the schedule
+ * Does some sanity checks on the schedule.
+ *
+ * @param irg  The irg to check
+ * @return 1 on success, 0 otherwise
  */
 int be_verify_schedule(ir_graph *irg);
 
-#endif
+#endif /* BEVERIFY_H_ */
