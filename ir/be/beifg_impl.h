e3326a81340415c0ba2874f404996bf57c8d09de
Johannes Spallek <spallek@ipd.info.uni-karlsruhe.de>
2006-05-31 15:13:05 +0000

    added new implementations to create an ifg

--- ir/be/beifg_impl.h
+++ ir/be/beifg_impl.h
@@ -16,5 +16,8 @@
 #include "bechordal_t.h"
 
 be_ifg_t *be_ifg_std_new(const be_chordal_env_t *env);
+be_ifg_t *be_ifg_list_new(const be_chordal_env_t *env);
+be_ifg_t *be_ifg_clique_new(const be_chordal_env_t *env);
+be_ifg_t *be_ifg_pointer_new(const be_chordal_env_t *env);
 
 #endif /* _BEIFG_IMPL_H */
