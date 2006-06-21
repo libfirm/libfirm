59128b1c881ffc7e06667a515bee860e619e3988
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2006-06-21 17:47:40 +0000

    updated for gcc version

--- ir/be/test/valist.c
+++ ir/be/test/valist.c
@@ -3,6 +3,10 @@
 #define va_end(v)	__builtin_va_end(v)
 #define va_arg(v,l)	__builtin_va_arg(v,l)
 #define va_copy(d,s)	__builtin_va_copy(d,s)
+
+typedef __builtin_va_list __gnuc_va_list;
+typedef __gnuc_va_list va_list;
+
 #else
 #include <stdarg.h>
 #endif
