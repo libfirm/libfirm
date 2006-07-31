fb3da822bfb91a6669d0365ec98fe47d90e3e583
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2006-07-31 11:24:31 +0000

    removed useless includes
    [r8077]

--- ir/ana/execfreq.h
+++ ir/ana/execfreq.h
@@ -1,10 +1,19 @@
 #ifndef EXECFREQ_H_
 #define EXECFREQ_H_
+/*
+ * Project:     libFIRM
+ * File name:   ir/ana/execfreq.h
+ * Purpose:     Compute an estimate of basic block executions.
+ * Author:      Adam M. Szalkowski
+ * Modified by:
+ * Created:     28.05.2006
+ * CVS-ID:      $Id$
+ * Copyright:   (c) 2006 Universitaet Karlsruhe
+ * Licence:     This file protected by GPL -  GNU GENERAL PUBLIC LICENSE.
+ */
+#include "firm_types.h"
 
-#include "irgraph_t.h"
-#include "irnode_t.h"
-#include "set.h"
-
+struct _exec_freq_t;
 typedef struct _exec_freq_t exec_freq_t;
 
 exec_freq_t * compute_execfreq(ir_graph *irg, double loop_weight);
@@ -14,4 +23,4 @@ void free_execfreq(exec_freq_t *ef);
 double get_block_execfreq(const exec_freq_t *ef, const ir_node * irn);
 unsigned long get_block_execfreq_ulong(const exec_freq_t *ef, const ir_node *bb);
 
-#endif
+#endif /* EXECFREQ_H_ */
