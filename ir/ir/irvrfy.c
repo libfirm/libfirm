faf861191503aa1160f3c3b8028d263f28e29cb3
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2006-07-31 09:41:30 +0000

    temporary switched off the detection of more than one node of mode M from the same block: this happens with the current exception implementation
    [r8075]

--- ir/ir/irvrfy.c
+++ ir/ir/irvrfy.c
@@ -1440,12 +1440,15 @@ static int verify_node_Phi(ir_node *n, ir_graph *irg) {
 
         if (is_Bad(pred_j))
           continue;
+#if 0
+        /* did NOT work with the current exception model where blocks a NOT basic blocks */
         ASSERT_AND_RET_DBG(
           (pred_i == pred_j) || (get_irn_n(pred_i, -1) != get_irn_n(pred_j, -1)),
           "At least two different PhiM predecessors are in the same block",
           0,
           ir_printf("%+F and %+F of %+F are in %+F\n", pred_i, pred_j, n, get_irn_n(pred_i, -1))
         );
+#endif
       }
     }
   }
