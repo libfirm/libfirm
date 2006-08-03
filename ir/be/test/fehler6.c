3dc176702165e8a201aad7ec1adefe28c3df7e07
Matthias Braun <matze@braunis.de>
2006-08-03 13:53:48 +0000

    reindenting source

--- ir/be/test/fehler6.c
+++ ir/be/test/fehler6.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 int main()
-   {
-   int i, n=3 , v, dig, set;
+{
+	int i, n=3 , v, dig, set;
 
-   //printf ("Enter n: ");
-   //scanf ("%d", &n);
+	//printf ("Enter n: ");
+	//scanf ("%d", &n);
 
-   v = 1 << n;
+	v = 1 << n;
 
-   for (i=0; i < v; i++) {
-      set = i ^ (i>>1);
+	for (i=0; i < v; i++) {
+		set = i ^ (i>>1);
 
-	printf(" i: %d  set: %d \n",i,set);
-      for (dig=1 << (n-1); dig; dig >>= 1)
-	{
-		printf("\ni: %d v: %d dig: %d set:%d\n",i,v,dig, set);
-         	printf (" %d", ((set & dig) ? 1 : 0));
-		printf("\ni: %d v: %d dig: %d set:%d\n",i,v,dig, set);
+		printf(" i: %d  set: %d \n",i,set);
+		for (dig=1 << (n-1); dig; dig >>= 1)
+		{
+			printf("\ni: %d v: %d dig: %d set:%d\n",i,v,dig, set);
+			printf (" %d", ((set & dig) ? 1 : 0));
+			printf("\ni: %d v: %d dig: %d set:%d\n",i,v,dig, set);
+		}
+		printf ("\n");
 	}
-      printf ("\n");
-      }
 
-	 return 0;
-   }
+	return 0;
+}
