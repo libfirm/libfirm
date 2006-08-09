d32f2405e6f9cf480f6db0c62169b376e1d27fbf
Matthias Braun <matze@braunis.de>
2006-08-09 11:27:27 +0000

    return 0 from main function

--- ir/be/test/adam_putt.c
+++ ir/be/test/adam_putt.c
@@ -1,18 +1,21 @@
+#include <stdio.h>
+
 int hallo()
 {
-    int i = 0;
-      int j = 2;
+	int i = 0;
+	int j = 2;
 
-        i = add(i, j, i, j);
+	i = add(i, j, i, j);
 
-	  return i;
+	return i;
 }
 
 int add(int i, int j, int k, int l)
 {
-    return i + j + k +l;
+	return i + j + k +l;
 }
 
 int main() {
-	 printf("%d\n", hallo());
+	printf("%d\n", hallo());
+	return 0;
 }
