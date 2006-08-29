c4210affe324ffb34c5119bd012420f209843f44
Sebastian Hack <hack@ipd.info.uni-karlsruhe.de>
2006-08-29 08:14:58 +0000

    Re-checked it in.
    [r8138]

---
+++ ir/ir/iredgekinds.h
@@ -0,0 +1,28 @@
+/**
+ * @file   iredgekinds.h
+ * @date   29.08.2006
+ * @author Sebastian Hack
+ *
+ * Copyright (C) 2006 Universitaet Karlsruhe
+ * Released under the GPL
+ */
+
+#ifndef _IREDGEKINDS_H
+#define _IREDGEKINDS_H
+
+enum _ir_edge_kind_t {
+	EDGE_KIND_NORMAL,
+	EDGE_KIND_BLOCK,
+	EDGE_KIND_DEP,
+	EDGE_KIND_LAST
+};
+
+typedef enum _ir_edge_kind_t ir_edge_kind_t;
+
+/*
+ * It's ugly but we need this forward ref.
+ */
+
+void edges_notify_edge_kind(ir_node *src, int pos, ir_node *tgt, ir_node *old_tgt, ir_edge_kind_t kind, ir_graph *irg);
+
+#endif /* _IREDGEKINDS_H */
