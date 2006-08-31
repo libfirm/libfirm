4aa04cf34cafb2390ea2ad730ffbb027b6a3e759
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2006-08-31 12:35:10 +0000

    updated

--- ir/be/test/thread.c
+++ ir/be/test/thread.c
@@ -1,18 +1,18 @@
 #ifdef _WIN32
 #define __thread   __declspec( thread )
-__thread int tls_i = 1;
-
-int test(void) {
-	return tls_i;
-}
-#else
-int test() {
-    return 1;
-}
 #endif
+
+__thread int tls_i[10];
+__thread int tls_j = 5;
+
+int test(int i) {
+        tls_j = i;
+	return tls_i[i];
+}
 
 int main()
 {
-    printf("Result: %d\n", test());
+    printf("tls_i: %d\n", test(3));
+    printf("tls_j: %d\n", tls_j);
     return 0;
 }
