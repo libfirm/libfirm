67ee0da561f2445bf27813dc1cb566edd9eb9c4e
Sebastian Hack <hack@ipd.info.uni-karlsruhe.de>
2006-09-03 15:30:29 +0000

    added statistics events

---
+++ ir/be/bestatevent.c
@@ -0,0 +1,58 @@
+/**
+* Statistic events
+* @date 3.9.2006
+* @author Sebastian Hack
+*/
+
+#include <assert.h>
+#include <string.h>
+
+#include "bestatevent.h"
+
+#define STACK_SIZE 16
+
+typedef struct {
+	FILE *f;
+	char tag[512];
+} ev_env_t;
+
+static ev_env_t envs[STACK_SIZE];
+static int sp = 0;
+
+void be_stat_ev_push(const char **tags, int n_tags, FILE *f)
+{
+	int i;
+	ev_env_t *env;
+
+	assert(sp < STACK_SIZE && "stat event stack full");
+	env = &envs[sp++];
+
+	env->tag[0] = '\0';
+	for(i = 0; i < n_tags; ++i) {
+		strncat(env->tag, tags[i], sizeof(env->tag));
+		strncat(env->tag, ";", sizeof(env->tag));
+	}
+	env->tag[sizeof(env->tag) - 1] = '\0';
+	env->f = f;
+}
+
+void be_stat_ev_pop(void)
+{
+	if(sp > 0) {
+		envs[--sp].f = NULL;
+	}
+}
+
+void be_stat_ev(const char *ev, int value)
+{
+	if(sp > 0) {
+		ev_env_t *env = &envs[sp - 1];
+		if(env->f)
+			fprintf(env->f, "%s%s;%d\n", env->tag, ev, value);
+	}
+}
+
+int be_stat_ev_is_active(void)
+{
+	return sp > 0 && envs[sp - 1].f;
+}
---
+++ ir/be/bestatevent.h
@@ -0,0 +1,23 @@
+/**
+ * Statistic events
+ * @date 3.9.2006
+ * @author Sebastian Hack
+ */
+
+#ifndef _BESTATEVENT_H
+#define _BESTATEVENT_H
+
+#include <stdio.h>
+
+#define BE_STAT_EV_N_INSN                    "n_insn"
+#define BE_STAT_EV_PHI_BEFORE_SPILL          "phi_before_spill"
+#define BE_STAT_EV_PHI_AFTER_SPILL           "phi_after_spill"
+
+void be_stat_ev_push(const char **tags, int n_tags, FILE *f);
+void be_stat_ev_pop(void);
+
+void be_stat_ev(const char *ev, int value);
+
+int be_stat_ev_is_active(void);
+
+#endif /* _BESTATEVENT_H */
