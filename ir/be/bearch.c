b80270e390c2db5e65ee51a298f13ee58109f4d2
Adam Szalkowski <adam@ipd.info.uni-karlsruhe.de>
2006-09-04 11:48:04 +0000

    another typo

--- ir/be/bearch.c
+++ ir/be/bearch.c
@@ -321,7 +321,7 @@ extern char *arch_register_req_format(char *buf, size_t len, const arch_register
 }
 
 int
-arch_ent_is_constructor(const arch_env_t *arch_env, const entitiy * ent)
+arch_ent_is_constructor(const arch_env_t *arch_env, const entity * ent)
 {
 
 
