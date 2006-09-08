b74bde1857916a3a76cb93c298f780075a573341
Matthias Braun <matze@braunis.de>
2006-09-08 13:08:54 +0000

    fix test

--- ir/be/test/func_arg.c
+++ ir/be/test/func_arg.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
+
+void mymain(int argc, const char * const*argv) {
+	printf("A0: %s AC: %d\n", argv[0], argc);
+}
+
 int main(argc, argv)
   int argc;
   char **argv;
 {
-  printf("ARGV0: %s ARGC: %d", argv[0], argc);
-  return 0;
+	const char *args[] = { "blup", "bla" };
+
+	mymain(2, args);
+	return 0;
 }
