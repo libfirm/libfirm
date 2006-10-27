dc077c0346923c31747743dceab90730f13e1e37
Matthias Braun <matze@braunis.de>
2006-10-27 08:22:57 +0000

    improved docu of bespill.h

--- ir/be/bespill.h
+++ ir/be/bespill.h
@@ -21,8 +21,6 @@ typedef struct _spill_env_t spill_env_t;
 
 /**
  * Creates a new spill environment.
- *
- * @param chordal
  */
 spill_env_t *be_new_spill_env(const be_chordal_env_t *chordal);
 
@@ -31,31 +29,52 @@ spill_env_t *be_new_spill_env(const be_chordal_env_t *chordal);
  */
 void be_delete_spill_env(spill_env_t *senv);
 
-void be_add_reload(spill_env_t *senv, ir_node *to_spill, ir_node *before);
-
-void be_add_reload_on_edge(spill_env_t *senv, ir_node *to_spill, ir_node *bl, int pos);
-
-void be_insert_spills_reloads(spill_env_t *senv);
-
-/**
- * Marks a phi-node for spilling. So when reloading from this phi-node, not
- * only its value but the whole phi will be spilled.
- * This might place be_Copy nodes in predecessor blocks.
- */
-void be_spill_phi(spill_env_t *env, ir_node *node);
-
-/**
- * Returns the estimated costs if a node would get reloaded at a specific place
- * (This looks whether the value already has a spill or if rematerialisation
- *  is possible)
- */
-int be_get_reload_costs(spill_env_t *env, ir_node *to_spill, ir_node *before);
-
-int be_get_reload_costs_on_edge(spill_env_t *env, ir_node *to_spill, ir_node *block, int pos);
-
 /**
  * Sets the debug module of a spill environment.
  */
 DEBUG_ONLY(void be_set_spill_env_dbg_module(spill_env_t *env, firm_dbg_module_t *dbg));
 
-#endif /* BESPILL_H_ */
+/**
+ * Inserts a new entry into the list of reloads to place (the real nodes will
+ * be created when be_insert_spills_reloads is run). You don't have to
+ * explicitely create spill nodes, they will be created automatically as soon
+ * as a reload is created.
+ */
+void be_add_reload(spill_env_t *senv, ir_node *to_spill, ir_node *before);
+
+/**
+ * Analog to be_add_reload, but places the reload "on an edge" between 2 blocks
+ */
+void be_add_reload_on_edge(spill_env_t *senv, ir_node *to_spill, ir_node *bl, int pos);
+
+/**
+ * The main function that places real spills/reloads (or rematerializes values)
+ * for all values where be_add_reload was called. It then rebuilds the
+ * SSA-form and updates liveness information
+ */
+void be_insert_spills_reloads(spill_env_t *senv);
+
+/**
+ * There are 2 possibilities to spill a phi node: Only it's value, or replacing
+ * the whole phi-node with a memory phi. Normally only the value of a phi will
+ * be spilled unless you mark the phi with be_spill_phi.
+ * (Remember that each phi needs a register, so you have to spill phis when
+ *  there are more phis than registers in a block)
+ */
+void be_spill_phi(spill_env_t *env, ir_node *node);
+
+/**
+ * Returns the estimated costs if a node would get reloaded at a specific place
+ * This usually returns the cost of spill + reload operation. But might return
+ * smaller values if the value has already been spilled in a former run or
+ * when it is possible to rematerialize the value.
+ */
+int be_get_reload_costs(spill_env_t *env, ir_node *to_spill, ir_node *before);
+
+/**
+ * Analog to be_get_reload_costs but returns the cost if the reload would be
+ * placed "on an edge" between 2 blocks
+ */
+int be_get_reload_costs_on_edge(spill_env_t *env, ir_node *to_spill, ir_node *block, int pos);
+
+#endif
