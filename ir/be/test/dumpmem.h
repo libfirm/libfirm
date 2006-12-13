a3e1114647fcd76f50b66a535d9d264568b9ce4d
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2006-12-13 13:30:21 +0000

    more portable printf format

--- ir/be/test/dumpmem.h
+++ ir/be/test/dumpmem.h
@@ -3,10 +3,10 @@
 static void dumpMem(void* ptr, size_t size)
 {
     size_t i;
-    char* p = ptr;
+    unsigned char* p = ptr;
     printf("\n");
     for(i = 0; i < size; ++i) {
-        printf("%02hhx", p[i]);
+        printf("%02x", p[i]);
         if((i % 4) == 3) {
             printf("\n");
         }
