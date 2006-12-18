4032a5c080d6801aacbc352b650f8ca28edf0248
Matthias Braun <matze@braunis.de>
2006-12-18 14:25:54 +0000

    full blown version of convtest works again

--- ir/be/test/convtest.c
+++ ir/be/test/convtest.c
@@ -7,22 +7,14 @@ int bla(char a, char b, char c, char d, short e, short f, short g, int h, int i,
 int convtest_func(char c, short s, int i, float f, double d) {
 	char sc = c + s;
 	char ic = c + i;
-	//char fc = c + f;
-	//char dc = c + d;
-	char fc = c;
-	char dc = c;
-	//short is = s + i;
-	//short fs = s + f;
-	//short ds = s + d;
-	short is = s;
+	char fc = c + f;
+	char dc = c + d;
+	short is = s + i;
 	short fs = s + f;
-	short ds = s;
-	//int fi = i + f;
-	//int di = i + d;
+	short ds = s + d;
 	int fi = i + f;
-	int di = i;
-	//float df = d + f;
-	double df = d;
+	int di = i + d;
+	float df = d + f;
 
 	return bla(sc, ic, fc, dc, is, fs, ds, fi, di, df);
 }
