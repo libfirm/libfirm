bf4bcb9247ccff26ead0a4aa037f36ce6751f993
Matthias Braun <matze@braunis.de>
2007-02-16 09:29:20 +0000

    improve vararray test, make the array bigger so that the app crashs on wrong implementations :)

--- ir/be/test/vararray.c
+++ ir/be/test/vararray.c
@@ -11,12 +11,17 @@ int test(int data[], int len) {
   for(i = 0; i < len; ++i) {
     printf("%d\n", arr[i]);
   }
+
+  return 0;
 }
 
-int _data[] = {3, 2, 1};
+int _data[] = {0, 0, 0, 1, 11, 111, 2, 22, 222, 3, 33, 333,
+               4, 44, 444, 5, 55, 555, 6, 66, 666, 7, 77, 777,
+			   8, 88, 888, 9, 99, 999};
+static const size_t len = sizeof(_data)/sizeof(_data[0]);
 
 int main()
 {
-  test(_data, 3);
+  test(_data, len);
   return 0;
 }
