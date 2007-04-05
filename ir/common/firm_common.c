e6530a701d2d840a095062177d80065bcc42b8b3
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2007-04-05 12:43:13 +0000

    k_ir_region added
    [r12658]

--- ir/common/firm_common.c
+++ ir/common/firm_common.c
@@ -6,7 +6,7 @@
  * Modified by: Goetz Lindenmaier, Michael Beck
  * Created:
  * CVS-ID:      $Id$
- * Copyright:   (c) 1998-2006 Universität Karlsruhe
+ * Copyright:   (c) 1998-2007 Universität Karlsruhe
  * Licence:     This file protected by GPL -  GNU GENERAL PUBLIC LICENSE.
  */
 #ifdef HAVE_CONFIG_H
@@ -26,77 +26,81 @@
 
 /* returns the kind of the thing */
 firm_kind get_kind(const void *firm_thing) {
-  return POINTER_READ(firm_thing, sizeof(firm_kind)) ? *(firm_kind *)firm_thing : k_BAD;
+	return POINTER_READ(firm_thing, sizeof(firm_kind)) ? *(firm_kind *)firm_thing : k_BAD;
 }  /* get_kind */
 
 const char *print_firm_kind(void *firm_thing) {
-  if (! firm_thing)
-    return "(NULL)";
+	if (! firm_thing)
+		return "(NULL)";
 
-  switch (*(firm_kind *)firm_thing) {
-  case k_entity                 : return "k_entity";
-  case k_type                   : return "k_type";
-  case k_ir_graph               : return "k_ir_graph";
-  case k_ir_node                : return "k_ir_node";
-  case k_ir_mode                : return "k_ir_mode";
-  case k_ir_op                  : return "k_ir_op";
-  case k_tarval                 : return "k_tarval";
-  case k_ir_loop                : return "k_ir_loop";
-  case k_ir_compound_graph_path : return "k_ir_compound_graph_path";
-  case k_ir_extblk              : return "k_ir_extblk";
-  case k_ir_prog                : return "k_ir_prog";
-  default: return "";
-  }
+	switch (*(firm_kind *)firm_thing) {
+	case k_entity                 : return "k_entity";
+	case k_type                   : return "k_type";
+	case k_ir_graph               : return "k_ir_graph";
+	case k_ir_node                : return "k_ir_node";
+	case k_ir_mode                : return "k_ir_mode";
+	case k_ir_op                  : return "k_ir_op";
+	case k_tarval                 : return "k_tarval";
+	case k_ir_loop                : return "k_ir_loop";
+	case k_ir_compound_graph_path : return "k_ir_compound_graph_path";
+	case k_ir_extblk              : return "k_ir_extblk";
+	case k_ir_prog                : return "k_ir_prog";
+	case k_ir_region              : return "k_ir_region";
+
+	default: return "";
+	}
 }  /* print_firm_kind */
 
 /*
  * identify a firm thing
  */
-void firm_identify_thing(void *X)
-{
-  if (! X) {
-    printf("(NULL)\n");
-    return;
-  }
+void firm_identify_thing(void *X) {
+	if (! X) {
+		printf("(NULL)\n");
+		return;
+	}
 
-  switch (get_kind(X)) {
-  case k_BAD:
-    printf("BAD: (%p)\n", X);
-    break;
-  case k_entity:
-    printf("entity: %s: %ld (%p)\n", get_entity_name(X), get_entity_nr(X), X);
-    break;
-  case k_type:
-    printf("type: %s %s: %ld (%p)\n", get_type_tpop_name(X), get_type_name(X), get_type_nr(X), X);
-    break;
-  case k_ir_graph:
-    printf("graph: %s: %ld (%p)\n", get_entity_name(get_irg_entity(X)), get_irg_graph_nr(X), X);
-    break;
-  case k_ir_node:
-    printf("irnode: %s%s %ld (%p)\n", get_irn_opname(X), get_mode_name(get_irn_mode(X)), get_irn_node_nr(X), X);
-    break;
-  case k_ir_mode:
-    printf("mode %s: (%p)\n", get_mode_name(X), X);
-    break;
-  case k_ir_op:
-    printf("op %s: (%p)\n", get_op_name(X), X);
-    break;
-  case k_tarval:
-    printf("tarval : "); tarval_printf(X); printf(" (%p)\n", X);
-    break;
-  case k_ir_loop:
-    printf("loop: with depth %d: (%p)\n", get_loop_depth(X), X);
-    break;
-  case k_ir_compound_graph_path:
-    printf("compound_graph_path: (%p)\n", X);
-    break;
-  case k_ir_extblk:
-    printf("extended block: (%p)\n", X);
-    break;
-  case k_ir_prog:
-    printf("irp: (%p)\n", X);
-    break;
-  default:
-    printf("Cannot identify thing at (%p).\n", X);
-  }
+	switch (get_kind(X)) {
+	case k_BAD:
+		printf("BAD: (%p)\n", X);
+		break;
+	case k_entity:
+		printf("entity: %s: %ld (%p)\n", get_entity_name(X), get_entity_nr(X), X);
+		break;
+	case k_type:
+		printf("type: %s %s: %ld (%p)\n", get_type_tpop_name(X), get_type_name(X), get_type_nr(X), X);
+		break;
+	case k_ir_graph:
+		printf("graph: %s: %ld (%p)\n", get_entity_name(get_irg_entity(X)), get_irg_graph_nr(X), X);
+		break;
+	case k_ir_node:
+		printf("irnode: %s%s %ld (%p)\n", get_irn_opname(X), get_mode_name(get_irn_mode(X)), get_irn_node_nr(X), X);
+		break;
+	case k_ir_mode:
+		printf("mode %s: (%p)\n", get_mode_name(X), X);
+		break;
+	case k_ir_op:
+		printf("op %s: (%p)\n", get_op_name(X), X);
+		break;
+	case k_tarval:
+		printf("tarval : "); tarval_printf(X); printf(" (%p)\n", X);
+		break;
+	case k_ir_loop:
+		printf("loop: with depth %d: (%p)\n", get_loop_depth(X), X);
+		break;
+	case k_ir_compound_graph_path:
+		printf("compound_graph_path: (%p)\n", X);
+		break;
+	case k_ir_extblk:
+		printf("extended block: (%p)\n", X);
+		break;
+	case k_ir_prog:
+		printf("irp: (%p)\n", X);
+		break;
+	case k_ir_region:
+		printf("region: (%p)\n", X);
+		break;
+	default:
+		printf("Cannot identify thing at (%p).\n", X);
+	}
 }  /* firm_identify_thing */
--- ir/common/firm_common.h
+++ ir/common/firm_common.h
@@ -6,7 +6,7 @@
  * Modified by: Goetz Lindenmaier
  * Created:
  * CVS-ID:      $Id$
- * Copyright:   (c) 1998-2003 Universität Karlsruhe
+ * Copyright:   (c) 1998-2007 Universität Karlsruhe
  * Licence:     This file protected by GPL -  GNU GENERAL PUBLIC LICENSE.
  */
 
@@ -17,26 +17,26 @@
  *
  * @author Martin Trapp, Christian Schaefer & Goetz Lindenmaier
  */
-
-# ifndef _FIRM_COMMON_H_
-# define _FIRM_COMMON_H_
+#ifndef _FIRM_COMMON_H_
+#define _FIRM_COMMON_H_
 
 /** a list of firm kinds
  @@@ not all datatypes are tagged yet. */
 typedef enum {
-  k_BAD = 0,    /**< an invalid firm node */
-  k_entity,     /**< an entity */
-  k_type,       /**< a type */
-  k_ir_graph,   /**< an ir graph */
-  k_ir_node,    /**< an ir node */
-  k_ir_mode,    /**< an ir mode */
-  k_ir_op,      /**< an ir opcode */
-  k_tarval,     /**< a tarval */
-  k_ir_loop,    /**< a loop */
-  k_ir_compound_graph_path, /**< a compound graph path, see entity.h */
-  k_ir_extblk,  /**< an extended block */
-  k_ir_prog,    /**< a program representation (irp) */
-  k_ir_max      /**< maximum value -- illegal for firm nodes. */
+	k_BAD = 0,                /**< An invalid firm node. */
+	k_entity,                 /**< An entity. */
+	k_type,                   /**< A type. */
+	k_ir_graph,               /**< An IR graph. */
+	k_ir_node,                /**< An IR node. */
+	k_ir_mode,                /**< An IR mode. */
+	k_ir_op,                  /**< An IR opcode. */
+	k_tarval,                 /**< A tarval. */
+	k_ir_loop,                /**< A loop. */
+	k_ir_compound_graph_path, /**< A compound graph path, see entity.h. */
+	k_ir_extblk,              /**< An extended basic block. */
+	k_ir_prog,                /**< A program representation (irp). */
+	k_ir_region,              /**< A region. */
+	k_ir_max                  /**< maximum value -- illegal for firm nodes. */
 } firm_kind;
 
 /**
@@ -47,9 +47,9 @@ typedef enum {
 firm_kind get_kind(const void *firm_thing);
 
 /** Returns the kind of a thing as a string. */
-const char* print_firm_kind(void *firm_thing);
+const char *print_firm_kind(void *firm_thing);
 
 /** Print an identification of a firm thing. */
 void firm_identify_thing(void *X);
 
-# endif /*_FIRM_COMMON_H_ */
+#endif /*_FIRM_COMMON_H_ */
