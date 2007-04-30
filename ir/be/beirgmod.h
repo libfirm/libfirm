fa8d14874bdb094c6efc51e29efe9a89c4f1d721
Christian Würdig <chriswue@ipd.info.uni-karlsruhe.de>
2007-04-30 12:03:16 +0000

    removed unused code
    [r13562]

--- ir/be/beirgmod.h
+++ ir/be/beirgmod.h
@@ -35,44 +35,6 @@
 #include "irnode.h"
 #include "beirg.h"
 
-#if 0
-/**
- * Introduce several copies for one node.
- *
- * A copy in this context means, that you want to introduce several new
- * abstract values (in Firm: nodes) for which you know, that they
- * represent the same concrete value. This is the case if you
- * - copy
- * - spill and reload
- * - re-materialize
- * a value.
- *
- * This function reroutes all uses of the original value to the copies in the
- * corresponding dominance subtrees and creates Phi functions where necessary.
- *
- * @note The visited flag and link fields are used.
- *
- * @param info		  Dominance frontier information.
- * @param lv          Liveness information to be updated. If NULL, no liveness
- *                    updating is performed.
- * @param value       The value that has been duplicated.
- * @param copies_len  the length of the copies array
- * @param copie       an array holding all copies of the value
- * @param phis        An ARR_F where all newly created phis will be inserted,
- *                    may be NULL
- * @param ignore_uses A set of nodes probably using one of the nodes in
- *                    @p nodes. Their usage will not adjusted. They remain
- *                    untouched by this function. May be NULL.
- */
-ir_node **be_ssa_construction(const be_dom_front_info_t *info, be_lv_t *lv,
-                            ir_node *value, int copies_len, ir_node **copies,
-                            const ir_nodeset_t *ignore_uses, int need_new_phis);
-
-/** @deprecated */
-void be_ssa_constr_set_ignore(const be_dom_front_info_t *info, be_lv_t *lv,
-                              pset *nodes, pset *ignores);
-#endif
-
 /**
  * Insert a Perm which permutes all (non-ignore) live values of a given register class
  * after a certain instruction.
