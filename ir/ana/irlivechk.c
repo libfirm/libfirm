6c694085a1d9ea8d0be49170e2f1ce64682f4d54
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2007-05-10 21:40:37 +0000

    Added new header
    [r13778]

--- ir/ana/irlivechk.c
+++ ir/ana/irlivechk.c
@@ -1,7 +1,28 @@
+/*
+ * Copyright (C) 1995-2007 University of Karlsruhe.  All right reserved.
+ *
+ * This file is part of libFirm.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.
+ *
+ * Licensees holding valid libFirm Professional Edition licenses may use
+ * this file in accordance with the libFirm Commercial License.
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ */
+
 /**
- * @file   livechk.c
- * @date   21.04.2007
- * @author Sebastian Hack
+ * @file    livechk.c
+ * @date    21.04.2007
+ * @author  Sebastian Hack
+ * @version $Id: $
+ * @summary
  *
  * Liveness checks as developed by Benoit Boissinot, Fabrice Rastello and myself.
  *
--- ir/ana/irlivechk.h
+++ ir/ana/irlivechk.h
@@ -1,7 +1,28 @@
+/*
+ * Copyright (C) 1995-2007 University of Karlsruhe.  All right reserved.
+ *
+ * This file is part of libFirm.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.
+ *
+ * Licensees holding valid libFirm Professional Edition licenses may use
+ * this file in accordance with the libFirm Commercial License.
+ * Agreement provided with the Software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ */
+
 /**
- * @file   irlivechk.h
- * @date   22.04.2007
- * @author Sebastian Hack
+ * @file    irlivechk.h
+ * @author  Sebastian Hack
+ * @date    22.04.2007
+ * @version $Id: $
+ * @summary
  *
  * Live in/end checks whose only precomputation concerns the structure of the CFG.
  * Hence, nothing has to be updated if the program is modified unless the CFG is touched.
@@ -10,9 +31,8 @@
  * Copyright (C) 2007 Universitaet Karlsruhe
  * Released under the GPL
  */
-
-#ifndef _IRLIVECHK_H
-#define _IRLIVECHK_H
+#ifndef FIRM_ANA_IRLIVECHK_H
+#define FIRM_ANA_IRLIVECHK_H
 
 #include "irgraph.h"
 #include "irnode.h"
@@ -59,4 +79,4 @@ extern int lv_chk_bl_out(const lv_chk_t *lv, const ir_node *bl, const ir_node *i
  */
 extern int lv_chk_bl_in(const lv_chk_t *lv, const ir_node *bl, const ir_node *irn);
 
-#endif /* _IRLIVECHK_H */
+#endif /* FIRM_ANA_IRLIVECHK_H */
