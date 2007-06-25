80237bb44317735dd457c36558b1c2b540dbdea2
Christoph Mallon <christoph.mallon@gmx.de>
2007-06-25 17:11:38 +0000

    Testcase for array initialisation.
    [r14746]

---
+++ ir/be/test/fehler22.c
@@ -0,0 +1,28 @@
+#include <wchar.h>
+
+const wchar_t AmmoCaliber[][20] =
+{
+	L"0",
+	L".38 Kal",
+	L"9mm",
+	L".45 Kal",
+	L".357 Kal",
+	L"12 Kal",
+	L"CAWS",
+	L"5.45mm",
+	L"5.56mm",
+	L"7.62mm NATO",
+	L"7.62mm WP",
+	L"4.7mm",
+	L"5.7mm",
+	L"Monster",
+	L"Rakete",
+	L"",
+	L"",
+};
+
+
+int main(void)
+{
+	return 0;
+}
