a51a8ea3c493ad17d0774cc610097818cff36386
Matthias Braun <matze@braunis.de>
2007-06-29 13:25:39 +0000

    fehler 31
    [r14845]

---
+++ ir/be/test/fehler31.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+static unsigned hash_ptr(const void *ptr)
+{
+	unsigned ptr_int = (ptr - NULL);
+	return ptr_int >> 3;
+}
+
+void *p = (void*) 0xdeadbeef;
+
+int main()
+{
+	printf("0x%x (shoulde be 0x1bd5b7dd)\n", hash_ptr(p));
+
+	return 0;
+}
