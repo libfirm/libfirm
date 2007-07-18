6a10e8978bcee6ea364d057e55205b9fa0c8b331
Matthias Braun <matze@braunis.de>
2007-07-18 16:52:17 +0000

    not good enough yet
    [r15229]

--- ir/be/test/fehler44.c
+++ ir/be/test/fehler44.c
@@ -1,13 +1,19 @@
-unsigned int current_random = 1892341778;
-int imax = 403;
 #define IM 2147483648u
+
+unsigned int current_random1 = 1892341778;
+int imax1 = 403;
+unsigned int current_random2 = 4247132568;
+int imax2 = 403;
 
 int main()
 {
-	int ival = current_random & (IM - 1);
-	ival = (int) ((float) ival * (float) (imax + 0.999) / (float) IM);
+	int ival1 = current_random1 & (IM - 1);
+	ival1 = (int) ((float) ival1 * (float) (imax1 + 0.999) / (float) IM);
+	int ival2 = current_random2 & (IM - 1);
+	ival2 = (int) ((float) ival2 * (float) (imax2 + 0.999) / (float) IM);
 
-	printf("Res: %d\n", ival);
+	printf("Res1: %d\n", ival1);
+	printf("Res2: %d\n", ival2);
 
 	return 0;
 }
