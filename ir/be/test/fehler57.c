bfdabdaec7466dcdee5461313e48b117b3fce57a
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2007-08-03 14:05:43 +0000

    enhanced fehler57 test
    [r15454]

--- ir/be/test/fehler57.c
+++ ir/be/test/fehler57.c
@@ -4,7 +4,21 @@ int floor_log2_wide (unsigned int x)
 	return x;
 }
 
+unsigned test_div(int x) {
+  x /= -1;
+  return x;
+}
+
+int X;
+
 int main()
 {
 	printf("Res: %d\n", floor_log2_wide(4294967251));
+	printf("Res: %d\n", test_div(-5));
+        printf("Res: %d\n", -5%-1);
+        printf("Res: %d\n", +5%-1);
+        printf("Res: %d\n", -5%+1);
+        printf("Res: %d\n", +5%+1);
+        printf("Res: %d\n", X % -1);
+        return 0;
 }
