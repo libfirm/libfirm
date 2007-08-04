902417cfad363f2609f13039f78d85908e7d70ed
Matthias Braun <matze@braunis.de>
2007-08-04 09:50:09 +0000

    make fehler59 consistent
    [r15467]

--- ir/be/test/fehler59.c
+++ ir/be/test/fehler59.c
@@ -1,13 +1,18 @@
 #include <alloca.h>
+#include <assert.h>
 
 int main()
 {
 	char *b1 = alloca(13);
 	int i;
+	int lastoffs = 0;
 
 	for(i = 0; i < 5; ++i) {
-		char *bfs = alloca(24);
-		printf("Offset: %d\n", bfs - b1);
+		char *bfs    = alloca(24);
+		int   offset = b1 - bfs;
+		memset(bfs, 0, 24);
+		assert(offset > lastoffs);
+		lastoffs = offset;
 	}
 
 	return 0;
