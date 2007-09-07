d38b9d32578517557f1ecf3bceddbae9e3789abe
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2007-09-07 10:18:35 +0000

    another more readable version
    [r15704]

---
+++ ir/be/test/apfel_readable.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+float o=0.075,h=1.5,T,r,O,l,I;
+int _,L=80,s=3200;
+
+int main(int argc, char *argv[]){
+	init_checkpoints(&argc, argv);
+	for (; s;) {
+		if (s%L == 0) {
+			h -= o;
+			T = -2;
+		}
+
+		O = I*2*O+h;
+		I = l+T-r;
+		if (4 -(r=O*O)<(l=I*I) | ++_==L) {
+			int index;
+			char c;
+
+			index = --s%L ? (_ < L ? --_ %6:6) : 7;
+			c = "Sascha \n"[index];
+			putchar(c);
+			O = I = l = _ = r = 0;
+			T += o/2;
+		}
+	}
+}
