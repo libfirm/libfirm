34487e746e4293ddb3111289d21aa811a4c7a865
Matthias Braun <matze@braunis.de>
2007-09-12 17:02:29 +0000

    fixed testcase
    [r15758]

--- ir/be/test/add64.c
+++ ir/be/test/add64.c
@@ -1,10 +1,15 @@
+long long k;
+long long k2;
 
-long long f(long long a, long long b)
+void f(long long a, long long b)
 {
-	long long c = (a & 0xffffffff) | 0x100000000;
-	return a + b + c;
+	long long c = (a & 0xffffffffLL) | 0x100000000LL;
+	k  = c + b;
+	k2 = a + b;
 }
 
 int main(int argc, char **argv) {
-	printf("Res: %llx\n", f(0x100000000, 0x100000000));
+	f(0x100000000LL, 0x100000000LL);
+	printf("Res: %llx %llx\n", k, k2);
+	return 0;
 }
