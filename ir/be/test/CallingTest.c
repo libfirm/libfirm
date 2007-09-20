31ab70c9b25b7203422c12d18439cfdee95a6d8d
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2007-09-20 08:51:23 +0000

    Add test for double call
    [r15890]

--- ir/be/test/CallingTest.c
+++ ir/be/test/CallingTest.c
@@ -10,12 +10,18 @@ float float_func(void)
   return 13.5f;
 }
 
+double double_func(void)
+{
+  return 13.5;
+}
+
 int main(int argc, char *argv[])
 {
   printf("calltest.c\n");
 
-  printf("  Calling int   function: %d\n", int_func());
-  printf("  Calling float function: %f\n", float_func());
+  printf("  Calling int    function: %d\n", int_func());
+  printf("  Calling float  function: %f\n", float_func());
+  printf("  Calling double function: %f\n", double_func());
 
   return 0;
 }
