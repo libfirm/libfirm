92485ca1d9c0032a90b8e104203be1a45cc6f892
Matthias Braun <matze@braunis.de>
2007-10-12 23:18:47 +0000

    fehler94
    [r16184]

---
+++ ir/be/test/fehler94.c
@@ -0,0 +1,23 @@
+
+struct decompostition {
+	int a, b;
+};
+
+static struct decompostition do_something(void)
+{
+	struct decompostition c;
+	rand();
+	return c;
+}
+
+int main(void) {
+	int arr[5];
+	struct decompostition dc;
+
+	arr[0] = 123;
+	arr[1] = 245;
+	dc = do_something();
+	printf("%d %d\n", arr[0], arr[1]);
+
+	return 0;
+}
