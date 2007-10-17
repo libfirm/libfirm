199c6562889adf23bbc346e2c5599f6b3e1778a2
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2007-10-17 15:21:20 +0000

    extended version
    [r16252]

--- ir/be/test/fehler96.c
+++ ir/be/test/fehler96.c
@@ -2,7 +2,15 @@ int shrs1(unsigned x) {
 	return -(x >> 31);
 }
 
+int shr1(int x) {
+	return -(x >> 31);
+}
+
 int main(void)
 {
 	printf("%d (should be -1)\n", shrs1(-3));
+	printf("%d (should be 1)\n", shr1(-3));
+	printf("%d (should be 0)\n", shrs1(3));
+	printf("%d (should be 0)\n", shr1(3));
+	return 0;
 }
