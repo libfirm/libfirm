ab604dc5edd76eb5834f309ffed13f54ab5c7c9d
Christoph Mallon <christoph.mallon@gmx.de>
2007-10-24 18:33:44 +0000

    Emit the "t" suffix for f96 numbers in the ia32 backend.
    [r16334]

--- ir/be/ia32/ia32_emitter.c
+++ ir/be/ia32/ia32_emitter.c
@@ -257,7 +257,8 @@ static void ia32_emit_mode_suffix_mode(const ir_mode *mode)
 		switch(get_mode_size_bits(mode)) {
 		case 32: be_emit_char('s'); return;
 		case 64: be_emit_char('l'); return;
-		case 80: be_emit_char('t'); return;
+		case 80:
+		case 96: be_emit_char('t'); return;
 		}
 	} else {
 		assert(mode_is_int(mode) || mode_is_reference(mode));
