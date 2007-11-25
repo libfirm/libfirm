349d1f7026c4489d49e5c84b56214d7fc502d270
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2007-11-25 17:24:38 +0000

    Fixed warning regarding const
    [r16839]

--- ir/be/beschedrss.c
+++ ir/be/beschedrss.c
@@ -300,7 +300,7 @@ static const ir_node **build_sorted_array_from_list(plist_t *irn_list, struct ob
 	plist_element_t *el;
 	int             i   = 0;
 	int             len = plist_count(irn_list);
-	const ir_node **arr = (const ir_node **) NEW_ARR_D(ir_node *, obst, len);
+	ir_node         **arr = (ir_node **) NEW_ARR_D(ir_node *, obst, len);
 
 	/* copy the list into the array */
 	foreach_plist(irn_list, el) {
