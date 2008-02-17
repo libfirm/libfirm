49eabe3e88095e0067e352961231ec4552622b12
Matthias Braun <matze@braunis.de>
2008-02-17 11:45:48 +0000

    remove tf_lowered_type flag when cloning method types (not sure if that is the correct fix)
    [r17757]

--- ir/tr/type.c
+++ ir/tr/type.c
@@ -1227,7 +1227,7 @@ ir_type *clone_type_method(ir_type *tp, ident *prefix) {
 
 	res = new_type(type_method, mode, name, db);
 
-	res->flags                         = tp->flags;
+	res->flags                         = tp->flags & ~tf_lowered_type;
 	res->size                          = tp->size;
 	res->attr.ma.n_params              = n_params;
 	res->attr.ma.params                = xcalloc(n_params, sizeof(res->attr.ma.params[0]));
