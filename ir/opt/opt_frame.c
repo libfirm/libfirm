c04778051703517e339ed91c11cfc880b57c4426
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2008-03-10 10:43:55 +0000

    - as Sel's from the frame type are not lowered anymore, we can safely remove the assert(phase_high) - indentation fixed
    [r18035]

--- ir/opt/opt_frame.c
+++ ir/opt/opt_frame.c
@@ -41,64 +41,63 @@
  * never touched entities.
  */
 void opt_frame_irg(ir_graph *irg) {
-  ir_type   *frame_tp = get_irg_frame_type(irg);
-  ir_entity *ent, *list;
-  ir_node   *frame, *sel;
-  int       i, n = get_class_n_members(frame_tp);
+	ir_type   *frame_tp = get_irg_frame_type(irg);
+	ir_entity *ent, *list;
+	ir_node   *frame, *sel;
+	int       i, n = get_class_n_members(frame_tp);
 
-  assert(get_irg_phase_state(irg) == phase_high && "Graph must be in high phase");
-  if (n <= 0)
-    return;
+	if (n <= 0)
+		return;
 
-  /* clear all entity links */
-  for (i = n - 1; i >= 0; --i) {
-    ent = get_class_member(frame_tp, i);
-    set_entity_link(ent, NULL);
-  }
+	/* clear all entity links */
+	for (i = n - 1; i >= 0; --i) {
+		ent = get_class_member(frame_tp, i);
+		set_entity_link(ent, NULL);
+	}
 
-  /* look for uses */
-  frame = get_irg_frame(irg);
+	/* look for uses */
+	frame = get_irg_frame(irg);
 
-  if (edges_activated(irg)) { /* use inplace edges */
-    const ir_edge_t *edge;
+	if (edges_activated(irg)) { /* use inplace edges */
+		const ir_edge_t *edge;
 
-    /* mark all used entities */
-    foreach_out_edge(frame, edge) {
-      sel = get_edge_src_irn(edge);
-      ent = get_Sel_entity(sel);
-      set_entity_link(ent, ent);
-    }
-  }
-  else {
-    /* use traditionally out edges */
-    if (get_irg_outs_state(irg) != outs_consistent)
-      compute_irg_outs(irg);
+		/* mark all used entities */
+		foreach_out_edge(frame, edge) {
+			sel = get_edge_src_irn(edge);
+			ent = get_Sel_entity(sel);
+			set_entity_link(ent, ent);
+		}
+	}
+	else {
+		/* use traditionally out edges */
+		if (get_irg_outs_state(irg) != outs_consistent)
+			compute_irg_outs(irg);
 
-    /* mark all used entities */
-    for (i = get_irn_n_outs(frame) - 1; i >= 0; --i) {
-      sel = get_irn_out(frame, i);
-      ent = get_Sel_entity(sel);
-      set_entity_link(ent, ent);
-    }
-  }
+		/* mark all used entities */
+		for (i = get_irn_n_outs(frame) - 1; i >= 0; --i) {
+			sel = get_irn_out(frame, i);
+			ent = get_Sel_entity(sel);
+			set_entity_link(ent, ent);
+		}
+	}
 
-  /* link unused ones */
-  list = NULL;
-  for (i = n - 1; i >= 0; --i) {
-    ent = get_class_member(frame_tp, i);
-    if (get_entity_link(ent) == NULL) {
-      set_entity_link(ent, list);
-      list = ent;
-    }
-  }
+	/* link unused ones */
+	list = NULL;
+	for (i = n - 1; i >= 0; --i) {
+		ent = get_class_member(frame_tp, i);
+		if (get_entity_link(ent) == NULL) {
+			set_entity_link(ent, list);
+			list = ent;
+		}
+	}
 
-  if (list) {
-    /* delete list members */
-    for (ent = list; ent; ent = list) {
-      list = get_entity_link(ent);
-      remove_class_member(frame_tp, ent);
-    }
-    /* we changed the frame type, it's layout should be redefined */
-    set_type_state(frame_tp, layout_undefined);
-  }
+	if (list != NULL) {
+		/* delete list members */
+		for (ent = list; ent; ent = list) {
+			list = get_entity_link(ent);
+			remove_class_member(frame_tp, ent);
+		}
+		/* we changed the frame type, it's layout should be redefined */
+		set_type_state(frame_tp, layout_undefined);
+	}
 }
