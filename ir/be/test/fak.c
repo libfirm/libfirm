558c2a2d05d32622da09ce56021935e733fa287a
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2008-07-30 00:38:46 +0000

    the famous Factorial example
    [r20776]

---
+++ ir/be/test/fak.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int factorial(int x) {
+	if (x == 0)
+		return 1;
+
+	return x * factorial(x-1);
+}
+
+int main(int argc, char **argv) {
+	int val = atoi(argv[0]);
+	printf("%d\n", factorial(val));
+}
