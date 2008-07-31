6f745b98af2463d407641a06b3893f45ae9715b9
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2008-07-31 20:58:58 +0000

    combo test case
    [r20865]

---
+++ ir/be/test/fehler122.c
@@ -0,0 +1,16 @@
+int *zptr;
+
+#define swap(lv1, lv2) \
+   { int tmp = lv1; lv1 = lv2; lv2 = tmp; }
+
+void vswap ( int p1, int p2, int n )
+{
+   while (n > 0) {
+      swap(zptr[p1], zptr[p2]);
+      p1++; p2++; n--;
+   }
+}
+
+int main() {
+	return 0;
+}
