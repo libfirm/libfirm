d8ed0076cc0340ebd4bce1ba77ea7276ada603e2
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2008-08-01 10:03:27 +0000

    fixed const
    [r20887]

--- ir/be/test/nulladd.c
+++ ir/be/test/nulladd.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static const int *a = (int *)0x10;
-static const int *b = (int *)0x20;
+static int * const a = (int *)0x10;
+static int * const b = (int *)0x20;
 
 int main() {
 	printf("%p\n", b - a);
