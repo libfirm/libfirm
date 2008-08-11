a58edbdeea416d231c014776726ff22b878cc5c5
Matthias Braun <matze@braunis.de>
2008-08-11 17:09:03 +0000

    more asm trickery
    [r21100]

---
+++ ir/be/test/asmq3.c
@@ -0,0 +1,21 @@
+/*$ -fomit-frame-pointer $*/
+
+int main(void)
+{
+	int in = 42;
+	int out;
+	__asm__(
+			"xorl %%eax,%%eax\n"
+			"xorl %%ebx,%%ebx\n"
+			"xorl %%ecx,%%ecx\n"
+			"xorl %%edx,%%edx\n"
+			"xorl %%esi,%%esi\n"
+			"xorl %%edi,%%edi\n"
+			"movl %1,%0\n"
+			"incl %0\n"
+			: "=r" (out) : "r" (in)
+			: "eax", "ebx", "ecx", "edx", "esi", "edi", "cc"
+	);
+
+	return out != 43;
+}
