d44b3844af84684de9b0213b77c175f80f8b84ee
Christoph Mallon <christoph.mallon@gmx.de>
2008-08-23 13:19:27 +0000

    Fix output for long double initializers.
    [r21382]

--- ir/be/begnuas.c
+++ ir/be/begnuas.c
@@ -249,27 +249,33 @@ static void dump_arith_tarval(tarval *tv, int bytes)
 		return;
 
 	case 12:
-		be_emit_irprintf("0x%02x%02x%02x%02x%02x%02x%02x%02x"
-		                   "%02x%02x%02x%02x", get_tarval_sub_bits(tv, 11),
-				get_tarval_sub_bits(tv, 10), get_tarval_sub_bits(tv, 9),
-	 			get_tarval_sub_bits(tv, 8), get_tarval_sub_bits(tv, 7),
-				get_tarval_sub_bits(tv, 6), get_tarval_sub_bits(tv, 5),
-				get_tarval_sub_bits(tv, 4), get_tarval_sub_bits(tv, 3),
-				get_tarval_sub_bits(tv, 2), get_tarval_sub_bits(tv, 1),
-				get_tarval_sub_bits(tv, 0));
+		/* Beware: Mixed endian output!  One little endian number emitted as
+		 * three longs.  Each long initializer is written in big endian. */
+		be_emit_irprintf(
+			"\t.long\t0x%02x%02x%02x%02x\n"
+			"\t.long\t0x%02x%02x%02x%02x\n"
+			"\t.long\t0x%02x%02x%02x%02x",
+			get_tarval_sub_bits(tv,  3), get_tarval_sub_bits(tv,  2),
+			get_tarval_sub_bits(tv,  1), get_tarval_sub_bits(tv,  0),
+			get_tarval_sub_bits(tv,  7), get_tarval_sub_bits(tv,  6),
+			get_tarval_sub_bits(tv,  5), get_tarval_sub_bits(tv,  4),
+			get_tarval_sub_bits(tv, 11), get_tarval_sub_bits(tv, 10),
+			get_tarval_sub_bits(tv,  9), get_tarval_sub_bits(tv,  8)
+		);
 		return;
 
 	case 16:
-		be_emit_irprintf("0x%02x%02x%02x%02x%02x%02x%02x%02x"
-				               "%02x%02x%02x%02x%02x%02x%02x%02x",
+		be_emit_irprintf(
+			"\t.long\t0x%02x%02x%02x%02x0x%02x%02x%02x%02x0x%02x%02x%02x%02x0x%02x%02x%02x%02x",
 			get_tarval_sub_bits(tv, 15), get_tarval_sub_bits(tv, 16),
 			get_tarval_sub_bits(tv, 13), get_tarval_sub_bits(tv, 12),
 			get_tarval_sub_bits(tv, 11), get_tarval_sub_bits(tv, 10),
-			get_tarval_sub_bits(tv, 9), get_tarval_sub_bits(tv, 8),
-			get_tarval_sub_bits(tv, 7), get_tarval_sub_bits(tv, 6),
-			get_tarval_sub_bits(tv, 5), get_tarval_sub_bits(tv, 4),
-			get_tarval_sub_bits(tv, 3), get_tarval_sub_bits(tv, 2),
-			get_tarval_sub_bits(tv, 1), get_tarval_sub_bits(tv, 0));
+			get_tarval_sub_bits(tv,  9), get_tarval_sub_bits(tv,  8),
+			get_tarval_sub_bits(tv,  7), get_tarval_sub_bits(tv,  6),
+			get_tarval_sub_bits(tv,  5), get_tarval_sub_bits(tv,  4),
+			get_tarval_sub_bits(tv,  3), get_tarval_sub_bits(tv,  2),
+			get_tarval_sub_bits(tv,  1), get_tarval_sub_bits(tv,  0)
+		);
 		return;
 	}
 
