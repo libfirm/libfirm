6050fa8834ddb4fc3979c949efb3bd8a7d51bc40
Matthias Braun <matze@braunis.de>
2008-09-02 09:23:51 +0000

    eccp optimized the assert away!?!, anyway I wanna fix the real error first...
    [r21637]

--- ir/be/test/fehler142.c
+++ ir/be/test/fehler142.c
@@ -1,15 +1,10 @@
-#include <assert.h>
-
-void foo(float zNear, float zFar)
+float foo(float zNear, float zFar)
 {
-	float v;
 	float depth = zFar - zNear;
 
-	v = -2 * zFar * zNear / depth;
-	assert(v < 0);
+	return -2 * zFar * zNear / depth;
 }
 
 int main(void) {
-	foo(4, 2048);
-	return 0;
+	return foo(4, 2048) >= 0;
 }
