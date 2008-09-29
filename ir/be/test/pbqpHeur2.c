dc5203fc6fba1cf44168e68e379ac2233a6d5c9d
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2008-09-29 23:00:06 +0000

    Added more users to calculated value, but there are no real skip pattern for this nodes which is a little bit strange. Maybe there's a bug which prevents consuming direct users of "memop"-nodes.
    [r22351]

--- ir/be/test/pbqpHeur2.c
+++ ir/be/test/pbqpHeur2.c
@@ -1,8 +1,9 @@
-int *block;
+int *block1, *block2, *block3, *block4, *block5;
 volatile int arr[100];
 int ca,cb,cc;
 int b = 3008;
 
+// TODO what if we use int* instead of char*?
 int k3_3(char* base, int i1, int i2, int i3, int k1, int k2, int k3)
 {
 	char a1, a2, a3;
@@ -36,17 +37,20 @@ int h1,h2,h3;
 int k1,k2,k3;
 int k4,k5,k6;
 int k7,k8,k9;
+int k10,k11,k12;
 
 void full_am(int base, int index)
 {
-	ca = arr[index] + b;
+	int ca = arr[index] + b;
 
 	//b = k3_3(base + 4 * index, g1, g2, g3, 1, 2, 3);
 	//b = k3_3(block, h1, h2, h3, 42, 5, 6);
 
-	b = k3_3(block, ca, k2, k3, 7, 8, 9);
-	//b = k3_3(cb, k4, k5, k6, 10, 11, 12);
-	//b = k3_3(cc, k7, k8, k9, 13, 14, 15);
+	b = k3_3(block1, ca, k2, k3, 7, 8, 9);
+	b = k3_3(block2, ca, k5, k6, 10, 11, 12);
+	b = k3_3(block3, ca, k8, k9, 13, 14, 15);
+	b = k3_3(block4, ca, k11, k12, 16, 17, 18);
+	b = k3_3(block5, ca, k7, k10, 19, 20, 21);
 }
 
 int main(int argc, char **argv) {
