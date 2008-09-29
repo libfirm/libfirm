ef2407fb8293923b527141910e97b70bb482c86c
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2008-09-29 18:23:04 +0000

    Tried to create a PBQP graph with can't be solved heuristically (not finished yet)
    [r22347]

---
+++ ir/be/test/pbqpHeur2.c
@@ -0,0 +1,56 @@
+char *block;
+volatile char arr[100];
+char ca,cb,cc;
+int b;
+
+int k3_3(char* base, int i1, int i2, int i3, int k1, int k2, int k3)
+{
+	char a1, a2, a3;
+	char b1, b2, b3;
+	char c1, c2, c3;
+
+	a1 = base[i1 + k1];
+	a2 = base[i2 + k1];
+	a3 = base[i3 + k1];
+
+	b1 = base[i1 + k2];
+	b2 = base[i2 + k2];
+	b3 = base[i3 + k2];
+
+	c1 = base[i1 + k3];
+	c2 = base[i2 + k3];
+	c3 = base[i3 + k3];
+
+	if (a1 != a2)
+		return a3;
+	if (b1 != b2)
+		return b3;
+	if (c1 != c2)
+		return c3;
+
+	return 0;
+}
+
+int g1,g2,g3;
+int h1,h2,h3;
+int k1,k2,k3;
+int k4,k5,k6;
+int k7,k8,k9;
+
+void full_am(int base, int index)
+{
+	ca = arr[base + 4*index];
+	cb = arr[base + 4*index];
+	cc = arr[base + 4*index];
+
+	b = k3_3(base + 4 * index, g1, g2, g3, 1, 2, 3);
+	b = k3_3(block, h1, h2, h3, 42, 5, 6);
+
+	b = k3_3(ca, k1, k2, k3, 7, 8, 9);
+	b = k3_3(cb, k4, k5, k6, 10, 11, 12);
+	b = k3_3(cc, k7, k8, k9, 13, 14, 15);
+}
+
+int main(int argc, char **argv) {
+	return 0;
+}
