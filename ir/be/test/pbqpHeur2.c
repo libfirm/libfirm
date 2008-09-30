1151094828819e1c82c0d197270d4a06720048c7
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2008-09-30 11:27:05 +0000

    All heuristic applications at right place, but there's a conv between :-(
    [r22363]

--- ir/be/test/pbqpHeur2.c
+++ ir/be/test/pbqpHeur2.c
@@ -1,6 +1,6 @@
 unsigned *block;
 unsigned *block1, *block2, *block3, *block4, *block5;
-volatile unsigned arr[100];
+volatile char arr[100];
 unsigned ca,cb,cc;
 unsigned b = 3008;
 
@@ -62,6 +62,8 @@ unsigned k3_3_am(char* base, unsigned i1, unsigned i2, unsigned i3, unsigned k1,
 }
 
 unsigned g1,g2,g3;
+unsigned g4,g5,g6;
+unsigned g7,g8,g9;
 unsigned h1,h2,h3;
 unsigned h4,h5,h6;
 unsigned h7,h8,h9;
@@ -72,19 +74,21 @@ unsigned k10,k11,k12;
 
 void full_am(unsigned base, unsigned index)
 {
-	unsigned ca = arr[index] + b;
+	unsigned ca = arr[base + 4 * index] + b;
 
 	/* user for shift const */
 	b = k3_3_am(block, h1, h2, h3, 2, 3, 4);
 	b = k3_3_am(block, h4, h5, h6, 2, 5, 6);
 	b = k3_3_am(block, h7, h8, h9, 2, 7, 8);
 
-	//b = k3_3(base + 4 * index, g1, g2, g3, 1, 2, 3);
+	b = k3_3(block1, base + 4 * index, g2, g3, 31, 32, 33);
+	b = k3_3(block2, base + 4 * index, g5, g6, 34, 35, 36);
+	//b = k3_3(base + 4 * index, base + 4 * index, g8, g9, 37, 38, 39);
 
 	/* user for computed value */
 	//b = k3_3(block1, ca, k2, k3, 7, 8, 9);
 	//b = k3_3(block2, ca, k5, k6, 10, 11, 12);
-	//b = k3_3(block3, ca, k8, k9, 13, 14, 15);
+	b = k3_3(block3, ca, k8, k9, 13, 14, 15);
 	b = k3_3(block4, ca, k11, k12, 16, 17, 18);
 	b = k3_3(block5, ca, k7, k10, 19, 20, 21);
 }
