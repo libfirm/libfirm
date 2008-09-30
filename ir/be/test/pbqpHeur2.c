0aeb151c762fd3593809926684f6b64af2f597c2
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2008-09-30 09:24:48 +0000

    Provoke heuristical PBQP decision for computed value.
    [r22360]

--- ir/be/test/pbqpHeur2.c
+++ ir/be/test/pbqpHeur2.c
@@ -1,24 +1,24 @@
-int *block1, *block2, *block3, *block4, *block5;
-volatile int arr[100];
-int ca,cb,cc;
-int b = 3008;
+unsigned *block1, *block2, *block3, *block4, *block5;
+volatile unsigned arr[100];
+unsigned ca,cb,cc;
+unsigned b = 3008;
 
-// TODO what if we use int* instead of char*?
-int k3_3(char* base, int i1, int i2, int i3, int k1, int k2, int k3)
+// TODO what if we use unsigned* instead of char*?
+unsigned k3_3(char* base, unsigned i1, unsigned i2, unsigned i3, unsigned k1, unsigned k2, unsigned k3)
 {
 	char a1, a2, a3;
 	char b1, b2, b3;
 	char c1, c2, c3;
 
-	a1 = base[i1 + k1];
+	a1 = i1 + k1;
 	a2 = base[i2 + k1];
 	a3 = base[i3 + k1];
 
-	b1 = base[i1 + k2];
+	b1 = i1 + k2;
 	b2 = base[i2 + k2];
 	b3 = base[i3 + k2];
 
-	c1 = base[i1 + k3];
+	c1 = i1 + k3;
 	c2 = base[i2 + k3];
 	c3 = base[i3 + k3];
 
@@ -32,16 +32,16 @@ int k3_3(char* base, int i1, int i2, int i3, int k1, int k2, int k3)
 	return 0;
 }
 
-int g1,g2,g3;
-int h1,h2,h3;
-int k1,k2,k3;
-int k4,k5,k6;
-int k7,k8,k9;
-int k10,k11,k12;
+unsigned g1,g2,g3;
+unsigned h1,h2,h3;
+unsigned k1,k2,k3;
+unsigned k4,k5,k6;
+unsigned k7,k8,k9;
+unsigned k10,k11,k12;
 
-void full_am(int base, int index)
+void full_am(unsigned base, unsigned index)
 {
-	int ca = arr[index] + b;
+	unsigned ca = arr[index] + b;
 
 	//b = k3_3(base + 4 * index, g1, g2, g3, 1, 2, 3);
 	//b = k3_3(block, h1, h2, h3, 42, 5, 6);
