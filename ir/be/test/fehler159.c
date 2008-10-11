45ecc187cee7107c83c1f9618a1e1e586df73644
Christoph Mallon <christoph.mallon@gmx.de>
2008-10-11 06:26:42 +0000

    Make fehler159 compilable.
    [r22663]

--- ir/be/test/fehler159.c
+++ ir/be/test/fehler159.c
@@ -1,14 +1,12 @@
 /*$  -fgvn-pre -fno-gcse $*/
 
-int something(int n);
-
 int test() {
 	int i;
 	char inUse16[16];
 
 	i = 0;
    	do {
-    	if (something(1) == 1)
+    	if (rand() == 1)
         	inUse16[i] = 1;
         else
         	inUse16[i] = 0;
@@ -16,4 +14,9 @@ int test() {
     } while (i < 16);
 
     return inUse16[0];
+}
+
+int main(void)
+{
+	return 0;
 }
