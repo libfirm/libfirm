a1c500a075e6e17b37b16f1974ab22110c525bcd
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2008-10-15 19:11:56 +0000

    Simplify test case.
    [r22927]

--- ir/be/test/pbqpHeur3.c
+++ ir/be/test/pbqpHeur3.c
@@ -1,9 +1,7 @@
 unsigned   a;
 unsigned   b;
 unsigned  *gi1, gi2, gi3, gi4, gi5;
-unsigned  *gi11, gi12, gi13, gi14, gi15;
 unsigned  *gi101, gi102, gi103, gi104, gi105;
-unsigned  *gi111, gi112, gi113, gi114, gi115;
 unsigned  *gi201, gi202, gi203, gi204, gi205;
 unsigned  *gi211, gi212, gi213, gi214, gi215;
 unsigned  *gi301, gi302, gi303, gi304, gi305;
@@ -86,27 +84,19 @@ void diamond(void)
 
 	b = sum;
 
-	/* Add 6 users for asb. */
+	/* Add 3 users for asb. */
 	use = add_3_add_const_shift_users(asb, gi1, gi2,
 			&gi3, &gi4, &gi5,
 			&gp[0], &gp[1], &gp[2], &gp[3], &gp[4], &gp[5], &gp[6], &gp[7], &gp[8],
 			7, 8);
-	use = add_3_add_const_shift_users(asb, gi11, gi12,
-			&gi13, &gi14, &gi15,
-			&gp[10], &gp[11], &gp[12], &gp[13], &gp[14], &gp[15], &gp[16], &gp[17], &gp[18],
-			17, 18);
 
-	/* Add 6 users for asc. */
+	/* Add 3 users for asc. */
 	use = add_3_add_const_shift_users(asc, gi101, gi102,
 			&gi103, &gi104, &gi105,
 			&gp[100], &gp[101], &gp[102], &gp[103], &gp[104], &gp[105], &gp[106], &gp[107], &gp[108],
 			107, 108);
-	use = add_3_add_const_shift_users(asc, gi111, gi112,
-			&gi113, &gi114, &gi115,
-			&gp[110], &gp[111], &gp[112], &gp[113], &gp[114], &gp[115], &gp[116], &gp[117], &gp[118],
-			117, 118);
 
-	/* Add 7 users for as. */
+	/* Add 4 users for as. */
 	use = add_1_shift_users(as, gi201, gi202,
 			&gi203, &gi204, &gi205,
 			&gp[200], &gp[201], &gp[202], &gp[203], &gp[204], &gp[205], &gp[206], &gp[207], &gp[208],
@@ -123,16 +113,4 @@ void diamond(void)
 			&gi503, &gi504, &gi505,
 			&gp[500], &gp[501], &gp[502], &gp[503], &gp[504], &gp[505], &gp[506], &gp[507], &gp[508],
 			500, 501, 502);
-	use = add_1_shift_users(as, gi601, gi602,
-			&gi603, &gi604, &gi605,
-			&gp[600], &gp[601], &gp[602], &gp[603], &gp[604], &gp[605], &gp[606], &gp[607], &gp[608],
-			600, 601, 602);
-	use = add_1_shift_users(as, gi701, gi702,
-			&gi703, &gi704, &gi705,
-			&gp[700], &gp[701], &gp[702], &gp[703], &gp[704], &gp[705], &gp[706], &gp[707], &gp[708],
-			700, 701, 702);
-	use = add_1_shift_users(as, gi801, gi802,
-			&gi803, &gi804, &gi805,
-			&gp[800], &gp[801], &gp[802], &gp[803], &gp[804], &gp[805], &gp[806], &gp[807], &gp[808],
-			800, 801, 802);
 }
