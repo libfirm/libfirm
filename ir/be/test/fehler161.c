8b508a29d6dcd39eccb39c49cbd39e966ee462e6
Matthias Braun <matze@braunis.de>
2008-10-25 19:30:12 +0000

    fix fehler161 to link correctly
    [r23185]

--- ir/be/test/fehler161.c
+++ ir/be/test/fehler161.c
@@ -1,12 +1,12 @@
 /*$ -O4 -march=core2 $*/
 #include <alloca.h>
 
-extern void print_spelling(char *text);
+extern void puts(const char *text);
 
 void test (void)
 {
 	char *temp = (char *) alloca (16);
-	print_spelling (temp);
+	puts(temp);
 }
 
 int main(int argc, char *argv[]) {
