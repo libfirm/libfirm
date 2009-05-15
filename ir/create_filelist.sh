e9b83cc6b672b55360da173904de004ecc90977e
Matthias Braun <matze@braunis.de>
2009-05-15 22:08:43 +0000

    update filelist script
    [r25981]

--- ir/create_filelist.sh
+++ ir/create_filelist.sh
@@ -3,7 +3,7 @@
 # Little helper script used to create the file list for Makefile.am
 # automatically
 
-DIRS="adt ana arch be common debug ident ir libcore lower net obstack opt stat tr tv"
+DIRS="adt ana be common debug ident ir libcore lower net obstack opt stat tr tv"
 
 echo "libfirm_la_SOURCES = \\" > FILELIST
 for dir in $DIRS; do
@@ -13,11 +13,14 @@ for dir in $DIRS; do
 done
 
 echo "" >> FILELIST
+echo "" >> FILELIST
 echo "EXTRA_DIST = \\" >> FILELIST
 FILES=`find $DIRS -maxdepth 1 -name "*.h" -o -name "*.def" -o -name "*.sh"`
+FILES="$FILES `find ir -name "*.inl"`"
 for f in $FILES; do
 	echo -e "\t$f \\" >> FILELIST
 done
 for f in be/scripts/*.pl; do
 	echo -e "\t$f \\" >> FILELIST
 done
+echo "" >> FILELIST
