b283f49a6f60742d61610acfd60ce71582c35426
Matthias Braun <matze@braunis.de>
2010-05-19 17:58:36 +0000

    adapt formating to coding conventions
    [r27559]

--- include/libfirm/ident.h
+++ include/libfirm/ident.h
@@ -91,7 +91,7 @@ struct ident_if_t {
  *
  * @see get_id_str(), get_id_strlen()
  */
-ident *new_id_from_str (const char *str);
+ident *new_id_from_str(const char *str);
 
 /** Store a string and create an ident.
  *
@@ -105,7 +105,7 @@ ident *new_id_from_str (const char *str);
  *
  * @see new_id_from_str(), get_id_strlen()
  */
-ident *new_id_from_chars (const char *str, int len);
+ident *new_id_from_chars(const char *str, int len);
 
 /**
  * Returns a string represented by an ident.
@@ -119,7 +119,7 @@ ident *new_id_from_chars (const char *str, int len);
  *
  * @see new_id_from_str(), new_id_from_chars(), get_id_strlen()
  */
-const char *get_id_str  (ident *id);
+const char *get_id_str(ident *id);
 
 /**
  * Returns the length of the string represented by an ident.
@@ -130,7 +130,7 @@ const char *get_id_str  (ident *id);
  *
  * @see new_id_from_str(), new_id_from_chars(), get_id_str()
  */
-int  get_id_strlen(ident *id);
+int get_id_strlen(ident *id);
 
 /**
  * Returns true if prefix is a prefix of an ident.
@@ -140,7 +140,7 @@ int  get_id_strlen(ident *id);
  *
  * @see new_id_from_str(), new_id_from_chars(), get_id_str(), id_is_prefix()
  */
-int id_is_prefix (ident *prefix, ident *id);
+int id_is_prefix(ident *prefix, ident *id);
 
 /**
  * Returns true if suffix is a suffix of an ident.
@@ -150,7 +150,7 @@ int id_is_prefix (ident *prefix, ident *id);
  *
  * @see new_id_from_str(), new_id_from_chars(), get_id_str(), id_is_prefix()
  */
-int id_is_suffix (ident *suffix, ident *id);
+int id_is_suffix(ident *suffix, ident *id);
 
 /**
  * Return true if an ident contains a given character.
@@ -160,7 +160,7 @@ int id_is_suffix (ident *suffix, ident *id);
  *
  * @see new_id_from_str(), new_id_from_chars(), get_id_str()
  */
-int id_contains_char (ident *id, char c);
+int id_contains_char(ident *id, char c);
 
 /**
  * helper function for creating unique idents. It contains an internal counter
@@ -169,24 +169,24 @@ int id_contains_char (ident *id, char c);
 ident *id_unique(const char *tag);
 
 /** initializes the name mangling code */
-void   firm_init_mangle (void);
+void firm_init_mangle(void);
 
 /** Computes a definite name for this entity by concatenating
    the name of the owner type and the name of the entity with
    a separating "_". */
-ident *id_mangle_entity (ir_entity *ent);
+ident *id_mangle_entity(ir_entity *ent);
 
 /** mangle underscore: Returns a new ident that represents first_scnd. */
-ident *id_mangle_u (ident *first, ident* scnd);
+ident *id_mangle_u(ident *first, ident* scnd);
 
 /** mangle dot: Returns a new ident that represents first.scnd. */
-ident *id_mangle_dot (ident *first, ident* scnd);
+ident *id_mangle_dot(ident *first, ident* scnd);
 
 /** mangle: Returns a new ident that represents firstscnd. */
-ident *id_mangle   (ident *first, ident* scnd);
+ident *id_mangle(ident *first, ident* scnd);
 
 /** Returns a new ident that represents 'prefixscndsuffix'. */
-ident *id_mangle3 (const char *prefix, ident *middle, const char *suffix);
+ident *id_mangle3(const char *prefix, ident *middle, const char *suffix);
 
 /** returns a mangled name for a Win32 function using its calling convention */
 ident *id_decorate_win32_c_fkt(ir_entity *ent, ident *id);
