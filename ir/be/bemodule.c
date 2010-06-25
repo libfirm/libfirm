62faa8261c55cf8ada462c1c187b1c0a6313683e
Matthias Braun <matze@braunis.de>
2010-06-25 14:02:01 +0000

    remove the ppc32 backend (it's in a very broken state and not maintained)
    [r27661]

--- ir/be/bemodule.c
+++ ir/be/bemodule.c
@@ -54,7 +54,6 @@ void be_init_copystat(void);
 void be_init_daemelspill(void);
 void be_init_dbgout(void);
 void be_init_arch_ia32(void);
-void be_init_arch_ppc32(void);
 void be_init_arch_mips(void);
 void be_init_arch_arm(void);
 void be_init_arch_sparc(void);
@@ -134,7 +133,6 @@ void be_init_modules(void)
 	be_init_stabs();
 
 	be_init_arch_ia32();
-	be_init_arch_ppc32();
 	be_init_arch_mips();
 	be_init_arch_arm();
 	be_init_arch_sparc();
