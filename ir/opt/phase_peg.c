8a282322099530fc8e1186056af671aeea19a393
Olaf Liebe <olaf.liebe@student.kit.edu>
2010-07-27 14:56:37 +0200

    Added creation of Theta nodes and partial Extract creation.

--- ir/opt/phase_peg.c
+++ ir/opt/phase_peg.c
@@ -38,7 +38,7 @@
 #include "pmap.h"
 #include "pset_new.h"
 #include "cdep.h"
-#include "irouts.h"
+#include "iredges.h"
 
 static int get_pred_index(ir_node *block, ir_node *pred_block)
 {
@@ -83,14 +83,16 @@ static void find_cdep_reach(pset_new_t *reach, ir_node *block, ir_node *idom)
 	}
 }
 
-static ir_node *select_values(pset_new_t *reach, ir_node *block,
-                              ir_node *end, ir_node **values)
+static ir_node *select_values_walk(
+	pset_new_t *reach,   /* Nodes that can reach the preds via cdeps. */
+	ir_node    *block,   /* The current block to construct a selector for. */
+	ir_node    *end,     /* The end node where selection takes place. */
+	ir_node    **values) /* A value for each of the preds of end. */
 {
 	ir_cdep *dep;
-
 	ir_node *used_deps[2];
-	int     num_used_deps;
-	int     pred_index;
+	int      num_used_deps;
+	int      pred_index;
 
 	/* Get the reverse deps for the block. */
 	dep = find_rev_cdep(block);
@@ -113,7 +115,7 @@ static ir_node *select_values(pset_new_t *reach, ir_node *block,
 	/* Check if this is a predecessor of end and get the index. There should
 	 * always be somewhere to recurse except on a predecessor block. */
 	pred_index = get_pred_index(end, block);
-	assert((pred_index >= 0) || (num_used_deps > 0));
+	assert(((pred_index >= 0) || (num_used_deps > 0)) && "Nowhere to walk.");
 
 	/* There are four situations that may happen. (1) This is a predecessor
 	 * we have a value for. (2) This is no predecessor but there is one dependee
@@ -128,28 +130,29 @@ static ir_node *select_values(pset_new_t *reach, ir_node *block,
 
 	/* Case two. */
 	if ((num_used_deps == 1) && (pred_index < 0)) {
-		return select_values(reach, used_deps[0], end, values);
+		return select_values_walk(reach, used_deps[0], end, values);
 	}
 
 	/* Case three. */
 	if ((num_used_deps == 2) && (pred_index < 0)) {
 
 		ir_mode *mode;
-		ir_node *cond;
+		ir_node *cond, *ir_true, *ir_false, *succ, *succ_proj;
+		int      succ_req, succ_cur;
 
 		/* Get the values for the gamma. True/false may still be flipped. */
-		ir_node *ir_true  = select_values(reach, used_deps[0], end, values);
-		ir_node *ir_false = select_values(reach, used_deps[1], end, values);
+		ir_true  = select_values_walk(reach, used_deps[0], end, values);
+		ir_false = select_values_walk(reach, used_deps[1], end, values);
 
 		/* Get the first successor and find out which branch it belongs to. */
-		ir_node *succ      = get_Block_cfg_out(block, 0);
-		ir_node *succ_proj = get_pred_in_block(succ, block);
+		succ      = get_edge_src_irn(get_block_succ_first(block));
+		succ_proj = get_pred_in_block(succ, block);
 
 		/* Determine where the successor should lead to and where it really
 		 * leads. Note that list[0] will postdominate the successor that
 		 * depends on block but not block itself (and the other successor). */
-		int succ_req = (get_Proj_proj(succ_proj) == pn_Cond_true);
-		int succ_cur = block_postdominates(used_deps[0], succ);
+		succ_req = (get_Proj_proj(succ_proj) == pn_Cond_true);
+		succ_cur = block_postdominates(used_deps[0], succ);
 
 		/* If succ_cur is true, succ leads to list[0] and therefore to the
 		 * currently assigned ir_true. This has to match succ_req which is
@@ -174,71 +177,230 @@ static ir_node *select_values(pset_new_t *reach, ir_node *block,
 	return NULL;
 }
 
-static void replace_phis_walk(ir_node *block, void *ctx)
+static ir_node *select_values(pset_new_t *preds, ir_node *block, ir_node **values)
 {
-	int i, j;
-	int num_preds;
+	int                  num_preds;
+	ir_node             *idom, *result, *pred;
+	pset_new_t           reach;
+	pset_new_iterator_t  it;
 
-	ir_node    **phis, **tuples;
-	ir_node    *current, *idom, *res;
-	pset_new_t reach;
+	num_preds = pset_new_size(preds);
+	if (num_preds == 0) return NULL;
 
-	(void)ctx;
-
-	assert(is_Block(block));
-
-	/* Skip blocks without phis. */
-	if (get_Block_phis(block) == NULL) {
-		return;
-	}
-
-	num_preds = get_Block_n_cfgpreds(block);
-	phis      = NEW_ARR_F(ir_node*, 0);
-	tuples    = NEW_ARR_F(ir_node*, num_preds);
-
-	/* Create an array of phi nodes for this block. */
-	current = get_Block_phis(block);
-	while (current != NULL) {
-		ARR_APP1(ir_node*, phis, current);
-		current = get_Phi_next(current);
-	}
-
-	/* Mark all reachable nodes and create the selection graph. */
-	pset_new_init(&reach);
-	idom = get_Block_idom(block);
-
-	/* For each predecessor create a tuple of values that are selected. */
-	for (i = 0; i < num_preds; i++) {
-
-		/* Collect the values of all phis on the given predecessor. */
-		ir_node **values = NEW_ARR_F(ir_node*, ARR_LEN(phis));
-		ir_node  *pred   = get_nodes_block(get_Block_cfgpred(block, i));
-				for (j = 0; j < ARR_LEN(phis); j++)
-		{
-			values[j] = get_Phi_pred(phis[j], i);
+	/* Shortcut for exactly one pred. */
+	if (num_preds == 1) {
+		foreach_pset_new(preds, pred, it) {
+			int index = get_pred_index(block, pred);
+			assert(index >= 0);
+			return values[index];
 		}
+	}
 
-		/* Create a tuple from them. */
-		tuples[i] = new_r_Tuple(block, ARR_LEN(phis), values);
-		DEL_ARR_F(values);
+	/* Get the idom of the given pred blocks. */
+	idom = NULL;
+	foreach_pset_new(preds, pred, it) {
+		if (idom == NULL) idom = get_Block_idom(pred);
+		else idom = node_smallest_common_dominator(idom, pred);
+		assert(idom);
+	}
 
+	/* Discover reachable deps for the preds. */
+	pset_new_init(&reach);
+	foreach_pset_new(preds, pred, it) {
 		find_cdep_reach(&reach, pred, idom);
 	}
 
-	res = select_values(&reach, idom, block, tuples);
-
+	/* Walk down the reachable deps from the idom and create gammas. */
+	result = select_values_walk(&reach, idom, block, values);
 	pset_new_destroy(&reach);
 
-	/* Now from the resulting tuple construct projs to replace the phis. */
-	for (i = 0; i < ARR_LEN(phis); i++) {
-		ir_node *phi  = phis[i];
-		ir_mode *mode = get_irn_mode(phi);
-		ir_node *proj = new_r_Proj(res, mode, i);
-		exchange(phi, proj);
+	return result;
+}
+
+static int is_in_loop(ir_node *node, ir_loop *outer_loop)
+{
+	ir_loop *last;
+	ir_loop *loop;
+
+	if (!is_Block(node)) node = get_nodes_block(node);
+	loop = get_irn_loop(node);
+	if (loop == NULL) return 0;
+
+	do {
+		if (loop == outer_loop) return 1;
+
+		/* Stop when get_loop_outer_loop returns loop again. */
+		last = loop;
+		loop = get_loop_outer_loop(loop);
+	}
+	while(loop != last);
+
+	return 0;
+}
+
+static ir_node **get_incoming_values(ir_node *block)
+{
+	int num_phis, i;
+	int num_preds = get_Block_n_cfgpreds(block);
+
+	ir_node **tuples = NEW_ARR_F(ir_node*, num_preds);
+	ir_node  *phi;
+
+	/* Count phi nodes in this block. */
+	phi = get_Block_phis(block);
+	while (phi != NULL) {
+		num_phis++;
+		phi = get_Phi_next(phi);
 	}
 
+	/* For each predecessor create a tuple of values selected by the phis. */
+	for (i = 0; i < num_preds; i++) {
+		int j = 0;
+
+		/* Collect the values of all phis on the given predecessor. */
+		ir_node **values = NEW_ARR_F(ir_node*, num_phis);
+
+		phi = get_Block_phis(block);
+		while (phi != NULL) {
+			values[j++] = get_Phi_pred(phi, i);
+			phi = get_Phi_next(phi);
+		}
+
+		/* Create a tuple from them. */
+		tuples[i] = new_r_Tuple(block, num_phis, values);
+		DEL_ARR_F(values);
+	}
+
+	return tuples;
+}
+
+static void free_incoming_values(ir_node **tuples)
+{
 	DEL_ARR_F(tuples);
-	DEL_ARR_F(phis);
+}
+
+static void replace_phis_by_projs(ir_node *block, ir_node *tuple) {
+
+	ir_node *phi, *next_phi;
+	int i = 0;
+
+	/* For each node create a proj getting a value from the tuple. */
+	phi = get_Block_phis(block);
+	while (phi != NULL) {
+
+		ir_mode *mode = get_irn_mode(phi);
+		ir_node *proj = new_r_Proj(tuple, mode, i++);
+
+		next_phi = get_Phi_next(phi);
+		exchange(phi, proj);
+		phi = next_phi;
+	}
+}
+
+static ir_node *create_break_cond(ir_node *block, ir_loop *loop)
+{
+	/* Not supported yet. */
+	(void)loop;
+	return new_r_Unknown(get_irn_irg(block), mode_b);
+}
+
+static void create_phi_extracts(ir_node *block)
+{
+	int      i;
+	ir_node *phi;
+	ir_loop *loop = get_irn_loop(block);
+
+	/* No extracts needed when accessing phis outside of loops. */
+	if (!loop) return;
+
+	/* Iterate all phi nodes in the current block. */
+	phi = get_Block_phis(block);
+	while (phi != NULL) {
+		const ir_edge_t *edge, *tmp;
+
+		foreach_out_edge_safe(phi, edge, tmp) {
+			ir_node *src = get_edge_src_irn(edge);
+
+			/* When someone outside the loop accesses the phi node, construct
+			 * an extract node to get the appropriate value. */
+			if (!is_in_loop(src, loop)) {
+
+				ir_mode  *mode      = get_irn_mode(phi);
+				ir_node  *cond      = create_break_cond(block, loop);
+				ir_node  *src_block = get_nodes_block(src);
+				ir_node  *extract   = new_r_Extract(src_block, phi, cond, mode);
+
+				/* Rewire src to access the extract node instead. */
+				for (i = 0; i < get_irn_arity(src); i++) {
+					ir_node *dep = get_irn_n(src, i);
+					if (dep == phi) set_irn_n(src, i, extract);
+				}
+			}
+		}
+
+		phi = get_Phi_next(phi);
+	}
+}
+
+static void replace_phis_walk(ir_node *block, void *ctx)
+{
+	int          i;
+	ir_loop     *loop;
+	ir_node    **values, *outer_tuple, *inner_tuple, *result;
+	pset_new_t   outer_preds, inner_preds;
+
+	(void)ctx;
+
+	/* Skip blocks without phis. */
+	if (get_Block_phis(block) == NULL) return;
+
+	/* Obtain for each predecessor the values selected by phis. */
+	values = get_incoming_values(block);
+
+	/* Find out if we need to construct a theta node or just gamma nodes.
+	 * If we are inside a loop header (there are incoming paths from the loop
+	 * and from outside the loop), we need a theta node. The values from those
+	 * predecessors outside the loop provide the init value and those from
+	 * inside the loop the next value. Use the usual gamma tree construction
+	 * for both values, but on a subset of predecessors only. */
+
+	/* Collect pred nodes outside the current loop. */
+	pset_new_init(&outer_preds);
+	pset_new_init(&inner_preds);
+
+	loop = get_irn_loop(block);
+	for (i = 0; i < get_Block_n_cfgpreds(block); i++) {
+
+		ir_node    *pred = get_nodes_block(get_Block_cfgpred(block, i));
+		pset_new_t *set  = &inner_preds;
+
+		/* Classify preds by their origin (inside the loop or outside). If
+		 * there is no loop, put everything in inner_preds. */
+		if (loop && !is_in_loop(pred, loop)) {
+			set = &outer_preds;
+		}
+
+		pset_new_insert(set, pred);
+	}
+
+	/* Create gammas for the outer and inner preds. Can return NULL. */
+	inner_tuple = select_values(&inner_preds, block, values);
+	outer_tuple = select_values(&outer_preds, block, values);
+	assert(inner_tuple);
+
+	/* If there are outer and inner preds, construct a theta node. */
+	result = inner_tuple;
+	if (outer_tuple) {
+		result = new_r_Theta(block, outer_tuple, inner_tuple, mode_T);
+	}
+
+	/* Construct projs on the result. */
+	create_phi_extracts(block);
+	replace_phis_by_projs(block, result);
+
+	pset_new_destroy(&inner_preds);
+	pset_new_destroy(&outer_preds);
+	free_incoming_values(values);
 }
 
 static void replace_phis(ir_graph *irg)
@@ -249,7 +411,7 @@ static void replace_phis(ir_graph *irg)
 
 	/* We need to walk the CFG in reverse order and access dominators. */
 	assure_doms(irg);
-	assure_irg_outs(irg);
+	assure_cf_loop(irg);
 
 	/* Create lists of phi nodes in each block. */
 	ir_reserve_resources(irg, resources);
@@ -309,34 +471,36 @@ static void remove_blocks(ir_graph *irg)
  * Unfolds a value in a tree of tupelized gammas. This replicates the tree
  * along the way with the values mode.
  */
-static ir_node *extract_gammas(ir_node *irn, int idx, ir_mode *mode)
+static ir_node *unfold_tuples_walk(ir_node *irn, int idx, ir_mode *mode)
 {
-	if (is_Gamma(irn))
-	{
-		/* Construct a new gamma for the value. */
-		ir_node *block    = get_nodes_block(irn);
-		ir_node *cond     = get_Gamma_cond(irn);
-		ir_node *ir_true  = extract_gammas(get_Gamma_true(irn),  idx, mode);
-		ir_node *ir_false = extract_gammas(get_Gamma_false(irn), idx, mode);
-		ir_node *gamma    = new_r_Gamma(block, cond, ir_false, ir_true, mode);
+	ir_node *block = get_nodes_block(irn);
 
-		return gamma;
+	if (is_Gamma(irn)) {
+		/* Construct a new gamma for the value. */
+		ir_node *cond     = get_Gamma_cond(irn);
+		ir_node *ir_true  = unfold_tuples_walk(get_Gamma_true(irn),  idx, mode);
+		ir_node *ir_false = unfold_tuples_walk(get_Gamma_false(irn), idx, mode);
+		return new_r_Gamma(block, cond, ir_false, ir_true, mode);
 	}
-	else if (is_Tuple(irn))
-	{
+	else if (is_Theta(irn)) {
+		/* Same thing for the theta. */
+		ir_node *init = unfold_tuples_walk(get_Theta_init(irn), idx, mode);
+		ir_node *next = unfold_tuples_walk(get_Theta_next(irn), idx, mode);
+		return new_r_Theta(block, init, next, mode);
+	}
+	else if (is_Tuple(irn)) {
 		/* Extract the appropriate value from the tuple. */
 		ir_node *res = get_Tuple_pred(irn, idx);
 		assert((mode == get_irn_mode(res)) && "Unexpected mode.");
-
 		return res;
 	}
 
 	assert(0 && "Invalid tupelized gamma tree.");
 }
 
-static void unfold_tuples_walk(ir_node *irn, void *ctx)
+static void unfold_tuples_find(ir_node *irn, void *ctx)
 {
-	ir_node *gamma, *value;
+	ir_node *tuple, *value;
 	ir_mode *value_mode;
 	int      value_pn;
 
@@ -345,27 +509,27 @@ static void unfold_tuples_walk(ir_node *irn, void *ctx)
 	/* Search for proj nodes. */
 	if (!is_Proj(irn)) return;
 
-	/* That point to gammas. */
-	gamma = get_Proj_pred(irn);
-	if (!is_Gamma(gamma)) return;
-
-	/* Unfold the value from the tupelized gamma. */
+	/* That point to gammas or thetas. */
+	tuple      = get_Proj_pred(irn);
 	value_pn   = get_Proj_proj(irn);
 	value_mode = get_irn_mode(irn);
-	value      = extract_gammas(gamma, value_pn, value_mode);
 
-	exchange(irn, value);
+	if (is_Gamma(tuple) || is_Theta(tuple)) {
+		value = unfold_tuples_walk(tuple, value_pn, value_mode);
+		exchange(irn, value);
+	}
 }
 
 static void unfold_tuples(ir_graph *irg)
 {
-	irg_walk_graph(irg, NULL, unfold_tuples_walk, NULL);
+	irg_walk_graph(irg, NULL, unfold_tuples_find, NULL);
 }
 
 void convert_to_peg(ir_graph *irg)
 {
 	/* Use automatic out edges. Makes things easier later. */
 	int opt_level = get_optimize();
+	int had_edges = edges_assure(irg);
 	set_optimize(0);
 
 	dump_ir_graph(irg, "cfg");
@@ -401,4 +565,5 @@ void convert_to_peg(ir_graph *irg)
 
 	optimize_cf(irg);
 	set_optimize(opt_level);
+	if (!had_edges) edges_deactivate(irg);
 }
