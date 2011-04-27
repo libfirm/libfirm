3dbd0a96a49a2312f0d1e7b90cd1ff152c1f204a
Matthias Braun <matze@braunis.de>
2011-04-27 13:34:45 +0200

    cleanup/reformat irop_t.h

--- ir/ir/irop_t.h
+++ ir/ir/irop_t.h
@@ -27,6 +27,9 @@
 #define FIRM_IR_IROP_T_H
 
 #include "irop.h"
+
+#include <stdbool.h>
+
 #include "irtypes.h"
 #include "tv.h"
 
@@ -55,7 +58,8 @@ void default_copy_attr(ir_graph *irg, const ir_node *old_node,
  * Returns the attribute size of nodes of this opcode.
  * @note Use not encouraged, internal feature.
  */
-static inline size_t get_op_attr_size (const ir_op *op) {
+static inline size_t get_op_attr_size (const ir_op *op)
+{
 	return op->attr_size;
 }
 
@@ -63,111 +67,135 @@ static inline size_t get_op_attr_size (const ir_op *op) {
  * Returns non-zero if op is a control flow opcode,
  * like Start, End, Jmp, Cond, Return, Raise or Bad.
  */
-static inline int is_op_cfopcode(const ir_op *op) {
+static inline bool is_op_cfopcode(const ir_op *op)
+{
 	return op->flags & irop_flag_cfopcode;
 }
 
 /** Returns non-zero if operation is commutative */
-static inline int is_op_commutative(const ir_op *op) {
+static inline bool is_op_commutative(const ir_op *op)
+{
 	return op->flags & irop_flag_commutative;
 }
 
 /** Returns non-zero if operation is fragile */
-static inline int is_op_fragile(const ir_op *op) {
+static inline bool is_op_fragile(const ir_op *op)
+{
 	return op->flags & irop_flag_fragile;
 }
 
 /** Returns non-zero if operation is forking control flow */
-static inline int is_op_forking(const ir_op *op) {
+static inline bool is_op_forking(const ir_op *op)
+{
 	return op->flags & irop_flag_forking;
 }
 
 /** Returns non-zero if operation is a high-level op */
-static inline int is_op_highlevel(const ir_op *op) {
+static inline bool is_op_highlevel(const ir_op *op)
+{
 	return op->flags & irop_flag_highlevel;
 }
 
 /** Returns non-zero if operation is a const-like op */
-static inline int is_op_constlike(const ir_op *op) {
+static inline bool is_op_constlike(const ir_op *op)
+{
 	return op->flags & irop_flag_constlike;
 }
 
-static inline int is_op_uses_memory(const ir_op *op) {
+static inline bool is_op_uses_memory(const ir_op *op)
+{
 	return op->flags & irop_flag_uses_memory;
 }
 
 /** Returns non-zero if operation must always be optimized */
-static inline int is_op_always_opt(const ir_op *op) {
+static inline bool is_op_always_opt(const ir_op *op)
+{
 	return op->flags & irop_flag_always_opt;
 }
 
 /** Returns non-zero if operation is a keep-like op */
-static inline int is_op_keep(const ir_op *op) {
+static inline bool is_op_keep(const ir_op *op)
+{
 	return op->flags & irop_flag_keep;
 }
 
 /** Returns non-zero if operation must always be placed in the start block. */
-static inline int is_op_start_block_placed(const ir_op *op) {
+static inline bool is_op_start_block_placed(const ir_op *op)
+{
 	return op->flags & irop_flag_start_block;
 }
 
 /** Returns non-zero if operation is a machine operation */
-static inline int is_op_machine(const ir_op *op) {
+static inline bool is_op_machine(const ir_op *op)
+{
 	return op->flags & irop_flag_machine;
 }
 
 /** Returns non-zero if operation is a machine operand */
-static inline int is_op_machine_operand(const ir_op *op) {
+static inline bool is_op_machine_operand(const ir_op *op)
+{
 	return op->flags & irop_flag_machine_op;
 }
 
 /** Returns non-zero if operation is CSE neutral */
-static inline int is_op_cse_neutral(const ir_op *op) {
+static inline bool is_op_cse_neutral(const ir_op *op)
+{
 	return op->flags & irop_flag_cse_neutral;
 }
 
 /** Returns non-zero if operation is a machine user op number n */
-static inline int is_op_machine_user(const ir_op *op, unsigned n) {
-  return op->flags & (irop_flag_user << n);
+static inline bool is_op_machine_user(const ir_op *op, unsigned n)
+{
+	return op->flags & (irop_flag_user << n);
 }
 
-static inline unsigned _get_op_code(const ir_op *op) {
-  return op->code;
+static inline unsigned _get_op_code(const ir_op *op)
+{
+	return op->code;
 }
 
-static inline ident *_get_op_ident(const ir_op *op){
-  return op->name;
+static inline ident *_get_op_ident(const ir_op *op)
+{
+	return op->name;
 }
 
-static inline op_pin_state _get_op_pinned(const ir_op *op) {
-  return op->pin_state;
+static inline op_pin_state _get_op_pinned(const ir_op *op)
+{
+	return op->pin_state;
 }
 
-static inline void _set_generic_function_ptr(ir_op *op, op_func func) {
-  op->ops.generic = func;
+static inline void _set_generic_function_ptr(ir_op *op, op_func func)
+{
+	op->ops.generic = func;
 }
 
-static inline op_func _get_generic_function_ptr(const ir_op *op) {
-  return op->ops.generic;
+static inline op_func _get_generic_function_ptr(const ir_op *op)
+{
+	return op->ops.generic;
 }
 
-static inline const ir_op_ops *_get_op_ops(const ir_op *op) {
-  return &op->ops;
+static inline const ir_op_ops *_get_op_ops(const ir_op *op)
+{
+	return &op->ops;
 }
 
-static inline void _set_op_tag(ir_op *op, unsigned tag) {
+static inline void _set_op_tag(ir_op *op, unsigned tag)
+{
 	op->tag = tag;
 }
 
-static inline unsigned _get_op_tag(const ir_op *op) {
+static inline unsigned _get_op_tag(const ir_op *op)
+{
 	return op->tag;
 }
 
-static inline void _set_op_attr(ir_op *op, void *attr) {
+static inline void _set_op_attr(ir_op *op, void *attr)
+{
 	op->attr = attr;
 }
 
-static inline void *_get_op_attr(const ir_op *op) {
+static inline void *_get_op_attr(const ir_op *op)
+{
 	return op->attr;
 }
 
