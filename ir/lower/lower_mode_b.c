1cfc0c70928892617963518f03107b977aea9180
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-27 10:38:33 +0200

    edges deactivate fix
    Broken with csmith 2.0.0 seed 909842313
    in libFirm 1953ce88fe94d589a9b47d2ab8969c21bfb55b3a

--- ir/lower/lower_mode_b.c
+++ ir/lower/lower_mode_b.c
@@ -460,5 +460,6 @@ void ir_lower_mode_b(ir_graph *irg, const lower_mode_b_config_t *nconfig)
 		/* lowering might create new blocks, so be sure to handle this */
 		set_irg_extblk_inconsistent(irg);
 		set_irg_doms_inconsistent(irg);
+		edges_deactivate(irg);
 	}
 }
