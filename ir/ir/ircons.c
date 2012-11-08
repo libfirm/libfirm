3f93aa800a9e13f0f8e5b082b51fe655c06dc759
Christoph Mallon <christoph.mallon@gmx.de>
2012-11-08 16:26:00 +0100

    Remove further keep-edge hacks for endless loops.

--- ir/ir/ircons.c
+++ ir/ir/ircons.c
@@ -240,11 +240,6 @@ static ir_node *set_phi_arguments(ir_node *phi, int pos)
 
 	irn_verify_irg(phi, irg);
 
-	/* Memory Phis in endless loops must be kept alive.
-	   As we can't distinguish these easily we keep all of them alive. */
-	if (mode == mode_M)
-		add_End_keepalive(get_irg_end(irg), phi);
-
 	try_remove_unnecessary_phi(phi);
 	return phi;
 }
