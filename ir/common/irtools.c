cae33b2510c0e87d5066d55a180a750044922f11
Matthias Braun <matze@braunis.de>
2013-07-31 01:34:14 -0700

    cleanup, use C99

--- ir/common/irtools.c
+++ ir/common/irtools.c
@@ -50,22 +50,21 @@ void firm_collect_block_phis(ir_node *node, void *env)
 
 void copy_irn_to_irg(ir_node *n, ir_graph *irg)
 {
-	ir_graph *old_irg;
-	ir_node *nn = NULL;
-
 	/* do not copy standard nodes */
+	ir_node *nn = NULL;
 	switch (get_irn_opcode(n)) {
 	case iro_NoMem:
-		n = get_irg_no_mem(irg);
+		nn = get_irg_no_mem(irg);
 		break;
 
-	case iro_Block:
-		old_irg = get_irn_irg(n);
+	case iro_Block: {
+		ir_graph *old_irg = get_irn_irg(n);
 		if (n == get_irg_start_block(old_irg))
 			nn = get_irg_start_block(irg);
 		else if (n == get_irg_end_block(old_irg))
 			nn = get_irg_end_block(irg);
 		break;
+	}
 
 	case iro_Start:
 		nn = get_irg_start(irg);
@@ -75,8 +74,8 @@ void copy_irn_to_irg(ir_node *n, ir_graph *irg)
 		nn = get_irg_end(irg);
 		break;
 
-	case iro_Proj:
-		old_irg = get_irn_irg(n);
+	case iro_Proj: {
+		ir_graph *old_irg = get_irn_irg(n);
 		if (n == get_irg_initial_exec(old_irg))
 			nn = get_irg_initial_exec(irg);
 		else if (n == get_irg_frame(old_irg))
@@ -86,6 +85,7 @@ void copy_irn_to_irg(ir_node *n, ir_graph *irg)
 		else if (n == get_irg_args(old_irg))
 			nn = get_irg_args(irg);
 		break;
+	}
 	}
 
 	if (nn) {
@@ -116,27 +116,20 @@ void copy_irn_to_irg(ir_node *n, ir_graph *irg)
 
 ir_node *irn_copy_into_irg(const ir_node *node, ir_graph *irg)
 {
-	ir_node  *block = NULL;
 	ir_op    *op    = get_irn_op(node);
-	int       arity = get_irn_arity(node);
+	ir_node  *block = op != op_Block ? get_nodes_block(node) : NULL;
 	dbg_info *dbgi  = get_irn_dbg_info(node);
 	ir_mode  *mode  = get_irn_mode(node);
 	ir_node  *res;
-	int       n_deps;
-	int       i;
-
-	if (op != op_Block)
-		block = get_nodes_block(node);
-
 	if (op->opar == oparity_dynamic) {
-		int i;
 		res = new_ir_node(dbgi, irg, block, op, mode, -1, NULL);
-		for (i = 0; i < arity; ++i) {
+		for (int i = 0, arity = get_irn_arity(node); i < arity; ++i) {
 			ir_node *in = get_irn_n(node, i);
 			add_irn_n(res, in);
 		}
 	} else {
-		ir_node **ins = get_irn_in(node)+1;
+		ir_node **ins   = get_irn_in(node)+1;
+		int       arity = get_irn_arity(node);
 		res = new_ir_node(dbgi, irg, block, op, mode, arity, ins);
 	}
 
@@ -144,8 +137,7 @@ ir_node *irn_copy_into_irg(const ir_node *node, ir_graph *irg)
 	copy_node_attr(irg, node, res);
 
 	/* duplicate dependency edges */
-	n_deps = get_irn_deps(node);
-	for (i = 0; i < n_deps; ++i) {
+	for (int i = 0, n_deps = get_irn_deps(node); i < n_deps; ++i) {
 		ir_node *dep = get_irn_dep(node, i);
 		add_irn_dep(res, dep);
 	}
@@ -165,28 +157,20 @@ static ir_node *get_new_node(const ir_node *old_node)
 
 void irn_rewire_inputs(ir_node *node)
 {
-	ir_node *new_node;
-	int      arity;
-	int      n_deps;
-	int      i;
-
-	new_node = get_new_node(node);
-
+	ir_node *new_node = get_new_node(node);
 	if (!is_Block(node)) {
 		ir_node *block     = get_nodes_block(node);
 		ir_node *new_block = get_new_node(block);
 		set_nodes_block(new_node, new_block);
 	}
 
-	arity = get_irn_arity(new_node);
-	for (i = 0; i < arity; ++i) {
+	for (int i = 0, arity = get_irn_arity(new_node); i < arity; ++i) {
 		ir_node *in     = get_irn_n(node, i);
 		ir_node *new_in = get_new_node(in);
 		set_irn_n(new_node, i, new_in);
 	}
 
-	n_deps = get_irn_deps(new_node);
-	for (i = 0; i < n_deps; ++i) {
+	for (int i = 0, n_deps = get_irn_deps(new_node); i < n_deps; ++i) {
 		ir_node *dep     = get_irn_dep(node, i);
 		ir_node *new_dep = get_new_node(dep);
 		set_irn_dep(new_node, i, new_dep);
