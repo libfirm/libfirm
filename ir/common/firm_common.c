a2f0f545fcae7419b4daf83ed0032d9ad28e7b49
Christoph Mallon <christoph.mallon@gmx.de>
2014-01-06 13:35:00 +0100

    Remove the unimplemented macro POINTER_READ().

--- ir/common/firm_common.c
+++ ir/common/firm_common.c
@@ -10,14 +10,8 @@
 #include "irloop.h"
 #include "tv.h"
 
-/**
- * Ideally, this macro would check if size bytes could be read at
- * pointer p. No generic solution.
- */
-#define POINTER_READ(p, size) (p)
-
 /* returns the kind of the thing */
 firm_kind get_kind(const void *firm_thing)
 {
-	return POINTER_READ(firm_thing, sizeof(firm_kind)) ? *(firm_kind *)firm_thing : k_BAD;
+	return *(firm_kind*)firm_thing;
 }
