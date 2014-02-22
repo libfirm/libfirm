2eb309fcc39936499138b98aff532f5f5393895f
Christoph Mallon <christoph.mallon@gmx.de>
2014-02-22 12:54:10 +0100

    convopt: Cleanup.

--- ir/opt/convopt.c
+++ ir/opt/convopt.c
@@ -42,36 +42,32 @@ static inline int imin(int a, int b) { return a < b ? a : b; }
 static bool is_optimizable_node(const ir_node *node, ir_mode *dest_mode)
 {
 	switch (get_irn_opcode(node)) {
-	case iro_Minus:
-	case iro_Phi:
 	case iro_And:
 	case iro_Eor:
-	case iro_Or:
+	case iro_Minus:
 	case iro_Not:
+	case iro_Or:
+	case iro_Phi:
 		return true;
+
 	case iro_Add:
 	case iro_Mul:
 	case iro_Sub:
-		if (mode_is_float(get_irn_mode(node)))
-			return false;
-		return true;
+		return !mode_is_float(get_irn_mode(node));
+
 	case iro_Shr:
 	case iro_Shrs: {
 		int dest_size = get_mode_size_bits(dest_mode);
 		int size      = get_mode_size_bits(get_irn_mode(node));
-
 		if (dest_size != size)
 			return false;
-
-		/* fall through */
+		/* FALLTHROUGH */
 	}
 	case iro_Shl: {
 		int modulo_shift = get_mode_modulo_shift(dest_mode);
 		int old_shift    = get_mode_modulo_shift(get_irn_mode(node));
 		/* bail out if modulo shift changes */
-		if (modulo_shift != old_shift)
-			return false;
-		return true;
+		return modulo_shift == old_shift;
 	}
 
 	default:
@@ -91,28 +87,32 @@ static bool is_downconv(ir_mode *src_mode, ir_mode *dest_mode)
 		&& get_mode_size_bits(dest_mode) <= get_mode_size_bits(src_mode);
 }
 
-static int get_conv_costs(const ir_node *node, ir_mode *dest_mode)
+static bool is_shift(ir_node const *const node)
 {
-	ir_mode *mode = get_irn_mode(node);
-	int arity;
-	int i;
-	int costs;
+	return is_Shl(node) || is_Shr(node) || is_Shrs(node);
+}
 
+static int get_conv_costs(const ir_node *const node, ir_mode *const dest_mode)
+{
+	ir_mode *const mode = get_irn_mode(node);
 	if (mode == dest_mode)
 		return 0;
 
-	if (is_Const(node)) {
+	switch (get_irn_opcode(node)) {
+	case iro_Const: {
 		ir_tarval *conved = conv_const_tv(node, dest_mode);
-		return (conved != tarval_bad && conved != tarval_unknown)
-			? 0 : 1;
-	} else if (is_Unknown(node)) {
-		return 0;
+		return (conved != tarval_bad && conved != tarval_unknown) ? 0 : 1;
 	}
 
-	if (is_Conv(node) &&
-			is_downconv(mode, dest_mode) &&
-			get_irn_mode(get_Conv_op(node)) == dest_mode) {
-		return -1;
+	case iro_Conv:
+		if (is_downconv(mode, dest_mode) &&
+				get_irn_mode(get_Conv_op(node)) == dest_mode) {
+			return -1;
+		}
+		break;
+
+	case iro_Unknown:
+		return 0;
 	}
 
 	if (get_irn_n_edges(node) > 1) {
@@ -120,40 +120,33 @@ static int get_conv_costs(const ir_node *node, ir_mode *dest_mode)
 		return 1;
 	}
 
-	if (ir_zero_when_converted(node, dest_mode)) {
+	if (ir_zero_when_converted(node, dest_mode))
 		return -1;
-	}
 
 	/* TODO: Phi nodes */
 
-	if (!is_downconv(mode, dest_mode)) {
+	if (!is_downconv(mode, dest_mode))
 		return 1;
-	}
 
 	if (is_Conv(node)) {
 		ir_node *pred      = get_Conv_op(node);
 		ir_mode *pred_mode = get_irn_mode(pred);
-
 		if (smaller_mode(pred_mode, dest_mode)) {
-			return get_conv_costs(get_Conv_op(node), dest_mode) - 1;
-		}
-		if (may_leave_out_middle_conv(pred_mode, mode, dest_mode)) {
+			return get_conv_costs(pred, dest_mode) - 1;
+		} else if (may_leave_out_middle_conv(pred_mode, mode, dest_mode)) {
 			return 0;
 		} else {
 			return 1;
 		}
 	}
 
-	if (!is_optimizable_node(node, dest_mode)) {
+	if (!is_optimizable_node(node, dest_mode))
 		return 1;
-	}
 
-	costs = 0;
+	int costs = 0;
 	// The shift count does not participate in the conv optimization
-	arity = is_Shl(node)  ? 1 :
-	        is_Shr(node)  ? 1 :
-	        is_Shrs(node) ? 1 : get_irn_arity(node);
-	for (i = 0; i < arity; ++i) {
+	int const arity = is_shift(node) ? 1 : get_irn_arity(node);
+	for (int i = 0; i < arity; ++i) {
 		ir_node *pred = get_irn_n(node, i);
 		costs += imin(get_conv_costs(pred, dest_mode), 1);
 	}
@@ -164,71 +157,62 @@ static int get_conv_costs(const ir_node *node, ir_mode *dest_mode)
 static ir_node *place_conv(ir_node *node, ir_mode *dest_mode)
 {
 	ir_node *block = get_nodes_block(node);
-	ir_node *conv = new_r_Conv(block, node, dest_mode);
+	ir_node *conv  = new_r_Conv(block, node, dest_mode);
 	return conv;
 }
 
 static ir_node *conv_transform(ir_node *node, ir_mode *dest_mode)
 {
-	ir_mode  *mode = get_irn_mode(node);
-	ir_graph *irg  = get_irn_irg(node);
-	int       arity;
-	int       conv_arity;
-	int       i;
-	ir_node  *new_node;
-	ir_node **ins;
-
+	ir_mode *const mode = get_irn_mode(node);
 	if (mode == dest_mode)
 		return node;
 
-	if (is_Const(node)) {
+	ir_graph *const irg  = get_irn_irg(node);
+	switch (get_irn_opcode(node)) {
+	case iro_Const: {
 		ir_tarval *tv = conv_const_tv(node, dest_mode);
 		if (tv == tarval_bad || tv == tarval_unknown) {
 			return place_conv(node, dest_mode);
 		} else {
 			return new_r_Const(irg, tv);
 		}
-	} else if (is_Unknown(node)) {
+	}
+
+	case iro_Conv:
+		if (is_downconv(mode, dest_mode) &&
+				get_irn_mode(get_Conv_op(node)) == dest_mode) {
+			return get_Conv_op(node);
+		}
+		break;
+
+	case iro_Unknown:
 		return new_r_Unknown(irg, dest_mode);
 	}
 
-	if (is_Conv(node) &&
-			is_downconv(mode, dest_mode) &&
-			get_irn_mode(get_Conv_op(node)) == dest_mode) {
-		return get_Conv_op(node);
-	}
-
-	if (get_irn_n_edges(node) > 1) {
+	if (get_irn_n_edges(node) > 1)
 		return place_conv(node, dest_mode);
-	}
 
-	if (!is_downconv(mode, dest_mode)) {
+	if (!is_downconv(mode, dest_mode))
 		return place_conv(node, dest_mode);
-	}
 
 	if (is_Conv(node)) {
 		ir_node *pred      = get_Conv_op(node);
 		ir_mode *pred_mode = get_irn_mode(pred);
-
-		if (smaller_mode(pred_mode, dest_mode)) {
+		if (smaller_mode(pred_mode, dest_mode))
 			return conv_transform(get_Conv_op(node), dest_mode);
-		}
 		return place_conv(node, dest_mode);
 	}
 
-	if (!is_optimizable_node(node, dest_mode)) {
+	if (!is_optimizable_node(node, dest_mode))
 		return place_conv(node, dest_mode);
-	}
 
 	// We want to create a new node with the right mode
-	arity = get_irn_arity(node);
-	ins = ALLOCAN(ir_node *, arity);
+	int       const arity = get_irn_arity(node);
+	ir_node **const ins   = ALLOCAN(ir_node *, arity);
 
 	// The shift count does not participate in the conv optimization
-	conv_arity = is_Shl(node)  ? 1 :
-	             is_Shr(node)  ? 1 :
-	             is_Shrs(node) ? 1 : arity;
-	for (i = 0; i < conv_arity; i++) {
+	int const conv_arity = is_shift(node) ? 1 : arity;
+	for (int i = 0; i < conv_arity; i++) {
 		ir_node *pred = get_irn_n(node, i);
 		ir_node *transformed;
 		if (get_conv_costs(pred, dest_mode) > 0) {
@@ -239,17 +223,14 @@ static ir_node *conv_transform(ir_node *node, ir_mode *dest_mode)
 		ins[i] = transformed;
 	}
 
-	for (i = conv_arity; i < arity; i++) {
+	for (int i = conv_arity; i < arity; i++) {
 		ins[i] = get_irn_n(node, i);
 	}
 
-	new_node = new_ir_node(get_irn_dbg_info(node),
-				irg,
-				get_nodes_block(node),
-				get_irn_op(node),
-				dest_mode,
-				arity,
-				ins);
+	dbg_info *const dbg      = get_irn_dbg_info(node);
+	ir_node  *const block    = get_nodes_block(node);
+	ir_op    *const op       = get_irn_op(node);
+	ir_node  *const new_node = new_ir_node(dbg, irg, block, op, dest_mode, arity, ins);
 	copy_node_attr(irg, node, new_node);
 
 	return new_node;
@@ -257,33 +238,30 @@ static ir_node *conv_transform(ir_node *node, ir_mode *dest_mode)
 
 static void conv_opt_walker(ir_node *node, void *data)
 {
-	ir_node *transformed;
-	ir_node *pred;
-	ir_mode *pred_mode;
-	ir_mode *mode;
-	int costs;
-	bool *changed = (bool*)data;
+	bool *const changed = (bool*)data;
 
 	if (!is_Conv(node))
 		return;
 
-	pred      = get_Conv_op(node);
-	mode      = get_irn_mode(node);
-	pred_mode = get_irn_mode(pred);
+	ir_mode *const mode = get_irn_mode(node);
+	if (mode_is_reference(mode))
+		return;
 
-	if (mode_is_reference(mode) || mode_is_reference(pred_mode))
+	ir_node *const pred      = get_Conv_op(node);
+	ir_mode *const pred_mode = get_irn_mode(pred);
+	if (mode_is_reference(pred_mode))
 		return;
 
 	if (!is_Phi(pred) && !is_downconv(pred_mode, mode))
 		return;
 
 	/* - 1 for the initial conv */
-	costs = get_conv_costs(pred, mode) - 1;
+	int const costs = get_conv_costs(pred, mode) - 1;
 	DB((dbg, LEVEL_2, "Costs for %+F -> %+F: %d\n", node, pred, costs));
 	if (costs > 0)
 		return;
 
-	transformed = conv_transform(pred, mode);
+	ir_node *const transformed = conv_transform(pred, mode);
 	if (node != transformed) {
 		exchange(node, transformed);
 		*changed = true;
@@ -292,14 +270,14 @@ static void conv_opt_walker(ir_node *node, void *data)
 
 void conv_opt(ir_graph *irg)
 {
-	bool global_changed = false;
-	bool changed;
 	FIRM_DBG_REGISTER(dbg, "firm.opt.conv");
 
 	assure_irg_properties(irg, IR_GRAPH_PROPERTY_CONSISTENT_OUT_EDGES);
 
 	DB((dbg, LEVEL_1, "===> Performing conversion optimization on %+F\n", irg));
 
+	bool global_changed = false;
+	bool changed;
 	do {
 		changed = false;
 		irg_walk_graph(irg, NULL, conv_opt_walker, &changed);
