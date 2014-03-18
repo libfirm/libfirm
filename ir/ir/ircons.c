106c319bb2784d280bcfcce82bc57b7e05a4af62
Christoph Mallon <christoph.mallon@gmx.de>
2014-03-18 14:52:34 +0100

    ircons: Cleanup.

--- ir/ir/ircons.c
+++ ir/ir/ircons.c
@@ -10,23 +10,25 @@
  * @author  Martin Trapp, Christian Schaefer, Goetz Lindenmaier, Boris Boesler
  *          Michael Beck, Matthias Braun
  */
-#include "irprog_t.h"
-#include "irgraph_t.h"
-#include "irnode_t.h"
-#include "irmode_t.h"
-#include "ircons_t.h"
-#include "irverify.h"
-#include "irop_t.h"
-#include "iropt_t.h"
-#include "irgmod.h"
-#include "irhooks.h"
+
+#include "../adt/util.h"
 #include "array.h"
+#include "error.h"
 #include "irbackedge_t.h"
-#include "irflag_t.h"
+#include "ircons_t.h"
 #include "iredges_t.h"
 #include "irflag_t.h"
-#include "error.h"
+#include "irflag_t.h"
+#include "irgmod.h"
 #include "irgopt.h"
+#include "irgraph_t.h"
+#include "irhooks.h"
+#include "irmode_t.h"
+#include "irnode_t.h"
+#include "irop_t.h"
+#include "iropt_t.h"
+#include "irprog_t.h"
+#include "irverify.h"
 
 /**
  * Language dependent variable initialization callback.
@@ -44,25 +46,26 @@ ir_node *new_rd_ASM(dbg_info *db, ir_node *block, ir_node *mem,
                     size_t n_outs, ir_asm_constraint *outputs, size_t n_clobber,
 	                ident *clobber[], ident *text)
 {
-	ir_graph *irg = get_irn_irg(block);
+	ir_graph *const irg = get_irn_irg(block);
 
-	int r_arity = arity+1;
-	ir_node **r_in = ALLOCAN(ir_node*,r_arity);
+	int       const r_arity = arity + 1;
+	ir_node **const r_in    = ALLOCAN(ir_node*, r_arity);
 	r_in[0] = mem;
-	memcpy(&r_in[1], in, arity*sizeof(ir_node*));
+	memcpy(&r_in[1], in, arity * sizeof(r_in[1]));
 
 	ir_node *res = new_ir_node(db, irg, block, op_ASM, mode_T, r_arity, r_in);
 
 	struct obstack *const obst = get_irg_obstack(irg);
-	res->attr.assem.pin_state          = op_pin_state_pinned;
-	res->attr.assem.input_constraints  = NEW_ARR_D(ir_asm_constraint, obst, arity);
-	res->attr.assem.output_constraints = NEW_ARR_D(ir_asm_constraint, obst, n_outs);
-	res->attr.assem.clobbers           = NEW_ARR_D(ident*,            obst, n_clobber);
-	res->attr.assem.text               = text;
+	asm_attr       *const a    = &res->attr.assem;
+	a->pin_state          = op_pin_state_pinned;
+	a->input_constraints  = NEW_ARR_D(ir_asm_constraint, obst, arity);
+	a->output_constraints = NEW_ARR_D(ir_asm_constraint, obst, n_outs);
+	a->clobbers           = NEW_ARR_D(ident*,            obst, n_clobber);
+	a->text               = text;
 
-	memcpy(res->attr.assem.input_constraints,  inputs,  sizeof(inputs[0]) * arity);
-	memcpy(res->attr.assem.output_constraints, outputs, sizeof(outputs[0]) * n_outs);
-	memcpy(res->attr.assem.clobbers, clobber, sizeof(clobber[0]) * n_clobber);
+	memcpy(a->input_constraints,  inputs,  sizeof(inputs[0])  * arity);
+	memcpy(a->output_constraints, outputs, sizeof(outputs[0]) * n_outs);
+	memcpy(a->clobbers,           clobber, sizeof(clobber[0]) * n_clobber);
 
 	verify_new_node(irg, res);
 	res = optimize_node(res);
@@ -109,8 +112,8 @@ static void try_remove_unnecessary_phi(ir_node *phi)
 {
 	ir_node *phi_value = NULL;
 
-	/* see if all inputs are either pointing to a single value or
-	 * are self references */
+	/* See if all inputs are either pointing to a single value or
+	 * are self references. */
 	foreach_irn_in(phi, i, in) {
 		if (in == phi)
 			continue;
@@ -126,12 +129,11 @@ static void try_remove_unnecessary_phi(ir_node *phi)
 		return;
 
 	/* if we're here then all phi inputs have been either phi_value
-	 * or self-references, we can replace the phi by phi_value.
-	 * We do this with an Id-node */
+	 * or self-references, we can replace the phi by phi_value. */
 	exchange(phi, phi_value);
 
 	/* recursively check phi_value, because it could be that we were the last
-	 * phi-node in a loop-body. Then our arguments is an unnecessary phi in
+	 * phi-node in a loop-body. Then our argument is an unnecessary phi in
 	 * the loop header which can be eliminated now */
 	if (is_Phi(phi_value)) {
 		try_remove_unnecessary_phi(phi_value);
@@ -151,12 +153,11 @@ static ir_node *set_phi_arguments(ir_node *phi, int pos)
 	int       arity        = get_irn_arity(block);
 	ir_node **in           = ALLOCAN(ir_node*, arity);
 	ir_mode  *mode         = get_irn_mode(phi);
-	int       i;
 
 	/* This loop goes to all predecessor blocks of the block the Phi node
-	   is in and there finds the operands of the Phi node by calling
-	   get_r_value_internal.  */
-	for (i = 0; i < arity; ++i) {
+	 * is in and there finds the operands of the Phi node by calling
+	 * get_r_value_internal.  */
+	for (int i = 0; i < arity; ++i) {
 		ir_node *cfgpred = get_Block_cfgpred_block(block, i);
 		ir_node *value;
 		if (cfgpred == NULL) {
@@ -188,14 +189,14 @@ static ir_node *set_phi_arguments(ir_node *phi, int pos)
  */
 static ir_node *get_r_value_internal(ir_node *block, int pos, ir_mode *mode)
 {
-	ir_node  *res = block->attr.block.graph_arr[pos];
-	ir_graph *irg = get_irn_irg(block);
+	ir_node *res = block->attr.block.graph_arr[pos];
 	if (res != NULL)
 		return res;
 
 	/* in a matured block we can immediately determine the phi arguments */
 	if (get_Block_matured(block)) {
-		int arity = get_irn_arity(block);
+		ir_graph *const irg   = get_irn_irg(block);
+		int       const arity = get_irn_arity(block);
 		/* no predecessors: use unknown value */
 		if (arity == 0) {
 			if (block == get_irg_start_block(irg)) {
@@ -208,20 +209,20 @@ static ir_node *get_r_value_internal(ir_node *block, int pos, ir_mode *mode)
 					res = new_r_Unknown(irg, mode);
 				}
 			} else {
-				/* unreachable block, use Bad */
-				res = new_r_Bad(irg, mode);
+				goto bad; /* unreachable block, use Bad */
 			}
 		/* one predecessor just use its value */
 		} else if (arity == 1) {
 			ir_node *cfgpred = get_Block_cfgpred(block, 0);
 			if (is_Bad(cfgpred)) {
+bad:
 				res = new_r_Bad(irg, mode);
 			} else {
 				ir_node *cfgpred_block = get_nodes_block(cfgpred);
 				res = get_r_value_internal(cfgpred_block, pos, mode);
 			}
-		/* multiple predecessors construct Phi */
 		} else {
+			/* multiple predecessors construct Phi */
 			res = new_rd_Phi0(NULL, block, mode, pos);
 			/* enter phi0 into our variable value table to break cycles
 			 * arising from set_phi_arguments */
@@ -241,49 +242,43 @@ static ir_node *get_r_value_internal(ir_node *block, int pos, ir_mode *mode)
 
 void mature_immBlock(ir_node *block)
 {
-	size_t   n_preds;
-	ir_node  *next;
-	ir_node  *phi;
-	ir_graph *irg;
-
 	if (get_Block_matured(block))
 		return;
 
-	irg     = get_irn_irg(block);
-	n_preds = ARR_LEN(block->in) - 1;
-	/* Fix block parameters */
-	block->attr.block.backedge = new_backedge_arr(get_irg_obstack(irg), n_preds);
+	ir_graph       *const irg     = get_irn_irg(block);
+	struct obstack *const obst    = get_irg_obstack(irg);
+	size_t          const n_preds = ARR_LEN(block->in) - 1;
+	/* Fix block parameters. */
+	block->attr.block.backedge = new_backedge_arr(obst, n_preds);
 
-	/* Traverse a chain of Phi nodes attached to this block and mature
-	these, too. */
-	for (phi = block->attr.block.phis; phi != NULL; phi = next) {
-		ir_node *new_value;
-		int      pos = phi->attr.phi.u.pos;
-
-		next = phi->attr.phi.next;
-		new_value = set_phi_arguments(phi, pos);
-		if (block->attr.block.graph_arr[pos] == phi) {
+	/* Traverse a chain of Phi nodes attached to this block and mature these,
+	 * too. */
+	for (ir_node *phi = block->attr.block.phis; phi != NULL;) {
+		ir_node *const next      = phi->attr.phi.next;
+		int      const pos       = phi->attr.phi.u.pos;
+		ir_node *const new_value = set_phi_arguments(phi, pos);
+		if (block->attr.block.graph_arr[pos] == phi)
 			block->attr.block.graph_arr[pos] = new_value;
-		}
+		phi = next;
 	}
 
 	set_Block_matured(block, 1);
 
-	/* create final in-array for the block */
+	/* Create final in-array for the block. */
 	if (block->attr.block.dynamic_ins) {
-		ir_node **const new_in = DUP_ARR_D(ir_node*, get_irg_obstack(irg), block->in);
+		ir_node **const new_in = DUP_ARR_D(ir_node*, obst, block->in);
 		DEL_ARR_F(block->in);
 		block->in = new_in;
 		block->attr.block.dynamic_ins = false;
 	}
 
 	/* Now, as the block is a finished Firm node, we can optimize it.
-	   Since other nodes have been allocated since the block was created
-	   we can not free the node on the obstack.  Therefore we have to call
-	   optimize_in_place().
-	   Unfortunately the optimization does not change a lot, as all allocated
-	   nodes refer to the unoptimized node.
-	   We can call optimize_in_place_2(), as global cse has no effect on blocks.
+	 * Since other nodes have been allocated since the block was created we can
+	 * not free the node on the obstack.  Therefore we have to call
+	 * optimize_in_place().
+	 * Unfortunately the optimization does not change a lot, as all allocated
+	 * nodes refer to the unoptimized node.
+	 * We can call optimize_in_place_2(), as global cse has no effect on blocks.
 	 */
 	verify_new_node(irg, block);
 	optimize_in_place_2(block);
@@ -314,17 +309,11 @@ ir_node *new_d_ASM(dbg_info *db, ir_node *mem, int arity, ir_node *in[],
 
 ir_node *new_rd_DivRL(dbg_info *dbgi, ir_node *block, ir_node * irn_mem, ir_node * irn_left, ir_node * irn_right, ir_mode* resmode, op_pin_state pin_state)
 {
-	ir_node *res;
-	ir_graph *irg = get_Block_irg(block);
-
-	ir_node *in[3];
-	in[0] = irn_mem;
-	in[1] = irn_left;
-	in[2] = irn_right;
-
-	res = new_ir_node(dbgi, irg, block, op_Div, mode_T, 3, in);
-	res->attr.div.resmode = resmode;
-	res->attr.div.no_remainder = 1;
+	ir_graph *const irg = get_Block_irg(block);
+	ir_node  *const in[] = { irn_mem, irn_left, irn_right };
+	ir_node        *res  = new_ir_node(dbgi, irg, block, op_Div, mode_T, 3, in);
+	res->attr.div.resmode       = resmode;
+	res->attr.div.no_remainder  = 1;
 	res->attr.div.exc.pin_state = pin_state;
 	verify_new_node(irg, res);
 	res = optimize_node(res);
@@ -338,10 +327,8 @@ ir_node *new_r_DivRL(ir_node *block, ir_node * irn_mem, ir_node * irn_left, ir_n
 
 ir_node *new_d_DivRL(dbg_info *dbgi, ir_node * irn_mem, ir_node * irn_left, ir_node * irn_right, ir_mode* resmode, op_pin_state pin_state)
 {
-	ir_node *res;
 	assert(irg_is_constrained(current_ir_graph, IR_GRAPH_CONSTRAINT_CONSTRUCTION));
-	res = new_rd_DivRL(dbgi, current_ir_graph->current_block, irn_mem, irn_left, irn_right, resmode, pin_state);
-	return res;
+	return new_rd_DivRL(dbgi, current_ir_graph->current_block, irn_mem, irn_left, irn_right, resmode, pin_state);
 }
 
 ir_node *new_DivRL(ir_node * irn_mem, ir_node * irn_left, ir_node * irn_right, ir_mode* resmode, op_pin_state pin_state)
@@ -351,17 +338,16 @@ ir_node *new_DivRL(ir_node * irn_mem, ir_node * irn_left, ir_node * irn_right, i
 
 ir_node *new_rd_immBlock(dbg_info *dbgi, ir_graph *irg)
 {
-	ir_node *res;
-
 	assert(irg_is_constrained(irg, IR_GRAPH_CONSTRAINT_CONSTRUCTION));
 	/* creates a new dynamic in-array as length of in is -1 */
-	res = new_ir_node(dbgi, irg, NULL, op_Block, mode_BB, -1, NULL);
+	ir_node *const res = new_ir_node(dbgi, irg, NULL, op_Block, mode_BB, -1, NULL);
 
 	set_Block_matured(res, 0);
-	res->attr.block.dynamic_ins = true;
-	res->attr.block.irg.irg     = irg;
-	res->attr.block.backedge    = NULL;
-	res->attr.block.entity      = NULL;
+	block_attr *const b = &res->attr.block;
+	b->dynamic_ins = true;
+	b->irg.irg     = irg;
+	b->backedge    = NULL;
+	b->entity      = NULL;
 
 	set_Block_block_visited(res, 0);
 
@@ -391,12 +377,11 @@ ir_node *new_immBlock(void)
 
 void add_immBlock_pred(ir_node *block, ir_node *jmp)
 {
-	int n = ARR_LEN(block->in) - 1;
-
 	assert(is_Block(block) && "Error: Must be a Block");
 	assert(!get_Block_matured(block) && "Error: Block already matured!\n");
 	assert(is_ir_node(jmp));
 
+	int const n = ARR_LEN(block->in) - 1;
 	ARR_APP1(ir_node *, block->in, jmp);
 	/* Call the hook */
 	hook_set_irn_n(block, n, jmp, NULL);
@@ -445,25 +430,20 @@ ir_node *get_value(int pos, ir_mode *mode)
  */
 static ir_mode *guess_recursively(ir_node *block, int pos)
 {
-	ir_node *value;
-	int      n_preds;
-	int      i;
-
 	if (irn_visited_else_mark(block))
 		return NULL;
 
 	/* already have a defintion -> we can simply look at its mode */
-	value = block->attr.block.graph_arr[pos];
+	ir_node *const value = block->attr.block.graph_arr[pos];
 	if (value != NULL)
 		return get_irn_mode(value);
 
 	/* now we try to guess, by looking at the predecessor blocks */
-	n_preds = get_irn_arity(block);
-	for (i = 0; i < n_preds; ++i) {
-		ir_node *pred_block = get_Block_cfgpred_block(block, i);
+	for (int i = 0, n_preds = get_irn_arity(block); i < n_preds; ++i) {
+		ir_node *const pred_block = get_Block_cfgpred_block(block, i);
 		if (pred_block == NULL)
 			continue;
-		ir_mode *mode = guess_recursively(pred_block, pos);
+		ir_mode *const mode = guess_recursively(pred_block, pos);
 		if (mode != NULL)
 			return mode;
 	}
@@ -474,17 +454,15 @@ static ir_mode *guess_recursively(ir_node *block, int pos)
 
 ir_mode *ir_r_guess_mode(ir_graph *irg, int pos)
 {
-	ir_node  *block = irg->current_block;
-	ir_node  *value = block->attr.block.graph_arr[pos+1];
-	ir_mode  *mode;
-
 	/* already have a defintion -> we can simply look at its mode */
+	ir_node *const block = irg->current_block;
+	ir_node *const value = block->attr.block.graph_arr[pos+1];
 	if (value != NULL)
 		return get_irn_mode(value);
 
 	ir_reserve_resources(irg, IR_RESOURCE_IRN_VISITED);
 	inc_irg_visited(irg);
-	mode = guess_recursively(block, pos+1);
+	ir_mode *const mode = guess_recursively(block, pos + 1);
 	ir_free_resources(irg, IR_RESOURCE_IRN_VISITED);
 
 	return mode;
@@ -499,7 +477,7 @@ void set_r_value(ir_graph *irg, int pos, ir_node *value)
 {
 	assert(irg_is_constrained(irg, IR_GRAPH_CONSTRAINT_CONSTRUCTION));
 	assert(pos >= 0);
-	assert(pos+1 < irg->n_loc);
+	assert(pos + 1 < irg->n_loc);
 	assert(is_ir_node(value));
 	irg->current_block->attr.block.graph_arr[pos + 1] = value;
 }
@@ -520,10 +498,8 @@ ir_node *get_store(void)
 	return get_r_store(current_ir_graph);
 }
 
-void set_r_store(ir_graph *irg, ir_node *store)
+void set_r_store(ir_graph *const irg, ir_node *store)
 {
-	ir_node *load, *pload, *pred, *in[2];
-
 	assert(irg_is_constrained(irg, IR_GRAPH_CONSTRAINT_CONSTRUCTION));
 	/* Beware: due to dead code elimination, a store might become a Bad node even in
 	   the construction phase. */
@@ -531,10 +507,9 @@ void set_r_store(ir_graph *irg, ir_node *store)
 
 	if (get_opt_auto_create_sync()) {
 		/* handle non-volatile Load nodes by automatically creating Sync's */
-		load = skip_Proj(store);
+		ir_node *const load = skip_Proj(store);
 		if (is_Load(load) && get_Load_volatility(load) == volatility_non_volatile) {
-			pred = get_Load_mem(load);
-
+			ir_node *const pred = get_Load_mem(load);
 			if (is_Sync(pred)) {
 				/* a Load after a Sync: move it up */
 				ir_node *mem = skip_Proj(get_Sync_pred(pred, 0));
@@ -543,14 +518,13 @@ void set_r_store(ir_graph *irg, ir_node *store)
 				add_Sync_pred(pred, store);
 				store = pred;
 			} else {
-				pload = skip_Proj(pred);
+				ir_node *const pload = skip_Proj(pred);
 				if (is_Load(pload) && get_Load_volatility(pload) == volatility_non_volatile) {
 					/* a Load after a Load: create a new Sync */
 					set_Load_mem(load, get_Load_mem(pload));
 
-					in[0] = pred;
-					in[1] = store;
-					store = new_r_Sync(irg->current_block, 2, in);
+					ir_node *in[] = { pred, store };
+					store = new_r_Sync(irg->current_block, ARRAY_SIZE(in), in);
 				}
 			}
 		}
@@ -610,21 +584,19 @@ ir_node *new_ASM(ir_node *mem, int arity, ir_node *in[],
 
 ir_node *new_r_Anchor(ir_graph *irg)
 {
-	ir_node *in[anchor_last+1];
-	ir_node *res;
-	size_t   i;
-	memset(in, 0, sizeof(in));
-	res = new_ir_node(NULL, irg, NULL, op_Anchor, mode_ANY, 0, NULL);
+	ir_node *const res = new_ir_node(NULL, irg, NULL, op_Anchor, mode_ANY, 0, NULL);
 	res->attr.anchor.irg.irg = irg;
 
 	/* hack to get get_irn_irg in set_irn_in() working */
 	res->in[0] = res;
 
 	/* we can't have NULL inputs so reference ourselves for now */
-	for (i = 0; i <= (size_t)anchor_last; ++i) {
+	int const arity = anchor_last + 1;
+	ir_node  *in[arity];
+	for (size_t i = 0; i < (size_t)arity; ++i) {
 		in[i] = res;
 	}
-	set_irn_in(res, anchor_last+1, in);
+	set_irn_in(res, arity, in);
 
 	return res;
 }
