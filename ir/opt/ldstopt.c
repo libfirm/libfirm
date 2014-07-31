36ebe0f17ffd53071d37a8f773fbaa4be474e1c4
Matthias Braun <matze@braunis.de>
2014-07-31 17:16:26 +0200

    ldstop: transfer keep-alive edge if necessary

--- ir/opt/ldstopt.c
+++ ir/opt/ldstopt.c
@@ -1310,7 +1310,9 @@ static changes_t optimize_phi(ir_node *phi, walk_env_t *wenv)
 	}
 
 	/* sixth step: replace old Phi */
-	remove_keep_alive(phi);
+	bool kept = remove_keep_alive(phi);
+	if (kept)
+		keep_alive(phiM);
 	exchange(phi, projM);
 
 	return res | DF_CHANGED;
