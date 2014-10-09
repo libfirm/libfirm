a70bbbe85b56926edca9a4f65a921881de446d4a
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2014-10-09 09:55:26 +0200

    Skip two levels of Proj nodes.
    This fixed opt/fehler274.c.

--- ir/ir/irnode_t.h
+++ ir/ir/irnode_t.h
@@ -402,7 +402,7 @@ static inline int is_irn_keep_(const ir_node *node)
 
 static inline int is_irn_start_block_placed_(const ir_node *node)
 {
-	const ir_node *const skip = skip_Proj_const(node);
+	const ir_node *const skip = skip_Proj_const(skip_Proj_const(node));
 	return is_op_start_block_placed(get_irn_op_(skip));
 }
 
