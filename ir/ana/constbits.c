d70cfa33a482c4b910502660e3211a1516e1c851
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2014-10-10 18:28:15 +0200

    Improved constant bits transfer function for Shrs.

--- ir/ana/constbits.c
+++ ir/ana/constbits.c
@@ -30,7 +30,7 @@
 #include "constbits.h"
 
 /* TODO:
- * - Implement cleared/set bit calculation for Div, Mod, Shrs
+ * - Implement cleared/set bit calculation for Div, Mod
  * - Implement min/max calculation for And, Eor, Or, Not, Conv, Shl, Shr, Shrs, Mux
  * - Implement min/max calculation for Add, Sub, Minus, Mul, Div, Mod, Conv, Shl, Shr, Shrs, Mux
  */
@@ -421,26 +421,45 @@ undefined:
 				}
 
 				case iro_Shrs: {
-					bitinfo   *const l  = get_bitinfo(get_Shrs_left(irn));
-					bitinfo   *const r  = get_bitinfo(get_Shrs_right(irn));
-					ir_tarval *const lz = l->z;
-					ir_tarval *const lo = l->o;
-					ir_tarval *const rz = r->z;
+					ir_node   *const right = get_Shrs_right(irn);
+					bitinfo   *const l     = get_bitinfo(get_Shrs_left(irn));
+					bitinfo   *const r     = get_bitinfo(right);
+					ir_tarval *const lz    = l->z;
+					ir_tarval *const lo    = l->o;
+					ir_tarval *const rz    = r->z;
+					ir_tarval *const ro    = r->o;
 					if (rz == r->o) {
 						z = tarval_shrs(lz, rz);
 						o = tarval_shrs(lo, rz);
 					} else {
-						int high = get_tarval_highest_bit(lz);
-						if (high >= 0) {
-							ir_tarval *one  = get_mode_one(m);
-							ir_tarval *shl  = tarval_shl_unsigned(one, high);
-							ir_tarval *mask = tarval_or(shl, tarval_sub(shl, one, m));
-							z = tarval_or(lz, mask);
-							o = tarval_andnot(lo, mask);
-						} else {
-							z = lz;
-							o = lo;
+						const long        size_bits     = get_mode_size_bits(m);
+						const long        modulo_shift  = get_mode_modulo_shift(m);
+						ir_mode    *const rmode         = get_irn_mode(right);
+						ir_tarval  *const rone          = get_mode_one(rmode);
+						ir_tarval  *const size_mask     = tarval_sub(new_tarval_from_long(size_bits, rmode), rone, NULL);
+						ir_tarval  *const modulo_mask   = tarval_sub(new_tarval_from_long(modulo_shift, rmode), rone, NULL);
+						ir_tarval  *const zero          = get_mode_null(m);
+						ir_tarval  *const all_one       = get_mode_all_one(m);
+						ir_tarval  *const oversize_mask = tarval_andnot(modulo_mask, size_mask);
+
+						z = zero;
+						o = tarval_is_null(tarval_and(rz, oversize_mask)) ? all_one : zero;
+
+						if (tarval_is_null(tarval_and(ro, oversize_mask))) {
+							ir_tarval *const rmask  = tarval_and(size_mask, modulo_mask);
+							ir_tarval *const rsure  = tarval_and(tarval_not(tarval_eor(ro, rz)), rmask);
+							ir_tarval *const rbound = tarval_add(rmask, rone);
+							ir_tarval *const rzero  = get_mode_null(rmode);
+							for (ir_tarval *shift_amount = rzero; shift_amount != rbound; shift_amount = tarval_add(shift_amount, rone)) {
+								if (tarval_is_null(tarval_and(rsure, tarval_eor(shift_amount, rz)))) {
+									z = tarval_or(z, tarval_shrs(lz, shift_amount));
+									o = tarval_and(o, tarval_shrs(lo, shift_amount));
+								}
+							}
 						}
+
+						/* Ensure that we do not create undefined bit information. */
+						assert(!tarval_is_null(z) || !tarval_is_all_one(o));
 					}
 					break;
 				}
