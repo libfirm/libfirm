cc70ccb0309228a914cbd8fe20196053bd548a4f
Christoph Mallon <mallon@cs.uni-saarland.de>
2014-10-26 10:43:27 +0100

    all: Reduce license headers, which were missed before.

--- ir/be/scripts/generate_new_opcodes.pl
+++ ir/be/scripts/generate_new_opcodes.pl
@@ -1,22 +1,8 @@
 #!/usr/bin/perl -w
 
 #
-# Copyright (C) 1995-2008 University of Karlsruhe.  All right reserved.
-#
 # This file is part of libFirm.
-#
-# This file may be distributed and/or modified under the terms of the
-# GNU General Public License version 2 as published by the Free Software
-# Foundation and appearing in the file LICENSE.GPL included in the
-# packaging of this file.
-#
-# Licensees holding valid libFirm Professional Edition licenses may use
-# this file in accordance with the libFirm Commercial License.
-# Agreement provided with the Software.
-#
-# This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-# WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE.
+# Copyright (C) 2014 University of Karlsruhe.
 #
 
 # This script generates the C code which creates the irop's and
