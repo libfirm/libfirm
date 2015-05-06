7a43347c6f027bc8c9a9bb192dbcc3334a7c3f13
Matthias Braun <matze@braunis.de>
2015-05-06 01:18:45 -0700

    Sort module initializers

--- ir/be/bemodule.c
+++ ir/be/bemodule.c
@@ -24,41 +24,41 @@ struct be_module_list_entry_t {
 	struct be_module_list_entry_t *next; /**< Points to the next entry. */
 };
 
-void be_init_sched(void);
+void be_init_arch_TEMPLATE(void);
+void be_init_arch_amd64(void);
+void be_init_arch_arm(void);
+void be_init_arch_ia32(void);
+void be_init_arch_sparc(void);
 void be_init_blocksched(void);
-void be_init_spill(void);
-void be_init_spilloptions(void);
-void be_init_listsched(void);
-void be_init_sched_rand(void);
-void be_init_sched_normal(void);
-void be_init_sched_trivial(void);
 void be_init_chordal(void);
-void be_init_pbqp_coloring(void);
-void be_init_chordal_main(void);
 void be_init_chordal_common(void);
-void be_init_copyopt(void);
+void be_init_chordal_main(void);
 void be_init_copyheur4(void);
+void be_init_copyilp(void);
 void be_init_copyilp2(void);
 void be_init_copynone(void);
+void be_init_copyopt(void);
 void be_init_copystat(void);
 void be_init_daemelspill(void);
 void be_init_dwarf(void);
-void be_init_arch_ia32(void);
-void be_init_arch_arm(void);
-void be_init_arch_amd64(void);
-void be_init_arch_sparc(void);
-void be_init_arch_TEMPLATE(void);
-void be_init_copyilp(void);
-void be_init_peephole(void);
-void be_init_ra(void);
-void be_init_spillbelady(void);
-void be_init_ssaconstr(void);
-void be_init_pref_alloc(void);
-void be_init_loopana(void);
-void be_init_spillslots(void);
+void be_init_listsched(void);
 void be_init_live(void);
-void be_init_state(void);
+void be_init_loopana(void);
 void be_init_pbqp(void);
+void be_init_pbqp_coloring(void);
+void be_init_peephole(void);
+void be_init_pref_alloc(void);
+void be_init_ra(void);
+void be_init_sched(void);
+void be_init_sched_normal(void);
+void be_init_sched_rand(void);
+void be_init_sched_trivial(void);
+void be_init_spill(void);
+void be_init_spillbelady(void);
+void be_init_spilloptions(void);
+void be_init_spillslots(void);
+void be_init_ssaconstr(void);
+void be_init_state(void);
 
 void be_quit_copystat(void);
 void be_quit_pbqp(void);
@@ -70,18 +70,32 @@ void be_quit_pbqp(void);
 void be_init_modules(void)
 {
 	static bool run_once = false;
-
 	if (run_once)
 		return;
 	run_once = true;
 
-	be_init_loopana();
-	be_init_live();
-	be_init_spillslots();
-	be_init_sched();
 	be_init_blocksched();
+	be_init_chordal_common();
+	be_init_copyopt();
+	be_init_copystat();
+	be_init_dwarf();
+	be_init_live();
+	be_init_loopana();
+	be_init_peephole();
+	be_init_ra();
+	be_init_sched();
 	be_init_spill();
 	be_init_spilloptions();
+	be_init_spillslots();
+	be_init_ssaconstr();
+	be_init_state();
+
+	/* in the following groups the first one is the default */
+	be_init_arch_ia32();
+	be_init_arch_arm();
+	be_init_arch_sparc();
+	be_init_arch_amd64();
+	be_init_arch_TEMPLATE();
 
 	be_init_listsched();
 	be_init_sched_normal();
@@ -89,29 +103,17 @@ void be_init_modules(void)
 	be_init_sched_trivial();
 
 	be_init_chordal_main();
-	be_init_chordal_common();
+	be_init_pref_alloc();
+
 	be_init_chordal();
-	be_init_copyopt();
-	be_init_copyheur4();
-	be_init_copyilp2();
 	be_init_pbqp_coloring();
-	be_init_copynone();
-	be_init_copystat();
-	be_init_peephole();
-	be_init_ra();
+
 	be_init_spillbelady();
 	be_init_daemelspill();
-	be_init_dwarf();
-	be_init_ssaconstr();
-	be_init_pref_alloc();
-	be_init_state();
 
-	be_init_arch_ia32();
-	be_init_arch_arm();
-	be_init_arch_sparc();
-	be_init_arch_amd64();
-	be_init_arch_TEMPLATE();
-
+	be_init_copyheur4();
+	be_init_copyilp2();
+	be_init_copynone();
 	be_init_copyilp();
 
 #ifdef FIRM_GRGEN_BE
