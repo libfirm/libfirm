4035bf2014a57ba929090ceaf42decc1641964ca
Matthias Braun <matze@braunis.de>
2015-12-05 22:55:20 -0800

    fix/improve make_release.sh

--- support/make_release.sh
+++ support/make_release.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
-
 set -eu
+
+if ! test -e .git; then
+	echo "Must be started from toplevel libfirm dir"
+	exit 1
+fi
+# Check that our git checkout is clean (remember that we use git archive
+# which will miss things uncommitted changes)
+if [ "$(git status --porcelain)" != "" ]; then
+	echo "Git checkout not clean!"
+	exit 1
+fi
 
 WORKDIR="release"
 VERSION_MAJOR="1"
 VERSION_MINOR="22"
 VERSION_MICRO="0"
 VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO}"
-RELEASEDIR="libfirm-$VERSION"
-FULLRELEASEDIR="$WORKDIR/$RELEASEDIR"
-RELEASEFILE="libfirm-$VERSION.tar.bz2"
-VERSIONFILE="../ir/common/version.h"
+RELEASEFILE="build/libfirm-$VERSION.tar.bz2"
+VERSIONFILE="ir/common/version.h"
 
 # test if versions match
-echo "Checking for version mismatch"
+echo "Checking version in $VERSIONFILE"
 egrep -q "#define libfirm_VERSION_MAJOR\\s*$VERSION_MAJOR" $VERSIONFILE
 egrep -q "#define libfirm_VERSION_MINOR\\s*$VERSION_MINOR" $VERSIONFILE
 egrep -q "#define libfirm_VERSION_MICRO\\s*$VERSION_MICRO" $VERSIONFILE
+echo "Checking version in CMakeLists.txt"
+grep -q "set(libfirm_VERSION \"${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO}\")" CMakeLists.txt
+echo "Checking version in NEWS.md"
+grep -q "$VERSION_MAJOR.$VERSION_MINOR.$VERSION_MICRO" NEWS.md
 
-rm -rf "$FULLRELEASEDIR"
 echo "Creating $RELEASEFILE"
-git archive HEAD | bzip2 > "$RELEASEFILE"
+git archive --prefix libfirm-$VERSION/ HEAD | bzip2 > "$RELEASEFILE"
