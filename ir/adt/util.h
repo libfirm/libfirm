ef64d1056826f160f309916bcb5d17803b814468
Christoph Mallon <christoph.mallon@gmx.de>
2015-12-08 12:29:36 +0100

    util: Add 'strstart()'.

--- ir/adt/util.h
+++ ir/adt/util.h
@@ -69,4 +69,13 @@ static inline bool streq(char const *const a, char const *const b)
 	return strcmp(a, b) == 0;
 }
 
+static inline char const* strstart(char const* str, char const* start)
+{
+	do {
+		if (*start == '\0')
+			return str;
+	} while (*str++ == *start++);
+	return NULL;
+}
+
 #endif
