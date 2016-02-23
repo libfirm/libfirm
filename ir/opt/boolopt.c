fa7eb6eee26542375a5cc5a02fd7ab060cb86389
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2016-02-23 23:15:23 +0100

    Check for equal modes before subtracting them.
    This fixes opt/boolopt4.c.

--- ir/opt/boolopt.c
+++ ir/opt/boolopt.c
@@ -178,7 +178,7 @@ static ir_node *bool_and(cond_pair* const cpair, ir_node *dst_block)
 	           (rel_hi == ir_relation_equal || rel_hi == ir_relation_greater_equal || rel_hi == ir_relation_greater)) {
 		/* x >=|>|!= lo && x ==|>=|> hi ==> x ==|>=|> hi */
 		return cmp_hi;
-	} else if (tarval_is_one(tarval_sub(tv_hi, tv_lo))) { /* lo + 1 == hi */
+	} else if (get_tarval_mode(tv_hi) == get_tarval_mode(tv_lo) && tarval_is_one(tarval_sub(tv_hi, tv_lo))) { /* lo + 1 == hi */
 		if (rel_lo == ir_relation_greater_equal && rel_hi == ir_relation_less) {
 			/* x >= c && x < c + 1 ==> x == c */
 			return make_Cmp(dst_block, cmp_lo, ir_relation_equal);
@@ -309,7 +309,7 @@ static ir_node *bool_or(cond_pair *const cpair, ir_node *dst_block)
 	           (rel_hi == ir_relation_equal || rel_hi == ir_relation_greater_equal || rel_hi == ir_relation_greater)) {
 		/* x >=|>|!= lo || x ==|>=|> hi ==> x >=|>|!= lo */
 		return cmp_lo;
-	} else if (tarval_is_one(tarval_sub(tv_hi, tv_lo))) { /* lo + 1 == hi */
+	} else if (get_tarval_mode(tv_hi) == get_tarval_mode(tv_lo) && tarval_is_one(tarval_sub(tv_hi, tv_lo))) { /* lo + 1 == hi */
 		if (rel_lo == ir_relation_less && rel_hi == ir_relation_greater_equal) {
 			/* x < c || x >= c + 1 ==> x != c */
 			return make_Cmp(dst_block, cmp_lo, ir_relation_less_greater);
