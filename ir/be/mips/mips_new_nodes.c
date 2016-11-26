24e1a0d570b30c365c2744b83145a9a10a28cb56
Christoph Mallon <christoph.mallon@gmx.de>
2016-11-26 12:24:34 +0100

    mips: Handle 'Div'.
    Now 'int f(int a, int b) { return a / b; }' can be translated.

--- ir/be/mips/mips_new_nodes.c
+++ ir/be/mips/mips_new_nodes.c
@@ -110,6 +110,8 @@ void mips_dump_node(FILE *const F, ir_node const *const n, dump_reason_t const r
 			case iro_mips_addu:
 			case iro_mips_and:
 			case iro_mips_b:
+			case iro_mips_div_lo:
+			case iro_mips_divu_lo:
 			case iro_mips_last:
 			case iro_mips_mult_lo:
 			case iro_mips_nor:
