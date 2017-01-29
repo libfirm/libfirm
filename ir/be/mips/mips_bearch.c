libfirm/libfirm

commit acf1a376620890041b2a637a79a46da6700012d6

irarch: Leave setup to backends
Let backends perform the irarch setup in lower_for_target(). Frontends
do not need to explicitely enable it any longer and there is no need to
keep the settings around in backend_params.

ir/be/mips/mips_bearch.c
@@ -13,7 +13,7 @@
 #include "betranshlp.h"
 #include "gen_mips_new_nodes.h"
 #include "gen_mips_regalloc_if.h"
-#include "irarch_t.h"
+#include "irarch.h"
 #include "iredges.h"
 #include "irgwalk.h"
 #include "irprog_t.h"
@@ -50,7 +50,6 @@ static backend_params mips_backend_params = {
 	.pic_supported                 = false,
 	.unaligned_memaccess_supported = false,
 	.modulo_shift                  = MIPS_MACHINE_SIZE,
-	.dep_param                     = &mips_arch_dep,
 	.allow_ifconv                  = &mips_is_mux_allowed,
 	.machine_size                  = MIPS_MACHINE_SIZE,
 	.mode_float_arithmetic         = NULL,  /* will be set later */ // TODO
@@ -273,6 +272,9 @@ static void mips_generate_code(FILE *const output, char const *const cup_name)
 
 static void mips_lower_for_target(void)
 {
+	ir_arch_lower(&mips_arch_dep);
+	be_after_irp_transform("lower-arch-dep");
+
 	lower_calls_with_compounds(LF_RETURN_HIDDEN | LF_DONT_LOWER_ARGUMENTS, NULL);
 	be_after_irp_transform("lower-calls");
 

include/libfirm/be.h
@@ -12,7 +12,6 @@
 #define FIRM_BE_H
 
 #include <stdio.h>
-#include "irarch.h"
 #include "iroptimize.h"
 #include "irmode.h"
 #include "begin.h"
@@ -103,9 +102,6 @@ typedef struct backend_params {
 	 * On an architecture without modulo_shift this value is 0.
 	 */
 	unsigned modulo_shift;
-
-	/** Settings for architecture dependent optimizations. */
-	const ir_settings_arch_dep_t *dep_param;
 
 	/** Backend settings for if-conversion. */
 	arch_allow_ifconv_func allow_ifconv;

include/libfirm/firm.h
@@ -77,7 +77,6 @@
 #include "firm_types.h"
 #include "heights.h"
 #include "ident.h"
-#include "irarch.h"
 #include "ircgopt.h"
 #include "irconsconfirm.h"
 #include "ircons.h"

include/libfirm/irarch.h
@@ -1,46 +1,0 @@
-/*
- * This file is part of libFirm.
- * Copyright (C) 2012 University of Karlsruhe.
- */
-
-/**
- * @file
- * @brief  Some machine dependent optimizations.
- * @date   1.10.2004
- * @author Sebastian Hack
- */
-#ifndef FIRM_IR_IRARCH_H
-#define FIRM_IR_IRARCH_H
-
-#include "firm_types.h"
-#include "begin.h"
-
-/**
- * @addtogroup iroptimize
- * @{
- */
-
-/**
- * Optimization flags.
- */
-typedef enum arch_dep_opts_t {
-	arch_dep_none         = 0,
-	arch_dep_mul_to_shift = 1u << 0,  /**< optimize Mul into Shift/Add/Sub */
-	arch_dep_div_by_const = 1u << 1,  /**< optimize Div into Shift/Add/Mulh */
-	arch_dep_mod_by_const = 1u << 2   /**< optimize Mod into Shift/Add/Mulh */
-} arch_dep_opts_t;
-ENUM_BITSET(arch_dep_opts_t)
-
-typedef struct ir_settings_arch_dep_t ir_settings_arch_dep_t;
-
-/**
- * Sets the optimizations that shall be applied.
- * @param opts  An optimization bit mask.
- */
-FIRM_API void arch_dep_set_opts(arch_dep_opts_t opts);
-
-/** @} */
-
-#include "end.h"
-
-#endif

ir/be/TEMPLATE/TEMPLATE_bearch.c
@@ -147,7 +147,6 @@ static const backend_params *TEMPLATE_get_backend_params(void)
 		.pic_supported                 = false,
 		.unaligned_memaccess_supported = false,
 		.modulo_shift                  = 32,
-		.dep_param                     = NULL,
 		.allow_ifconv                  = TEMPLATE_is_mux_allowed,
 		.machine_size                  = 32,
 		.mode_float_arithmetic         = NULL,

ir/be/amd64/amd64_bearch.c
@@ -24,7 +24,7 @@
 #include "beutil.h"
 #include "debug.h"
 #include "gen_amd64_regalloc_if.h"
-#include "irarch_t.h"
+#include "irarch.h"
 #include "ircons.h"
 #include "iredges_t.h"
 #include "irgmod.h"
@@ -685,8 +685,24 @@ static void amd64_generate_code(FILE *output, const char *cup_name)
 	pmap_destroy(amd64_constants);
 }
 
+static const ir_settings_arch_dep_t amd64_arch_dep = {
+	.replace_muls         = true,
+	.replace_divs         = true,
+	.replace_mods         = true,
+	.allow_mulhs          = true,
+	.allow_mulhu          = true,
+	.also_use_subs        = true,
+	.maximum_shifts       = 4,
+	.highest_shift_amount = 63,
+	.evaluate             = NULL,
+	.max_bits_for_mulh    = 32,
+};
+
 static void amd64_lower_for_target(void)
 {
+	ir_arch_lower(&amd64_arch_dep);
+	be_after_irp_transform("lower_arch-dep");
+
 	/* lower compound param handling */
 	lower_calls_with_compounds(LF_RETURN_HIDDEN, NULL);
 	be_after_irp_transform("lower-calls");
@@ -736,23 +752,12 @@ static int amd64_is_mux_allowed(ir_node *sel, ir_node *mux_false,
 	return false;
 }
 
-static const ir_settings_arch_dep_t amd64_arch_dep = {
-	.also_use_subs        = true,
-	.maximum_shifts       = 4,
-	.highest_shift_amount = 63,
-	.evaluate             = NULL,
-	.allow_mulhs          = true,
-	.allow_mulhu          = true,
-	.max_bits_for_mulh    = 32,
-};
-
 static backend_params amd64_backend_params = {
 	.experimental                  = "the amd64 backend is highly experimental and unfinished (consider the ia32 backend)",
 	.byte_order_big_endian         = false,
 	.pic_supported                 = true,
 	.unaligned_memaccess_supported = true,
 	.modulo_shift                  = 32,
-	.dep_param                     = &amd64_arch_dep,
 	.allow_ifconv                  = amd64_is_mux_allowed,
 	.machine_size                  = 64,
 	.mode_float_arithmetic         = NULL,  /* will be set later */

ir/be/arm/arm_bearch.c
@@ -23,7 +23,7 @@
 #include "besched.h"
 #include "betranshlp.h"
 #include "gen_arm_regalloc_if.h"
-#include "irarch_t.h"
+#include "irarch.h"
 #include "irgopt.h"
 #include "irgwalk.h"
 #include "irprog_t.h"
@@ -217,8 +217,24 @@ static int arm_is_mux_allowed(ir_node *sel, ir_node *mux_false,
 	return false;
 }
 
+static const ir_settings_arch_dep_t arm_arch_dep = {
+	.replace_muls         = true,
+	.replace_divs         = true,
+	.replace_mods         = true,
+	.allow_mulhs          = false,
+	.allow_mulhu          = false,
+	.also_use_subs        = true,
+	.maximum_shifts       = 1,
+	.highest_shift_amount = 31,
+	.evaluate             = NULL,
+	.max_bits_for_mulh    = ARM_MACHINE_SIZE,
+};
+
 static void arm_lower_for_target(void)
 {
+	ir_arch_lower(&arm_arch_dep);
+	be_after_irp_transform("lower-arch-dep");
+
 	/* lower compound param handling */
 	lower_calls_with_compounds(LF_RETURN_HIDDEN, NULL);
 	be_after_irp_transform("lower-calls");
@@ -250,22 +266,12 @@ static void arm_lower_for_target(void)
 	be_after_irp_transform("lower-64");
 }
 
-static const ir_settings_arch_dep_t arm_arch_dep = {
-	.also_use_subs        = true,
-	.maximum_shifts       = 1,
-	.highest_shift_amount = 31,
-	.evaluate             = NULL,
-	.allow_mulhs          = false,
-	.allow_mulhu          = false,
-	.max_bits_for_mulh    = ARM_MACHINE_SIZE,
-};
 static backend_params arm_backend_params = {
 	.experimental                  = "the arm backend is highly experimental and unfinished",
 	.byte_order_big_endian         = false,
 	.pic_supported                 = false,
 	.unaligned_memaccess_supported = false,
 	.modulo_shift                  = ARM_MODULO_SHIFT,
-	.dep_param                     = &arm_arch_dep,
 	.allow_ifconv                  = arm_is_mux_allowed,
 	.machine_size                  = ARM_MACHINE_SIZE,
 	.mode_float_arithmetic         = NULL,

ir/be/ia32/ia32_architecture.h
@@ -14,7 +14,7 @@
 #include <stdbool.h>
 
 #include "firm_types.h"
-#include "irarch_t.h"
+#include "irarch.h"
 
 typedef struct {
 	/** optimize for size */

commit 78a5cf6805cf3f85005926b177f48c33adcdddd9

mips: Need to set replace_xxx in irarch now
Hopefully this fixes the fallout from acf1a376620890041b2a637a79a46da6700012d6

ir/be/mips/mips_bearch.c
@@ -35,12 +35,15 @@ static int mips_is_mux_allowed(ir_node *const sel, ir_node *const mux_false, ir_
 }
 
 static ir_settings_arch_dep_t const mips_arch_dep = {
+	.replace_muls         = true,
+	.replace_divs         = true,
+	.replace_mods         = true,
+	.allow_mulhs          = true,
+	.allow_mulhu          = true,
 	.also_use_subs        = true,
 	.maximum_shifts       = 4,
 	.highest_shift_amount = MIPS_MACHINE_SIZE - 1,
 	.evaluate             = NULL,
-	.allow_mulhs          = true,
-	.allow_mulhu          = true,
 	.max_bits_for_mulh    = MIPS_MACHINE_SIZE,
 };
 