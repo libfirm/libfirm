65da1032822ab2955f44be4e8543da23562f5fa4
Elias Aebi <elias.aebi@student.kit.edu>
2018-01-07 21:16:44 +0100

    mark newly added phi nodes as visited since we don't want to add phi nodes for the phi nodes we added

--- ir/opt/loop2.c
+++ ir/opt/loop2.c
@@ -27,6 +27,7 @@ static ir_node *insert_phi(ir_node *const node, int const n, ir_node *const bloc
 	ir_node *const phi = new_r_Phi(block, block_arity, in, mode);
 	set_optimize(opt);
 	set_irn_n(node, n, phi);
+	mark_irn_visited(phi);
 	DB((dbg, LEVEL_3, "inserting phi %ld\n", get_irn_node_nr(phi)));
 	return phi;
 }
