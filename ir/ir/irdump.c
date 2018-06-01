1cf3fc34e7fb69d4a3a11f195c179efd9fafcd45
Christoph Mallon <christoph.mallon@gmx.de>
2018-06-01 17:24:33 +0200

    dump: For coloring nodes in the graph dump prioritize control flow over memory.
    This matters especially in the backend when there is a jump instruction which also accesses memory, e.g. IJmp on ia32.

--- ir/ir/irdump.c
+++ ir/ir/irdump.c
@@ -858,10 +858,10 @@ static void dump_node_vcgattr(FILE *F, const ir_node *node, const ir_node *local
 	ir_op *op = get_irn_op(n);
 	if (is_op_constlike(op)) {
 		print_vcg_color(F, ird_color_const);
-	} else if (is_op_uses_memory(op)) {
-		print_vcg_color(F, ird_color_memory);
 	} else if (is_op_cfopcode(op) || is_op_forking(op)) {
 		print_vcg_color(F, ird_color_controlflow);
+	} else if (is_op_uses_memory(op)) {
+		print_vcg_color(F, ird_color_memory);
 	}
 }
 
