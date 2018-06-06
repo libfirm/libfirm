4333c69a5188c5817d27fd6045e374e969bdcafd
Andreas Fried <andreas.fried@kit.edu>
2018-06-06 10:33:24 +0200

    Remove definition of Slice and Pack from vhdlops.c.
    Both are already declared in ir_spec.py.

--- ir/opt/firm2vhdl/vhdlops.c
+++ ir/opt/firm2vhdl/vhdlops.c
@@ -8,31 +8,7 @@
 
 #include <firm.h>
 
-static ir_op *op_Slice;
-static ir_op *op_Pack;
-
-typedef struct slice_attr {
-  char first;
-  char last;
-} slice_attr;
-
 void create_vhdl_opcodes(void)
 {
-  if (! op_Slice)
-    op_Slice = new_ir_op(get_next_ir_opcode(),
-			 "Slice",
-			 op_pin_state_floats,
-			 irop_flag_none,
-			 oparity_any,
-			 0,
-			 sizeof(slice_attr));
-
-  if (! op_Pack)
-    op_Pack = new_ir_op(get_next_ir_opcode(),
-			 "Pack",
-			 op_pin_state_floats,
-			 irop_flag_none,
-			 oparity_dynamic,
-			 0,
-			 0);
+	// Declared in ir_spec
 }
