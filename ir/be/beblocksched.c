39c8578861622d47c35247a29a3528af77d31df4
Christoph Breisacher <christoph.breisacher@student.kit.edu>
2019-02-12 12:16:25 +0100

    Add ExtTSP block scheduling algorithm.

--- ir/be/beblocksched.c
+++ ir/be/beblocksched.c
@@ -30,12 +30,15 @@
 #include "besched.h"
 #include "debug.h"
 #include "execfreq.h"
+#include "execfreq_t.h"
 #include "iredges_t.h"
 #include "irgmod.h"
 #include "irtools.h"
 #include "irgwalk.h"
 #include "irnode_t.h"
+#include <math.h>
 #include "pdeq.h"
+#include "irprintf.h"
 #include <sys/time.h>
 #include "util.h"
 
@@ -530,7 +533,6 @@ static ir_node **create_blocksched_array(blocksched_env_t *const env)
 	for (blocksched_entry_t const *entry = first; entry; entry = entry->next) {
 		assert(i < count);
 		block_list[i++] = entry->block;
-		DB((dbg, LEVEL_1, "\t%+F\n", entry->block));
 	}
 	assert(i == count);
 
@@ -606,12 +608,425 @@ static ir_node **be_create_random_block_schedule(ir_graph *irg)
 		block_list[j] = block_list[i];
 		block_list[i] = t;
 	}
-	// debug output
-	DB((dbg, LEVEL_1, "Random blockschedule:\n"));
-	DB((dbg, LEVEL_1, "(start: %+F, end: %+F)\n", start_block, end_block));
-	for (size_t i=0; i < block_count; i++) {
-		DB((dbg, LEVEL_1, "\t%+F\n", block_list[i]));
+	return block_list;
+}
+
+static unsigned int get_edgefreq_int(ir_execfreq_int_factors *factors,
+	ir_node *dest, const ir_edge_t *edge)
+{
+	int int_freq = get_block_execfreq_int(factors, dest);
+	double prob = get_edge_probability(edge);
+	return (unsigned int) round(int_freq * prob);
+}
+
+// TODO replace with better size estimation
+static unsigned int get_block_size(ir_node *block)
+{
+	// try to estimate block size in final binary
+	int size = 0;
+	sched_foreach(block, node) {
+		size += 4; // 32 bit per node
 	}
+	return size;
+}
+
+// ****************************************************************************
+// TODO remove graphviz debug code
+// ****************************************************************************
+static ir_execfreq_int_factors *execfreq_factors_graphviz;
+static FILE *dump_file_graphviz;
+
+static void print_block_graphviz(ir_node *block, void *env)
+{
+	int int_freq = get_block_execfreq_int(execfreq_factors_graphviz, block);
+	ir_fprintf(dump_file_graphviz,
+		"\t\"%+F\" [label=\"%+F\\n(freq:%f, int:%d)\\n size: %d bytes %s %s\"]\n",
+		block, block, get_block_execfreq(block),
+		int_freq,
+		get_block_size(block),
+		get_irg_start_block((ir_graph*)env)==block ? "\\nSTART" : "",
+		get_irg_end_block((ir_graph*)env)==block ? "\\nEND" : "");
+	foreach_block_succ(block, edge) {
+		ir_node *succ = get_edge_src_irn(edge);
+		ir_fprintf(dump_file_graphviz, "\t\"%+F\" -> \"%+F\"[label=\"p:%f\\n f:%f\\n%d\"]\n",
+			block, succ, get_edge_probability(edge),
+			get_block_execfreq(block)*get_edge_probability(edge),
+			get_edgefreq_int(execfreq_factors_graphviz, block, edge));
+	}
+}
+
+static void dump_cfg_graphviz(ir_graph *irg)
+{
+	char file[64];
+	ir_snprintf(file, 64, "%F-cfg-dump.gv", irg);
+	dump_file_graphviz = fopen(file, "w");
+	if (dump_file_graphviz) {
+		ir_fprintf(dump_file_graphviz, "digraph {\n");
+		irg_block_walk_graph(irg, print_block_graphviz, NULL, (void*)irg);
+		ir_fprintf(dump_file_graphviz, "}\n");
+		fclose(dump_file_graphviz);
+	}
+}
+// ****************************************************************************
+
+/*
+ * Data structures used by the ExtTSP algorithm.
+ */
+typedef double exttsp_score_t;
+typedef struct exttsp_edge_t exttsp_edge_t;
+typedef struct exttsp_block_t exttsp_block_t;
+typedef struct exttsp_chain_t exttsp_chain_t;
+typedef struct exttsp_env_t exttsp_env_t;
+
+struct exttsp_edge_t {
+	exttsp_block_t *src;       // cfg source of edge
+	exttsp_block_t *dest;      // cfg destination of edge
+	exttsp_edge_t *next;       // next edge of corresponding block
+	unsigned int freq;         // frequency of edge
+};
+
+struct exttsp_block_t {
+	unsigned int size;         // code size in bytes
+	unsigned int addr;         // address of block in memory relative to chain
+	exttsp_block_t *succ;      // next block in chain
+	exttsp_edge_t *first_edge; // first outgoing cfg edge
+	exttsp_chain_t *chain;     // chain the block is in
+	ir_node *irn;              // corresponding node
+};
+
+struct exttsp_chain_t {
+	exttsp_block_t *first;     // first block of chain
+	exttsp_block_t *last;      // last block of chain
+	unsigned int length;       // number of blocks in chain
+	exttsp_score_t score;      // cached score of the chain
+	bool contains_start;       // if the chain begins with the start block
+};
+
+struct exttsp_env_t {          // data needed by graph walkers
+	ir_graph *irg;
+	struct obstack obst;
+	ir_execfreq_int_factors execfreq_factors;
+	exttsp_chain_t *chains;
+	unsigned int chain_count;
+	exttsp_block_t *blocks;
+	unsigned int block_count;
+	exttsp_edge_t *edges;
+	unsigned int edge_count;
+};
+
+
+/*
+ * Constants for the ExtTSP algorithm.
+ */
+const double FALLTHROUGH_WEIGHT      = 1.0;
+const double FORWARD_WEIGHT          = 0.1;
+const double BACKWARD_WEIGHT         = 0.1;
+const unsigned int FORWARD_DISTANCE  = 1024;
+const unsigned int BACKWARD_DISTANCE = 640;
+
+/*
+ * Calculate the contribution of an edge to the ExtTSP score of a chain.
+ */
+static exttsp_score_t exttsp_rate_edge(exttsp_edge_t *edge)
+{
+	unsigned int jump_addr = edge->src->addr + edge->src->size;
+	unsigned int dest_addr = edge->dest->addr;
+	unsigned int freq = edge->freq;
+	// fallthrough edge
+	if (jump_addr == dest_addr) {
+		return freq * FALLTHROUGH_WEIGHT;
+	}
+	// forward edge
+	else if (jump_addr < dest_addr) {
+		unsigned int distance = dest_addr - jump_addr;
+		if (distance <= FORWARD_DISTANCE) {
+			double probability = 1.0 - ((double)distance)/((double)FORWARD_DISTANCE);
+			return freq * FORWARD_WEIGHT * probability;
+		}
+		return 0;
+	}
+	// backward edge
+	unsigned int distance = jump_addr - dest_addr;
+	if (distance <= BACKWARD_DISTANCE) {
+		double probability = 1.0 - ((double)distance)/((double)BACKWARD_DISTANCE);
+		return freq * BACKWARD_WEIGHT * probability;
+	}
+	return 0;
+}
+
+/*
+ * Calculate the ExtTSP score for a chain.
+ */
+static exttsp_score_t exttsp_rate_chain(exttsp_chain_t *chain)
+{
+	exttsp_block_t *block = chain->first;
+	exttsp_edge_t *edge;
+	exttsp_score_t score = 0;
+	while (block != NULL) {
+		edge = block->first_edge;
+		while (edge != NULL) {
+			if (edge->dest->chain == chain) { // ignore jumps leaving the chain
+				score += exttsp_rate_edge(edge);
+			}
+			edge = edge->next;
+		}
+		block = block->succ;
+	}
+	return score;
+}
+
+/*
+ * Count blocks and edges and set irn links of blocks to NULL.
+ * The end block is ignored.
+ */
+static void exttsp_init_counters(ir_node *block, void *env)
+{
+	exttsp_env_t *e = (exttsp_env_t*) env;
+	ir_node *end = get_irg_end_block(e->irg);
+	if (block == end) return;
+	e->chain_count++;
+	e->block_count++;
+	foreach_block_succ(block, edge) {
+		if (get_edge_src_irn(edge) == end) continue;
+		e->edge_count++;
+	}
+	set_irn_link(block, NULL);
+}
+
+/*
+ * Create initial chains consisting of one basic block each.
+ * The end block is ignored.
+ */
+static void exttsp_create_chains(ir_node *block, void *env)
+{
+	exttsp_env_t *e = (exttsp_env_t*) env;
+	ir_node *end = get_irg_end_block(e->irg);
+	if (block == end) return;
+	// create block
+	exttsp_block_t *b = (exttsp_block_t*) get_irn_link(block);
+	if (b == NULL) { // no edge to this block occured yet
+		b = &(e->blocks[e->block_count++]);
+	}
+	b->size = get_block_size(block);
+	b->addr = 0;
+	b->succ = NULL;
+	b->first_edge = NULL;
+	b->irn = block;
+	set_irn_link(block, (void*)b);
+	// create chain containing block
+	exttsp_chain_t *c = &(e->chains[e->chain_count++]);
+	c->first = c->last = b;
+	c->length = 1;
+	c->contains_start = (block == get_irg_start_block(e->irg));
+	b->chain = c;
+	// create outgoing edges
+	exttsp_edge_t *last = NULL;
+	foreach_block_succ(block, edge) {
+		ir_node *succ = get_edge_src_irn(edge);
+		if (succ == end) continue; // ignore edges to end block
+		exttsp_edge_t *succ_edge = &(e->edges[e->edge_count++]);
+		succ_edge->src = b;
+		exttsp_block_t *succ_block = (exttsp_block_t*) get_irn_link(succ);
+		if (succ_block == NULL) { // succ block does not exist yet
+			succ_block = &(e->blocks[e->block_count++]);
+			set_irn_link(succ, (void*)succ_block);
+		}
+		succ_edge->dest = succ_block;
+		if (last == NULL) { // current edge is the first
+			b->first_edge = succ_edge;
+		} else {
+			last->next = succ_edge;
+		}
+		last = succ_edge;
+		succ_edge->next = NULL; // list of edges is NULL terminated
+		succ_edge->freq = get_edgefreq_int(&(e->execfreq_factors), block, edge);
+	}
+	c->score = exttsp_rate_chain(c);
+}
+
+/*
+ * Compute gain in ExtTSP score reached by merging two chains.
+ */
+static exttsp_score_t exttsp_compute_merge_gain(exttsp_chain_t *a, exttsp_chain_t *b)
+{
+	exttsp_score_t score = 0;
+	// try simple concatination a+b
+	if (!b->contains_start) {
+		// save state of a
+		exttsp_chain_t save = *a;
+		// merge a and b
+		a->last->succ = b->first;
+		a->last = b->last;
+		a->length += b->length;
+		// recalculate block addresses and set chain of second half
+		// TODO possible to avoid this?
+		unsigned int addr = save.last->addr + save.last->size;
+		exttsp_block_t *cur = b->first;
+		while (cur != NULL) {
+			cur->addr = addr;
+			addr += cur->size;
+			cur->chain = a;
+			cur = cur->succ;
+		}
+		exttsp_score_t new_score = exttsp_rate_chain(a);
+		// restore a
+		*a = save;
+		a->last->succ = NULL;
+		// restore block addresses and chain of second half
+		addr = 0;
+		cur = b->first;
+		while (cur != NULL) {
+			cur->addr = addr;
+			addr += cur->size;
+			cur->chain = b;
+			cur = cur->succ;
+		}
+		new_score -= a->score + b->score; // gain
+		if (new_score > score) {
+			score = new_score;
+			// TODO save used merge type
+		}
+	} else {
+		score = -1; // TODO needed because some merge types are missing
+	}
+	// ...
+	// TODO implement nontrivial merges
+	return score;
+}
+
+// generate debug output for each iteration of the main loop
+#define EXTTSP_DEBUG_OUTPUT_CHAINS {                                                 \
+	DB((dbg, LEVEL_1, "\n%d(%ld) chains, %d(%ld) blocks, %d(%ld) edges\n",           \
+		env.chain_count, ARR_LEN(env.chains),env.block_count,                        \
+		ARR_LEN(env.blocks), env.edge_count, ARR_LEN(env.edges)));                   \
+	for (size_t x=0; x<ARR_LEN(env.chains); x++) {                                   \
+		if (env.chains[x].length == 0) continue;                                     \
+		DB((dbg, LEVEL_1, "\t chain: %zu \t length: %u \t score: %f \t start: %d\n", \
+			x, env.chains[x].length, env.chains[x].score,                            \
+			env.chains[x].contains_start));                                          \
+	}                                                                                \
+}
+
+/*
+ * Create block schedule using the ExtTSP algorithm.
+ */
+static ir_node **be_create_exttsp_block_schedule(ir_graph *irg)
+{
+	exttsp_env_t env;
+	env.irg = irg;
+
+	// count blocks and edges
+	ir_reserve_resources(irg, IR_RESOURCE_IRN_LINK);
+	env.chain_count = env.block_count = env.edge_count = 0;
+	irg_block_walk_graph(irg, exttsp_init_counters, NULL, &env);
+
+	// create data structures
+	obstack_init(&env.obst);
+	env.chains = NEW_ARR_D(exttsp_chain_t, &env.obst, env.chain_count);
+	env.blocks = NEW_ARR_D(exttsp_block_t, &env.obst, env.block_count);
+	env.edges  = NEW_ARR_D(exttsp_edge_t,  &env.obst, env.edge_count);
+	exttsp_score_t **gains = OALLOCN(&env.obst, exttsp_score_t*, ARR_LEN(env.chains));
+	for (size_t i=0; i<ARR_LEN(env.chains); i++)
+		gains[i] = OALLOCNZ(&env.obst, exttsp_score_t, ARR_LEN(env.chains));
+
+	// estimate execution frequencies
+	ir_calculate_execfreq_int_factors(&(env.execfreq_factors), irg);
+	execfreq_factors_graphviz = &(env.execfreq_factors); // TODO remove graphviz
+	dump_cfg_graphviz(irg); // TODO remove graphviz
+
+	// initial chain creation
+	env.chain_count = env.block_count = env.edge_count = 0;
+	irg_block_walk_graph(irg, exttsp_create_chains, NULL, &env);
+	ir_free_resources(irg, IR_RESOURCE_IRN_LINK);
+
+	// compute initial merge gains
+	for (size_t i=0; i<ARR_LEN(env.chains); i++) {
+		for (size_t j=0; j<ARR_LEN(env.chains); j++) {
+			if (i == j) continue;
+			gains[i][j] = exttsp_compute_merge_gain(&env.chains[i], &env.chains[j]);
+		}
+	}
+
+	// chain merging
+	int max_i, max_j;
+	exttsp_score_t max;
+	while (env.chain_count > 1) {
+		EXTTSP_DEBUG_OUTPUT_CHAINS;
+		// find maximum merge gain
+		max_i = max_j = -1;
+		max = 0;
+		for (size_t i=0; i<ARR_LEN(env.chains); i++) {
+			if (env.chains[i].length == 0) continue;
+			for (size_t j=0; j<ARR_LEN(env.chains); j++) {
+				if (i == j || env.chains[j].length == 0) continue;
+				if (gains[i][j] >= max) {
+					max_i = i; max_j = j; max = gains[i][j];
+				}
+			}
+		}
+		assert(max_i >= 0 && max_j >= 0 && "found no chain for merging!");
+		// merge chains with maximum gain    TODO implement nontrivial merges
+		DB((dbg, LEVEL_1, "merging chain %u and %u in a trivial way (gain:%f)\n",
+			max_i, max_j, gains[max_i][max_j]));
+		exttsp_chain_t *chain_i = &env.chains[max_i];
+		exttsp_chain_t *chain_j = &env.chains[max_j];
+		assert(!chain_j->contains_start && "start block is not kept at begin!");
+		chain_i->length += chain_j->length;
+		chain_i->contains_start =
+			chain_i->contains_start || chain_j->contains_start;
+		chain_i->last->succ = chain_j->first;
+		chain_i->last = chain_j->last;
+		// recalculate block addresses of new chain i
+		// TODO integrate into rate_chain to simplify compute_merge_gain?
+		unsigned int addr = 0;
+		exttsp_block_t *cur = chain_i->first;
+		while (cur != NULL) {
+			cur->addr = addr;
+			addr += cur->size;
+			cur->chain = chain_i; // set new chain!
+			cur = cur->succ;
+		}
+		chain_i->score = exttsp_rate_chain(chain_i);
+		// invalidate chain j
+		chain_j->length = 0;
+		chain_j->first = NULL;
+		chain_j->last = NULL;
+		chain_j->score = 0;
+		chain_j->contains_start = false;
+		for (size_t n=0; n<ARR_LEN(env.chains); n++) {
+			gains[max_j][n] = 0;
+			gains[n][max_j] = 0;
+		}
+		// recalculate gains affected by new chain i
+		for (size_t n=0; n<ARR_LEN(env.chains); n++) {
+			if (n == (unsigned int)max_i || env.chains[n].length == 0) continue;
+			gains[max_i][n] = exttsp_compute_merge_gain(&env.chains[max_i],
+								&env.chains[n]);
+			gains[n][max_i] = exttsp_compute_merge_gain(&env.chains[n],
+								&env.chains[max_i]);
+		}
+		env.chain_count--;
+	}
+	EXTTSP_DEBUG_OUTPUT_CHAINS;
+	// get remaining chain which contains the schedule
+	exttsp_chain_t *final_chain = NULL;
+	for (size_t i=0; i<ARR_LEN(env.chains); i++) {
+		if (env.chains[i].length != 0) {
+			final_chain = &env.chains[i];
+			break;
+		}
+	}
+	assert(final_chain != NULL && final_chain->length == env.block_count);
+	// create block schedule from remaining chain
+	struct obstack *const irg_obst = be_get_be_obst(irg);
+	ir_node **const block_list = NEW_ARR_D(ir_node*, irg_obst, env.block_count);
+	exttsp_block_t *block = final_chain->first;
+	int i = 0;
+	while (block != NULL) {
+		block_list[i++] = block->irn;
+		block = block->succ;
+	}
+	obstack_free(&env.obst, NULL);
 	return block_list;
 }
 
@@ -624,7 +1039,13 @@ static ir_node** (*scheduler)(ir_graph*);
 
 ir_node **be_create_block_schedule(ir_graph *irg)
 {
-	return scheduler(irg);
+	DB((dbg, LEVEL_1, "Creating block schedule for '%F'\n", irg));
+	ir_node **block_list = scheduler(irg);
+	DB((dbg, LEVEL_1, "Created blockschedule:\n"));
+	for (size_t i=0; i < ARR_LEN(block_list); i++) {
+		DB((dbg, LEVEL_1, "\t%+F\n", block_list[i]));
+	}
+	return block_list;
 }
 
 BE_REGISTER_MODULE_CONSTRUCTOR(be_init_blocksched)
@@ -636,6 +1057,8 @@ void be_init_blocksched(void)
 		(void*)be_create_normal_block_schedule);
 	be_add_module_to_list(&block_schedulers, "random",
 		(void*)be_create_random_block_schedule);
+	be_add_module_to_list(&block_schedulers, "exttsp",
+		(void*)be_create_exttsp_block_schedule);
 	// set standard scheduler
 	scheduler = (void*)be_create_normal_block_schedule;
 	// register option
